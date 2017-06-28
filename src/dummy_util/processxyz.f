687e6ae6f932cd4639b5946ceff2d41b9c1d85e0
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2017-06-28 15:40:35 +0200

    Add dummy_util with files to be replaced.
    OpenMolcas works (with default options) if these files are copied
    from molcas-extra. These are, however, just dummy placeholders with
    only the interface, so the code can at least be compiled. Files with
    "Empty file" are only needed because they are referenced by some
    of the other files.

---
+++ src/dummy_util/processxyz.f
@@ -0,0 +1,12 @@
+#ifndef _HAVE_EXTRA_
+
+      Subroutine ProcessXYZ(BasisSet, KeepBasis, KeepGroup, iBSSE,
+     &                      SymThr, isHold, ScaleFactor, HyperParSet,
+     &                      isXfield)
+      Logical :: BasisSet, HyperParSet
+      Character(Len=*) :: KeepBasis, KeepGroup
+      Integer :: iBSSE, isHold, isXfield
+      Real*8 :: SymThr, ScaleFactor
+      End Subroutine ProcessXYZ
+
+#endif
---
+++ src/dummy_util/svd_imls.c
@@ -0,0 +1,13 @@
+#ifndef _HAVE_EXTRA_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "svd.h"
+
+void svbksb(double **u, int m, int n, double *w, double **v, double *b, double *x) {};
+
+void svdcmp(double **a, int m, int n, double *w, double **v) {};
+
+#endif
