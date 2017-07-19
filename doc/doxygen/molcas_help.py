2dc24bf2f1dcc08160f027c060c779332fa08ac8
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2017-07-19 14:50:06 +0200

    doxygen: Make scripts executable

--- doc/doxygen/molcas_help.py
+++ doc/doxygen/molcas_help.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#***********************************************************************
+# This file is part of OpenMolcas.                                     *
+#                                                                      *
+# OpenMolcas is free software; you can redistribute it and/or modify   *
+# it under the terms of the GNU Lesser General Public License, v. 2.1. *
+# OpenMolcas is distributed in the hope that it will be useful, but it *
+# is provided "as is" and without any express or implied warranties.   *
+# For more details see the full text of the license in the file        *
+# LICENSE or in <http://www.gnu.org/licenses/>.                        *
+#***********************************************************************
 # Lots of problems with this script!!!
 
 from __future__ import print_function
