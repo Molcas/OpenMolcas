261dd246f7fe52ca21d827b45f7e479b20173759
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2017-07-19 13:38:32 +0200

    CMake: No dga_util without MPI, no warnings in dummy_util
    dga_util (or GA) is not required if MPI=OFF. Also added calls
    to unused_* in dummy_util to allow compilation with BIGOT=ON

--- src/dummy_util/eigen.f
+++ src/dummy_util/eigen.f
@@ -3,6 +3,11 @@
       SUBROUTINE EIGEN(A,R,N,MV,MFKR)
       IMPLICIT REAL*8 (A-H,O-Z)
       DIMENSION A(*),R(*)
+      CALL Unused_Real_Array(A)
+      CALL Unused_Real_Array(R)
+      CALL Unused_Integer(N)
+      CALL Unused_Integer(MV)
+      CALL Unused_Integer(MFKR)
       END SUBROUTINE EIGEN
 
 #endif
--- src/dummy_util/processxyz.f
+++ src/dummy_util/processxyz.f
@@ -7,6 +7,15 @@
       Character(Len=*) :: KeepBasis, KeepGroup
       Integer :: iBSSE, isHold, isXfield
       Real*8 :: SymThr, ScaleFactor
+      Call Unused_Logical(BasisSet)
+      Call Unused_Character(KeepBasis)
+      Call Unused_Character(KeepGroup)
+      Call Unused_Integer(iBSSE)
+      Call Unused_Real(SymThr)
+      Call Unused_Integer(isHold)
+      Call Unused_Real(ScaleFactor)
+      Call Unused_Logical(HyperParSet)
+      Call Unused_Integer(isXfield)
       End Subroutine ProcessXYZ
 
 #endif
