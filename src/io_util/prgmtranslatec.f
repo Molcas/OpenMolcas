4377c667db8f908df7b37b6d7cea11189d69f954
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2017-09-10 17:44:35 +0200

    Use a more portable C-Fortran interface for prgmtranslatec

--- src/io_util/prgmtranslatec.f
+++ src/io_util/prgmtranslatec.f
@@ -11,21 +11,31 @@
 * Copyright (C) 2017, Ignacio Fdez. Galvan                             *
 ************************************************************************
 
-* Wrapper for the OpenMolcas PrgmTranslateC routine, so it works without using the module
+* Wrapper for the OpenMolcas PrgmTranslate_Mod routine, so it works
+* without using the module (i.e., from C). Some C-to-Fortran conversion
+* needs to be done, which feels quite hackish.
 
 #ifndef _HAVE_EXTRA_
+#define MAXSTR 1024
       Subroutine PrgmTranslateC(InStr,l1,OutStr,l2,Par)
+      Use ISO_C_Binding, Only: C_CHAR, C_NULL_CHAR
       Use Prgm
       Implicit None
-      Character (Len=*), Intent(In) :: InStr
-      Character (Len=*), Intent(Out) :: OutStr
-      Character (Len=Len(InStr)) :: TmpStr
+      Character(Kind=C_CHAR), Intent(In) :: InStr(*)
+      Character(Kind=C_CHAR), Intent(Out) :: OutStr(*)
       Integer, Intent(In) :: Par, l1
       Integer, Intent(Out) :: l2
-      TmpStr=InStr
-      If (Len(InStr) .gt. l1) TmpStr(l1+1:)=''
-      Call PrgmTranslate_Mod(TmpStr,l1,OutStr,l2,Par)
-      If (Len(OutStr) .gt. l2) OutStr(l2+1:l2+1)=Char(0)
+      Character (Len=MAXSTR) :: TmpStr, TmpStr2
+      Integer :: i
+      TmpStr=''
+      Do i=1,l1
+        TmpStr(i:i)=InStr(i)
+      End Do
+      Call PrgmTranslate_Mod(TmpStr,l1,TmpStr2,l2,Par)
+      Do i=1,l2
+        OutStr(i)=TmpStr2(i:i)
+      End Do
+      OutStr(l2+1:l2+1)=C_NULL_CHAR
       End Subroutine PrgmTranslateC
 #elif defined (NAGFOR)
 c Some compilers do not like empty files
