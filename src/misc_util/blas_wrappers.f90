7596a2831ff058f3e703806f5f247e9cf0d1c559
Liviu Ungur <Liviu.Ungur@chem.kuleuven.be>
2018-01-12 19:51:13 +0100

    add zgemm_ support

--- src/misc_util/blas_wrappers.f90
+++ src/misc_util/blas_wrappers.f90
@@ -29,6 +29,7 @@
 ! dspmv_
 ! dgemv_
 ! dznrm2_
+! zgemm_
 
 ! Set the appropriate integer size of the library interface:
 #ifdef LINALG_I4
@@ -273,6 +274,57 @@ subroutine dgemm_(transa,transb,m_,n_,k_, &
   endif
 #endif
 end subroutine
+
+
+subroutine zgemm_(transa,transb,m_,n_,k_, &
+      &           alpha,a,lda_,b,ldb_,beta,c,ldc_)
+  implicit none
+  character        transa, transb
+  complex*16       alpha, beta
+  integer          k_, lda_, ldb_, ldc_, m_, n_
+  complex*16       a(lda_,*), b(ldb_,*), c(ldc_,*)
+#ifdef MOLCAS_TO_BLAS_INT
+  BLASINT          k,  lda,  ldb,  ldc,  m,  n
+#endif
+#ifdef _CUDA_BLAS_
+  parameter (ncuda=128*128)
+  integer*4        k4,lda4,ldb4,ldc4,m4,n4
+#endif
+
+  if(m_.eq.0.and.n_.eq.0) return
+
+#ifdef _CUDA_BLAS_
+  if(n*m.gt.ncuda) then
+    k4=k_
+    lda4=lda_
+    ldb4=ldb_
+    ldc4=ldc_
+    m4=m_
+    n4=n_
+    call cublas_zgemm(transa,transb,
+    &     m4,n4,k4,alpha,a,lda4,b,ldb4,beta,c,ldc4)
+  else
+#endif
+
+#ifdef MOLCAS_TO_BLAS_INT
+    m=m_
+    n=n_
+    k=k_
+    lda=lda_
+    ldb=ldb_
+    ldc=ldc_
+    call zgemm(transa,transb, &
+        &      m,n,k,alpha,a,lda,b,ldb,beta,c,ldc)
+#else
+    call zgemm(transa,transb,m_,n_,k_, &
+        &      alpha,a,lda_,b,ldb_,beta,c,ldc_)
+#endif
+
+#ifdef _CUDA_BLAS_
+  endif
+#endif
+end subroutine
+
 
 subroutine dspmv_ ( uplo,n_,alpha,ap,x,incx_,beta,y,incy_)
   implicit none
