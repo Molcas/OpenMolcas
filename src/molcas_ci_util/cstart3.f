d3683536c78a336d0bc2824bc68ec4dfa50d4813
Mickael Delcey <mickael.delcey@gmail.com>
2018-07-07 08:37:47 +0200

    Forgot a few

--- src/molcas_ci_util/cstart3.f
+++ src/molcas_ci_util/cstart3.f
@@ -73,7 +73,7 @@ c      Dimension iToc(15)
 C$$$        ExplE(1) = C(1)  ! Commented out by Jesper
 C$$$        ExplV(1) = 1.0d0 ! Commented out by Jesper
         C(1) = 1.0d0
-        Call Save_tmp_CI_vec(1,lRoots,nConf,C,LuDavid)
+        Call Save_tmp_CI_vec(1,nConf,C,LuDavid)
         Call qExit('CStart')
         Return
       End If
@@ -97,7 +97,7 @@ C$$$        ExplV(1) = 1.0d0 ! Commented out by Jesper
             k = iSel(j)
             C(k) = ExplV(j+(i-1)*nSel)
           End Do
-          Call Save_tmp_CI_vec(i,lRoots,nConf,C,LuDavid)
+          Call Save_tmp_CI_vec(i,nConf,C,LuDavid)
           If ( IPRLEV.ge. INSANE ) then
             Write (String,'(A,I2)') 'CI vector of root',i
             Write (String,'(A,I4,A)') '(max. ',nSel,' elements)'
