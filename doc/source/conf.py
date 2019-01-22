75274f6b7de2821b51ab205984c007b6933c2fb0
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2019-01-22 11:13:40 +0100

    Some updates for doc compilation
    Add link to PDF file, skip test jobs on master (relevant for manual pipelines).

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -123,10 +123,11 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'molcas'
-#html_theme_options = {
+html_theme_options = {
+  'pdf_file': os.environ.get('PDF_FILE', '')
 #    'rightsidebar': 'false',
 #    'stickysidebar': 'true'
-#}
+}
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
