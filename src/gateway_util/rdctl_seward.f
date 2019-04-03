f124aea760a06f7bd926e208eeed7288711657ab
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2019-04-03 11:27:37 +0200

    Allow COORD + TINKER
    If the "Expert" option is enabled, COORD and TINKER can be combined,
    where the latter will replace the coordinates (but not the atom
    names and labels) set by the former.

--- src/gateway_util/rdctl_seward.f
+++ src/gateway_util/rdctl_seward.f
@@ -1558,6 +1558,8 @@ c     Go To 998
 *
  9771 Expert = .True.
       GWInput = Run_Mode.eq.G_Mode
+      Call WarningMessage(1,
+     &   ' EXPERT option is ON!')
       Go To 998
 *                                                                      *
 ****** DIST ************************************************************
@@ -3271,6 +3273,13 @@ c
       ITkQMMM = IsFreeUnit(ITkQMMM)
       Call Molcas_Open (ITkQMMM,'QMMM')
 #ifdef _HAVE_EXTRA_
+      If (Expert) Then
+         If (iCoord.gt.1) Then
+            Call WarningMessage(1,
+     &         'TINKER and COORD keywords cannot be combined '//
+     &         'with molcas_extra')
+         End If
+      End If
       Call XYZread(ITkQMMM,ForceZMAT,nCoord,iErr)
       If (iErr.ne.0) Then
         Key='RdCtl_Seward: Tinker+XYZread failed:'//
@@ -3280,7 +3289,15 @@ c
       End If
       Call XYZcollect(iCoord,nCoord,OrigTrans,OrigRot,nFragment)
 #else
-      Call Read_XYZ(ITkQMMM,OrigRot,OrigTrans)
+      If (Expert) Then
+         If (iCoord.gt.1) Then
+            Call WarningMessage(1,
+     &          'TINKER coordinates replacing COORD')
+         End If
+         Call Read_XYZ(ITkQMMM,OrigRot,OrigTrans,Replace=(iCoord.gt.1))
+      Else
+         Call Read_XYZ(ITkQMMM,OrigRot,OrigTrans)
+      End If
 #endif
       Close(ITkQMMM)
       GWInput = .True.
@@ -3556,6 +3573,13 @@ c
       LuXYZ = isFreeUnit(LuXYZ)
       Call molcas_open(LuXYZ,'GMX.XYZ')
 #ifdef _HAVE_EXTRA_
+      If (Expert) Then
+         If (iCoord.gt.1) Then
+            Call WarningMessage(1,
+     &         'GROMACS and COORD keywords cannot be combined '//
+     &         'with molcas_extra')
+         End If
+      End If
       Call XYZread(LuXYZ,ForceZMAT,nCoord,iErr)
       If (iErr.NE.0) Then
          Message='RdCtl_Seward: XYZread returned non-zero error code'
@@ -3564,7 +3588,15 @@ c
       End If
       Call XYZcollect(iCoord,nCoord,OrigTrans,OrigRot,nFragment)
 #else
-      Call Read_XYZ(LuXYZ,OrigRot,OrigTrans)
+      If (Expert) Then
+         If (iCoord.gt.1) Then
+            Call WarningMessage(1,
+     &          'TINKER coordinates replacing COORD')
+         End If
+         Call Read_XYZ(LuXYZ,OrigRot,OrigTrans,Replace=(iCoord.gt.1))
+      Else
+         Call Read_XYZ(LuXYZ,OrigRot,OrigTrans)
+      End If
 #endif
       Close(LuXYZ)
 #else
@@ -3919,14 +3951,18 @@ c      endif
          Call Quit_OnUserError()
       End If
       If (DoTinker.and.iCoord.gt.1) Then
-         Call WarningMessage(2,
-     &      'TINKER and COORD keywords cannot be used together')
-         Call Quit_OnUserError()
+         If (.Not.Expert) Then
+            Call WarningMessage(2,
+     &         'TINKER and COORD keywords cannot be used together')
+            Call Quit_OnUserError()
+         End If
       End If
       If (DoGromacs.and.iCoord.gt.1) Then
-         Call WarningMessage(2,
-     &      'GROMACS and COORD keywords cannot be used together')
-         Call Quit_OnUserError()
+         If (.Not.Expert) Then
+            Call WarningMessage(2,
+     &         'GROMACS and COORD keywords cannot be used together')
+            Call Quit_OnUserError()
+         End If
       End If
 *
       If (Test) Then
@@ -3940,11 +3976,6 @@ c      endif
 *                                                                      *
 ************************************************************************
 *                                                                      *
-      if (Expert) then
-         Call WarningMessage(1,
-     &      ' EXPERT option is ON!')
-      endif
-
       IF (BSS.AND..Not.DKroll) Then
          Call WarningMessage(2,
      &           ';BSSM GOES ALWAYS WITH DOUGLAS.'//
