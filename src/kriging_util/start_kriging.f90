0c22823bdbd7f5843923f3f940b05489054a7ab3
roland <roland.lindh@kemi.uu.se>
2019-04-10 13:50:53 +0200

    Cleanup.

--- src/kriging_util/start_kriging.f90
+++ src/kriging_util/start_kriging.f90
@@ -12,86 +12,37 @@
 !***********************************************************************
 
       Subroutine Start_Kriging(iter,nInter,qInt,Grad,Energy,anAIT,pAIT,lbAI,npxAIT)
-!#include "stdalloc.fh"
         use globvar
         Integer npxAIT,nInter,iter
         Real*8 qInt(nInter,iter+1),Grad(nInter,iter),Energy(iter),pAIT,lbAI(3)
         Logical anAIT
-        ! if (allocated(x)) then
-        !   deallocate (x,y,lb,dy,nx)
-        ! else
-          allocate (x(nInter,iter),y(iter),lb(3),dy(nInter*iter), &
+!
+        allocate (x(nInter,iter),y(iter),lb(3),dy(nInter*iter), &
                     nx(nInter,1))
-        ! endif
-        !iter=iterAI
-        !nInter=nInterT
+!
         npxAI=npxAIT
         pAI=pAIT
         anAI=anAIT
         npx=1 !npxAI
         nx=qInt(:,iter+1:iter+1)
-        Write (6,*) 'Kriging values in Start Kriging'
-        !Write (6,*) 'iter:', iter
-        Write (6,*) 'nInter', nInter
-        Write (6,*) 'npxAI', npxAI
-        Write (6,*) 'Grad: ',Grad
-        Write (6,*) 'Grad shape: ',shape(Grad)
-        Write (6,*) 'Energy: ',Energy
-        Write (6,*) 'npx: ',npx
-        Write (6,*) 'lbAI: ',lbAI
-        !---------------Testing old data
-
-        !--------------------------------
-        !anamat = anAI
-        !p = pAI
-        !NS = iter
-        !dims = nInter
-        !parameters comming from the predicted points
-        !nx=x
         lb=lbAI
-        !----for test puorposes
-        ! lb(1)=0.1
-        ! lb(2)=6
-        ! lb(3)=100
-        ! npx=4
-        ! do i=1,int(npx)
-        !   nx(1,i)=(real(i)-1.0)*4.0/real(npx-1)
-        ! enddo
-        !-----------
         x = qInt(1:nInter,1:iter)
-#ifdef _NOT_USED_
-        Write (6,*) 'nx: ',nx
-        Write (6,*) 'nx size: ',size(nx)
-        Write (6,*) 'nx shape: ',shape(nx)
-        Write (6,*) 'x: ',x
-        Write (6,*) 'x size: ',size(x)
-        Write (6,*) 'x shape: ',shape(x)
-#endif
         y = Energy
-#ifdef _NOT_USED_
-        Write (6,*) 'y: ',y
-        Write (6,*) 'y size: ',size(y)
-        Write (6,*) 'y shape: ',shape(y)
-#endif
         do i=1,nInter
           do j=1,iter
             dy(j+(i-1)*iter) = Grad(i,j)
           enddo
         enddo
-#ifdef _NOT_USED_
-        Write (6,*) 'dy: ',dy
-        Write (6,*) 'dy size: ',size(dy)
-        Write (6,*) 'dy shape: ',shape(dy)
-#endif
+!
         call kernels(iter,nInter)
+!
         Energy(iter+1)=pred(npx)
         Grad(:,iter+1)=gpred
+
         write(6,*) 'New values of Energy and grad', pred(npx), gpred
         deallocate (x,y,lb,dy,nx,l)
-        !write (6,*) 'deallo x'
         deallocate (full_R,rl,dl,mat,Iden)
-        !write (6,*) 'deallo full_R'
         deallocate (kv,pred,gpred,hpred,var,sigma,cv,ll)
-        !write (6,*) 'deallo kv,pred,gpred,hpred,var,sigma,cv'
+!
         return
       end
