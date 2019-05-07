5c8f648a4475ea737a5f4bd3d81490194cc25748
Quan Phung <manhquank9@gmail.com>
2019-05-07 08:09:51 +0000

    Fix compiling error (Block and CheMPS2) introduced by merge request !142

--- src/rasscf/sgfcin.f
+++ src/rasscf/sgfcin.f
@@ -50,7 +50,8 @@
 #endif
       use rasscf_data, only : EMY, KSDFT, dftfock, exfac, nac, nacpar,
      &    noneq, potnuc, rfpert,
-     &    tot_charge, tot_el_charge, tot_nuc_charge
+     &    tot_charge, tot_el_charge, tot_nuc_charge,
+     &    doBlockDMRG
       use general_data, only : iSpin, nActEl, nSym, nTot1,
      &    nBas, nIsh, nAsh, nFro
       implicit none
