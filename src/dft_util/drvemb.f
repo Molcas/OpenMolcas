fa1ca2169c4185051372db1db47961a61bb8bcf4
Alexander Zech <alexander.zech@unige.ch>
2019-06-24 18:52:12 +0200

    Move done_rasscf function to file

--- src/dft_util/drvemb.f
+++ src/dft_util/drvemb.f
@@ -8,8 +8,8 @@
 * For more details see the full text of the license in the file        *
 * LICENSE or in <http://www.gnu.org/licenses/>.                        *
 *                                                                      *
-* Copyright (C) 2010-2017, Francesco Aquilante                         *
-*               2017,      Alexander Zech                              *
+* Copyright (C) 2010,2012,2017, Francesco Aquilante                    *
+*               2015,2017, Alexander Zech                              *
 ************************************************************************
       Subroutine DrvEMB(h1,D,RepNuc,nh1,
      &                  KSDFT,ExFac,Do_Grad,Grad,nGrad,
@@ -859,7 +859,7 @@ c Avoid unused argument warnings
 * Fill allocated mem with zeroes.
         Call dcopy_(nTot1,[0.0D0],0,Work(ipD),1)
 
-        Call DONE_RASSCF_X(Work(ipxxCMO),Work(ipxxOCCN),
+        Call DONE_RASSCF_x(Work(ipxxCMO),Work(ipxxOCCN),
      &                     Work(ipD)) ! computes D=CnC'
 * Nonelectr. Vemb with GS and excited state density
         Vemb_Xstate=ddot_(nVemb,Vemb,1,Work(ipD),1)
@@ -904,80 +904,4 @@ c Avoid unused argument warnings
 
       Return
       End
-************************************************************************
-*                                                                      *
-************************************************************************
-      Subroutine Done_RASSCF_X(CMO,OCC,D)
-************************************************************************
-*                                                                      *
-*     (c) Copyright. All rights reserved                               *
-*                                                                      *
-*     This code or parts thereof may not be copied or redistributed,   *
-*     without the written permission of the author. The use is re-     *
-*     stricted to research purposes only, and the material may not be  *
-*     included in any commercial product.                              *
-*                                                                      *
-************************************************************************
-*                                                                      *
-*     purpose:                                                         *
-*     Compute the active one-body density                              *
-*                                                                      *
-*     calling arguments:                                               *
-*     CMO     : input, array of real*8                                 *
-*               MO-coefficients                                        *
-*     OCC     : input, array of real*8                                 *
-*               occupation numbers                                     *
-*     D       : output, array of real*8                                *
-*               total one-body density                                 *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     written by:                                                      *
-*     M.P. Fuelscher                                                   *
-*     University of Lund, Sweden, 1996                                 *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     history: none                                                    *
-*                                                                      *
-************************************************************************
 
-      Implicit Real*8 (A-H,O-Z)
-
-      Dimension CMO(*) , OCC(*) , D(*)
-
-*      Include 'rasdim.inc'
-#include <rasdim.fh>
-#include "general.fh"
-#include "WrkSpc.fh"
-
-      iOff1 = 0
-      iOff2 = 0
-      iOff3 = 0
-      Do iSym = 1,nSym
-        iBas = nBas(iSym)
-        iFro = nFro (iSym)
-        iIsh = nIsh (iSym)
-        iAsh = nAsh (iSym)
-        If ( iBas.ne.0 ) then
-          Do i = 1,iBas
-            ii = (i*i-i)/2
-            Do j = 1,i
-              Sum = 0.0d0
-              Do k = 1,iFro+iIsh+iAsh
-                Sum = Sum + OCC(iOff3+k)
-     &                    * CMO(iOff1+(k-1)*iBas+i)
-     &                    * CMO(iOff1+(k-1)*iBas+j)
-              End Do
-              D(iOff2+ii+j) = 2.0d0*Sum
-              If (j.eq.i) D(iOff2+ii+j) = Sum
-            End Do
-          End Do
-        End If
-        iOff1 = iOff1 + iBas*iBas
-        iOff2 = iOff2 + (iBas*iBas+iBas)/2
-        iOff3 = iOff3 + iBas
-      End Do
-
-      Return
-      End
