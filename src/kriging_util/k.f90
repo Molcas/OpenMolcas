ef3bdb4b1e5f56a916a0365a37af6a0d65723eb9
roland <roland.lindh@kemi.uu.se>
2019-08-05 09:19:48 +0200

    Assign with correct type.

--- src/kriging_util/k.f90
+++ src/kriging_util/k.f90
@@ -92,7 +92,7 @@
             variance = dot_product(Ys,Kv)/m_t
 !
             detR = 0.0d0
-            sign = 1.0d0
+            sign = 1
             do i=1,m_t
                 ! if (diagA(i).le.0) sign=sign*(-1)
                 detR = detR + log(abs(diagA(i)))
