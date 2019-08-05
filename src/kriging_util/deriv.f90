1bbc963d3fc2571f908c49296ee7346fa3f63b1b
roland <roland.lindh@kemi.uu.se>
2019-08-05 11:56:07 +0200

    Remove on-the-fly allocations with explicit allocations.

--- src/kriging_util/deriv.f90
+++ src/kriging_util/deriv.f90
@@ -12,15 +12,23 @@
 !***********************************************************************
 subroutine deriv(finder,foutder,nd,d1,d2)
     use globvar
+#include "stdalloc.fh"
     integer d1,d2,nd
-    real*8 finder(d1,d2),foutder(d1,d2),a,b(d1,d2),kr,nr
-    nr=real(nd)
-    a=Gamma(nr+1.0)/h**nd
+    real*8 finder(d1,d2),foutder(d1,d2),a,kr,nr
+    real*8, Allocatable :: b(:,:)
+!
+    Call mma_allocate(b,d1,d2,label="b")
+!
+    nr=DBLE(nd)
+    a=Gamma(nr+1.0d0)/h**nd
     b=0.0
     do k=0,nd
-        kr=real(k)
+        kr=DBLE(k)
         finder=finder+kr*h
         b=b+DBLE((-1)**(k+1))/(Gamma(nr-kr+1.0D0)*Gamma(kr+1.0D0))*finder
     enddo
     foutder=a*b*DBLE((-1)**(nr+1))
+!
+    Call mma_deallocate(b)
+!
 end
