18864d7400accd06a5d0c3cc10d6cba9fa757945
Gerardo Raggi <gerardo.raggi@kemi.uu.se>
2019-09-17 09:06:22 +0200

    AnaHess c E w Num Rol

--- src/kriging_util/gradient_kriging.f90
+++ src/kriging_util/gradient_kriging.f90
@@ -15,6 +15,13 @@
         Integer nInter,nPoints
         Real*8 x_(ndimx,1),dy_(ndimx)
 !
+!#define _Grad_Test
+#ifdef _Grad_Test
+        Real*8 Scale,Delta,Fact,tempX(ndimx,1),tgrad(ndimx),thgrad(ndimx)
+        Real*8 GradT
+        !       Numerical Gradient-Hessian threshold numHt from outside parameter
+        GradT = numHt!1.0D-7
+#endif
         nPoints=nPoints_save
         nInter=nInter_save
 !
@@ -28,5 +35,28 @@
         call predict(1,nPoints,nInter)
         dy_=gpred(npx,:)
 !
+#ifdef _Grad_Test
+       ! Numerical Gradient of GEK
+        do i = 1,nInter
+!
+        ! write (6,*) 'hpred(npx,i,:): ', i, hpred(npx,i,:)
+        enddo
+        do i = 1,nInter
+                do j = 1,nInter
+                        write(6,*) 'i,j',i,j,HessT
+                        write(6,*) 'hpred, ddy_',hpred(npx,i,j),ddy_(i,j)
+                        if (abs(ddy_(i,j)-hpred(npx,i,j)).gt.HessT) then
+                                Write(6,*) 'Error in entry',i,',',j,'of the hessian matrix'
+                                Call RecPrt('Anna Hess',' ',ddy_,nInter,nInter)
+                                Call RecPrt('Num Hess',' ',hpred,nInter,nInter)
+                                Write(6,*) 'abs(ddy_(i,j)+ HessT)',abs(ddy_(i,j)+ HessT)
+                                Write(6,*) 'abs(ddy_(i,j)- HessT)',abs(ddy_(i,j)- HessT)
+                                Write(6,*) 'abs(hpred(npx,i,j))',abs(hpred(npx,i,j))
+                                Call Abend()
+                        endif
+                enddo
+        enddo
+        if (anHe.eqv..False.) ddy_ = hpred(npx,:,:)
+#endif
         return
       end
