07469b6b32883438b30786309fcbf35daa281b23
roland <roland.lindh@kemi.uu.se>
2019-10-08 16:18:38 +0200

    Fix too long line.

--- src/fock_util/cho_LK_rassi_x.f
+++ src/fock_util/cho_LK_rassi_x.f
@@ -1199,7 +1199,7 @@ C------------------------------------------------------------
                             ipFaa = ipFk + MxBasSh + iaSh - 1
 
                             iaSkip=Min(1,Max(0,
-     &                            abs(ipLab(iaSh,kDen)-ipAbs))) ! = 1 or 0
+     &                            abs(ipLab(iaSh,kDen)-ipAbs)))!= 1 or 0
 
                             iOffSha = kOffSh(iaSh,lSym)
 
@@ -1268,7 +1268,7 @@ C --------------------------------------------------------------------
                             ipFaa = ipFk + MxBasSh + iaSh - 1
 
                             iaSkip=Min(1,Max(0,
-     &                            abs(ipLab(iaSh,kDen)-ipAbs))) ! = 1 or 0
+     &                            abs(ipLab(iaSh,kDen)-ipAbs)))!= 1 or 0
 
                             iOffSha = kOffSh(iaSh,lSym)
 
