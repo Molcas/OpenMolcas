31132da45f96cb3822548b4c3a4037558b0e6cf9
Oskar Weser <oskar.weser@gmail.com>
2019-10-15 21:49:11 +0200

    MAINT: last cosmetic changes. Use LGPLv2 as well

--- Tools/analyze_molcas/setup.py
+++ Tools/analyze_molcas/setup.py
@@ -20,7 +20,7 @@ def read(*names, **kwargs):
 setup(
     name='analyze_molcas',
     version='0.0.0',
-    license='MIT license',
+    license='LGPLv2',
     description='Module for molcas utility functions.',
     author='Oskar Weser',
     author_email='oskar.weser@gmail.com',
@@ -31,26 +31,21 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Operating System :: Unix',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Utilities',
     ],
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.6',
     install_requires=[
         'click', 'attrs'
     ],
