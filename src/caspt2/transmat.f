ef3de7923c16ea335d5ab7229d9178b8c86a85bf
Stefano Battaglia <stefano.battaglia@kemi.uu.se>
2019-12-09 14:03:46 +0100

    diagonalize.f renamed to eigen.f to mirror subroutine name and bug in transmat fixed

--- src/caspt2/transmat.f
+++ src/caspt2/transmat.f
@@ -12,20 +12,27 @@
 ************************************************************************
       subroutine transmat(A,U,N)
       implicit none
+#include "stdalloc.fh"
 ***
 * This subroutine carries out the following transformation:
 *       U^T * A * U
 * where A and U are NxN matrices
 ***
-
       integer N
       real(8) A(N,N)
       real(8) U(N,N)
+      real(8),allocatable :: B(:,:)
 
-* A = U^T * A
-      call dgemm_('T', 'N', N, N, N, 1.0d0, U, N, A, N, 0.0d0, A, N)
-* A = A * U
-      call dgemm_('N', 'N', N, N, N, 1.0d0, A, N, U, N, 0.0d0, A, N)
+* Allocate temporary array B
+      call mma_allocate(B,N,N)
+      B=0.0d0
+
+* B = U^T * A
+      call dgemm_('T', 'N', N, N, N, 1.0d0, U, N, A, N, 0.0d0, B, N)
+* A = B * U
+      call dgemm_('N', 'N', N, N, N, 1.0d0, B, N, U, N, 0.0d0, A, N)
+
+      call mma_deallocate(B)
 
       return
       end
