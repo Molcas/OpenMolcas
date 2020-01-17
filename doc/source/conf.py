066f60119e026702166bdcf6cef8fb7fddce6963
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2020-01-17 11:23:14 +0100

    Update to 2020

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -67,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Molcas'
-copyright = u'2017–2019, MOLCAS Team'
+copyright = u'2017–2020, MOLCAS Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
