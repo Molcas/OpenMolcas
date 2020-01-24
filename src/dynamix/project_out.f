0eb8652d145f8182ce1f406bf229562ef9d7f14d
Morgan VACHER <vacher-m@jaws.cluster>
2020-01-24 11:37:30 +0100

    Correct copyright

--- src/dynamix/project_out.f
+++ src/dynamix/project_out.f
@@ -8,7 +8,7 @@
 * For more details see the full text of the license in the file        *
 * LICENSE or in <http://www.gnu.org/licenses/>.                        *
 *                                                                      *
-* Copyright (C) 2006, Igor Schapiro                                    *
+* Copyright (C) 2020, Morgane Vacher                                   *
 ************************************************************************
 C
 C *********************************************************************
