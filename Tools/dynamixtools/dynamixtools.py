e68c3ae12d24645bf9d10c5231c901a08f8ded64
roland <roland.lindh@kemi.uu.se>
2020-04-27 11:54:21 +0200

    Enable dynamixtool verification.

--- Tools/dynamixtools/dynamixtools.py
+++ Tools/dynamixtools/dynamixtools.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #***********************************************************************
 # This file is part of OpenMolcas.                                     *
 #                                                                      *
