f9b54447557b8d3fd7cbceee83797efc164a5dc0
roland <roland.lindh@kemi.uu.se>
2020-06-03 12:14:41 +0200

    cleanup.

--- src/amfi_util/contandmult.f
+++ src/amfi_util/contandmult.f
@@ -16,15 +16,18 @@
 #include "param.fh"
 #include "ired.fh"
 #include "Molcas.fh"
-#include "WrkSpc.fh"
+#include "stdalloc.fh"
+      Real*8, Allocatable:: Dummy(:), OCA(:,:), OCA2(:,:), OCA3(:,:)
       logical makemean,AIMP,oneonly
       character*8 xa,ya,za
       dimension xa(4),ya(4),za(4),
-     *   onecart(mxcontL,MxcontL,(Lmax+Lmax+1)*(Lmax+1),Lmax,3),
-     *   onecontr(mxcontL,MxcontL,-Lmax:Lmax,3,Lmax),
-     *   oneoverR3((MxprimL*MxprimL+MxprimL)/2,Lmax)
+     *          onecart(mxcontL,MxcontL,(Lmax+Lmax+1)*(Lmax+1),Lmax,3),
+     *          onecontr(mxcontL,MxcontL,-Lmax:Lmax,3,Lmax),
+     *          oneoverR3((MxprimL*MxprimL+MxprimL)/2,Lmax)
       common /nucleus/ charge,Exp_Finite
+*
       IPNT(I,J)=(J*J-J)/2+I
+*
 cbs   get back the real number of functions for the finite nucleus
       if (ifinite.eq.2) ncontrac(0)=ncontrac_keep
 c###############################################################################
@@ -47,14 +50,12 @@ c
 cbs   clean the arrays for cartesian integrals
 C
       length3=(numbalLcart*numbalLcart+numbalLcart)/2
-      Call GetMem('OCAX','Allo','Real',iocax,6*length3+MxContL*MxContL)
-      iocay=iocax+length3
-      iocaz=iocay+length3
-      iocax2=iocaz+length3
-      iocay2=iocax2+length3
-      iocaz2=iocay2+length3
-      idummy=iocaz2+length3
-      call dzero(work(iocax),6*length3+MxContL*MxContL)
+      Call mma_allocate(OCA,Length3,3,Label='OCA')
+      Call mma_allocate(OCA2,Length3,3,Label='OCA2')
+      Call mma_allocate(Dummy,MxContL**2,Label='Dummy')
+      Dummy(:)=0.0D0
+      OCA(:,:)=0.0D0
+      OCA2(:,:)=0.0D0
 c
 c
 c
@@ -76,7 +77,7 @@ cbs                                  are modelled for finite nucleus somewhere e
       do L=1,Lhigh
               call contone(L,oneoverr3(1,L),onecontr(1,1,-Lmax,1,L),
      *                     Lmax,contrarray(iaddtyp3(L)),nprimit(L),
-     *                     ncontrac(L),MxcontL,work(idummy),
+     *                     ncontrac(L),MxcontL,Dummy,
      *                     onecart(1,1,1,L,1),
      *                     onecart(1,1,1,L,2),
      *                     onecart(1,1,1,L,3),
@@ -98,130 +99,126 @@ cbs   determine  if L_X L_Y or L_Z
         ipowy=ipowxyz(2,mfirst,Lrun)+ipowxyz(2,msec,Lrun)
         ipowz=ipowxyz(3,mfirst,Lrun)+ipowxyz(3,msec,Lrun)
 c
-              mrun=mrun+1
+        mrun=mrun+1
 cbs     now determine the irreducable representations
-              iredfirst=iredLM(Mfirst,Lrun)
-              iredsec=iredLM(Msec,Lrun)
+        iredfirst=iredLM(Mfirst,Lrun)
+        iredsec=iredLM(Msec,Lrun)
 cbs     check out which IR is the lower one.
-              if (iredfirst.le.iredsec) then
+        if (iredfirst.le.iredsec) then
+*
 cbs     calculate shift to get to the beginning of the block
-       iredired=shiftIRIR((iredsec*iredsec-iredsec)/2+iredfirst)
-     *                +incrlm(Mfirst,Lrun)*itotalperIR(iredsec)+
-     *                incrLM(Msec,Lrun)
-       if (mod(ipowx,2).eq.0.and.mod(ipowy,2).eq.1.and.
-     * mod(ipowz,2).eq.1) then
-                      do icartfirst=1,ncontrac(Lrun) ! loop first index
-                      do icartsec=1,ncontrac(Lrun)   ! loop second index
-                      work(iocax+iredired+(icartsec-1))=
-     *                work(iocax+iredired+(icartsec-1))
+           iredired= shiftIRIR((iredsec*iredsec-iredsec)/2+iredfirst)
+     *             + incrlm(Mfirst,Lrun)*itotalperIR(iredsec)
+     *             + incrLM(Msec,Lrun)
+           if (mod(ipowx,2).eq.0.and.mod(ipowy,2).eq.1.and.
+     *         mod(ipowz,2).eq.1) then
+              do icartfirst=1,ncontrac(Lrun) ! loop first index
+                 do icartsec=1,ncontrac(Lrun)   ! loop second index
+                    oca(iredired+icartsec,1)=oca(iredired+icartsec,1)
      *                +onecart(icartfirst,icartsec,mrun,Lrun,1)
-                      enddo
-cbs             shift pointer by number of functions in IR
-                      iredired=iredired+itotalperIR(iredsec)
-                      enddo
-        endif
-       if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.0.and.
-     * mod(ipowz,2).eq.1) then
-                      do icartfirst=1,ncontrac(Lrun) ! loop first index
-                      do icartsec=1,ncontrac(Lrun)   ! loop second index
-                      work(iocay+iredired+(icartsec-1))=
-     *                work(iocay+iredired+(icartsec-1))
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredsec)
+             enddo
+          endif
+          if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.0.and.
+     *        mod(ipowz,2).eq.1) then
+              do icartfirst=1,ncontrac(Lrun) ! loop first index
+                 do icartsec=1,ncontrac(Lrun)   ! loop second index
+                    oca(iredired+icartsec,2)=oca(iredired+icartsec,2)
      *                +onecart(icartfirst,icartsec,mrun,Lrun,2)
-                      enddo
-cbs             shift pointer by number of functions in IR
-                      iredired=iredired+itotalperIR(iredsec)
-                      enddo
-        endif
-       if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.1.and.
-     * mod(ipowz,2).eq.0) then
-                      do icartfirst=1,ncontrac(Lrun) ! loop first index
-                      do icartsec=1,ncontrac(Lrun)   ! loop second index
-                      work(iocaz+iredired+(icartsec-1))=
-     *                work(iocaz+iredired+(icartsec-1))
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredsec)
+              enddo
+           endif
+           if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.1.and.
+     *         mod(ipowz,2).eq.0) then
+              do icartfirst=1,ncontrac(Lrun) ! loop first index
+                 do icartsec=1,ncontrac(Lrun)   ! loop second index
+                    oca(iredired+icartsec,3)=oca(iredired+icartsec,3)
      *                +onecart(icartfirst,icartsec,mrun,Lrun,3)
-                      enddo
-cbs             shift pointer by number of functions in IR
-                      iredired=iredired+itotalperIR(iredsec)
-                      enddo
-        endif
-              elseif (iredfirst.gt.iredsec) then
-cbs     In this case, indices are exchanged with respect to former
-cbs     symmetry of blocks. Therefore, there will be a minus sign
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredsec)
+              enddo
+           endif
+        elseif (iredfirst.gt.iredsec) then
+cbs        In this case, indices are exchanged with respect to former
+cbs        symmetry of blocks. Therefore, there will be a minus sign
 c
-cbs     calculate shift to get to the beginning of the block
-            iredired=shiftIRIR((iredfirst*iredfirst-iredfirst)/2+
-     *                iredsec)+
-     *                incrLM(Msec,Lrun)*itotalperIR(iredfirst)+
-     *                incrLM(Mfirst,Lrun)
-       if (mod(ipowx,2).eq.0.and.mod(ipowy,2).eq.1.and.
-     * mod(ipowz,2).eq.1) then
-                      do icartsec=1,ncontrac(Lrun) !loopsecond index
-                      do icartfirst=1,ncontrac(Lrun) !loop first index
-                      work(iocax+iredired+(icartfirst-1))=
-     *                work(iocax+iredired+(icartfirst-1))
+cbs        calculate shift to get to the beginning of the block
+           iredired=shiftIRIR((iredfirst*iredfirst-iredfirst)/2+iredsec)
+     *             + incrLM(Msec,Lrun)*itotalperIR(iredfirst)
+     *             + incrLM(Mfirst,Lrun)
+           if (mod(ipowx,2).eq.0.and.mod(ipowy,2).eq.1.and.
+     *         mod(ipowz,2).eq.1) then
+              do icartsec=1,ncontrac(Lrun) !loopsecond index
+                 do icartfirst=1,ncontrac(Lrun) !loop first index
+                    oca(iredired+icartfirst,1)=
+     *                oca(iredired+icartfirst,1)
      *               -onecart(icartsec,icartfirst,mrun,Lrun,1)
-                      enddo
-cbs             shift pointer by number of functions in IR
-                      iredired=iredired+itotalperIR(iredfirst)
-                      enddo
-        endif
-       if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.0.and.
-     * mod(ipowz,2).eq.1) then
-                      do icartsec=1,ncontrac(Lrun) !loop second index
-                      do icartfirst=1,ncontrac(Lrun) !loop first index
-                      work(iocay+iredired+(icartfirst-1))=
-     *                work(iocay+iredired+(icartfirst-1))
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredfirst)
+              enddo
+           endif
+           if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.0.and.
+     *         mod(ipowz,2).eq.1) then
+              do icartsec=1,ncontrac(Lrun) !loop second index
+                 do icartfirst=1,ncontrac(Lrun) !loop first index
+                    oca(iredired+icartfirst,2)=
+     *                oca(iredired+icartfirst,2)
      *               -onecart(icartsec,icartfirst,mrun,Lrun,2)
-                      enddo
-cbs             shift pointer by number of functions in IR
-                      iredired=iredired+itotalperIR(iredfirst)
-                      enddo
-        endif
-       if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.1.and.
-     * mod(ipowz,2).eq.0) then
-                      do icartsec=1,ncontrac(Lrun) !loop  second index
-                      do icartfirst=1,ncontrac(Lrun) !loop first index
-                      work(iocaz+iredired+(icartfirst-1))=
-     *                work(iocaz+iredired+(icartfirst-1))
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredfirst)
+              enddo
+           endif
+           if (mod(ipowx,2).eq.1.and.mod(ipowy,2).eq.1.and.
+     *         mod(ipowz,2).eq.0) then
+              do icartsec=1,ncontrac(Lrun) !loop  second index
+                 do icartfirst=1,ncontrac(Lrun) !loop first index
+                    oca(iredired+icartfirst,3)=
+     *                oca(iredired+icartfirst,3)
      *               -onecart(icartsec,icartfirst,mrun,Lrun,3)
-                      enddo
-                      iredired=iredired+itotalperIR(iredfirst)
-                      enddo
+                 enddo
+cbs              shift pointer by number of functions in IR
+                 iredired=iredired+itotalperIR(iredfirst)
+              enddo
+           endif
         endif
-      endif
       enddo
       enddo
       enddo
-C
 C
 cbs   copy integrals on arrays with no symmetry blocking at all
 cbs   which means huge triangular matrices
       irun=0
       do norb2=1,numballcarT
-      ired2=iredoffunctnew(norb2)
-      norbsh2=norb2-shiftIRED(ired2)
-      do norb1=1,norb2
-      ired1=iredoffunctnew(norb1)
-      norbsh1=noRb1-shiftIRED(ired1)
-      irun=irun+1
-      iredirEd=shiftIRIR((ired2*ired2-ired2)/2+
-     *                ired1)
-      if (ired1.ne.ired2) then
-        work(iocax2+irun-1)=work(iocax-1+iredired+norbsh2+
-     * (norbsH1-1)*itotalperIR(IREd2))
-        work(iocay2+irun-1)=work(iocay-1+iredired+norbsh2+
-     * (norbsH1-1)*itotalperIR(IREd2))
-        work(iocaz2+irun-1)=work(iocaz-1+iredired+norbsh2+
-     * (norbsH1-1)*itotalperIR(IREd2))
-      else
-       work(iocax2+irun-1)=work(iocax-1+iredired+norbsh2*
-     * (norbsH2-1)/2+norbsh1)
-       work(iocay2+irun-1)=work(iocay-1+iredired+norbsh2*
-     * (norbsH2-1)/2+norbsh1)
-       work(iocaz2+irun-1)=work(iocaz-1+iredired+norbsh2*
-     * (norbsH2-1)/2+norbsh1)
-      endif
-      Enddo
+         ired2=iredoffunctnew(norb2)
+         norbsh2=norb2-shiftIRED(ired2)
+         do norb1=1,norb2
+            ired1=iredoffunctnew(norb1)
+            norbsh1=noRb1-shiftIRED(ired1)
+            irun=irun+1
+            iredired=shiftIRIR((ired2*ired2-ired2)/2+ired1)
+            if (ired1.ne.ired2) then
+               oca2(irun,1)=
+     &            oca(iredired+norbsh2+(norbsH1-1)*itotalperIR(IREd2),1)
+               oca2(irun,2)=
+     &            oca(iredired+norbsh2+(norbsH1-1)*itotalperIR(IREd2),2)
+               oca2(irun,3)=
+     &            oca(iredired+norbsh2+(norbsH1-1)*itotalperIR(IREd2),3)
+            else
+               oca2(irun,1)=
+     &            oca(iredired+norbsh2*(norbsH2-1)/2+norbsh1,1)
+               oca2(irun,2)=
+     &            oca(iredired+norbsh2*(norbsH2-1)/2+norbsh1,2)
+               oca2(irun,3)=
+     &            oca(iredired+norbsh2*(norbsH2-1)/2+norbsh1,3)
+            endif
+         Enddo
       enddo
       if (.not.AIMP) then
 c     write a hermit-like file   b.s. 4.10.96
@@ -237,56 +234,59 @@ CBS    rewind LUPROP
      *        numballcart,(Loffunction(I),I=1,numballcart),
      *        (Moffunction(I),I=1,numballcart),
      *        Lhigh,(ncontrac(I),I=0,Lhigh)
-              write(LUPROP) (work(iocax2+irun),irun=0,length3-1)
+              write(LUPROP) (oca2(irun,1),irun=1,length3)
               write(LUPROP)  Ya
-              write(LUPROP) (work(iocay2+irun),irun=0,length3-1)
+              write(LUPROP) (oca2(irun,2),irun=1,length3)
               write(LUPROP)  Za
-              write(LUPROP) (work(iocaz2+irun),irun=0,length3-1)
+              write(LUPROP) (oca2(irun,3),irun=1,length3)
 CBS   close(luprop)
       else
 cbs   reorder for AIMP
 cbs   write(6,*) 'reorder integrals for AIMP'
       length3=ikeeporb*(ikeeporb+1)/2
-      Call GetMem('OCAX3','Allo','Real',iocax3,3*length3)
-      call dzero(work(iocax3),3*length3)
-      iocay3=iocax3+length3
-      iocaz3=iocay3+length3
+      Call mma_allocate(OCA3,length3,3,Label='OCA3')
+      OCA3(:,:)=0.0D0
 cbs   write(6,*) 'number of orbitals ',ikeeporb
 cbs   write(6,*) 'length of triangular matrix ', length3
       do irun2=1,ikeeporb
-      do irun1=1,irun2
-      ind2=ikeeplist(irun2)
-      ind1=ikeeplist(irun1)
-      ipntold=ipnt(ind1,ind2)-1
-      ipntnew=ipnt(irun1,irun2)-1
-      work(iocax3+ipntnew)=work(iocax2+ipntold)
-      work(iocay3+ipntnew)=work(iocay2+ipntold)
-      work(iocaz3+ipntnew)=work(iocaz2+ipntold)
-      enddo
+         do irun1=1,irun2
+            ind2=ikeeplist(irun2)
+            ind1=ikeeplist(irun1)
+            ipntold=ipnt(ind1,ind2)
+            ipntnew=ipnt(irun1,irun2)
+*
+            oca3(ipntnew,1)=oca2(ipntold,1)
+            oca3(ipntnew,2)=oca2(ipntold,2)
+            oca3(ipntnew,3)=oca2(ipntold,3)
+         enddo
       enddo
 CBS   write(6,*) 'transfered to new blocks'
-CBS    Luprop=19
-CBS           OPEN(LUPROP,STATUS='UNKNOWN',FORM='UNFORMATTED',
-CBS  *        FILE='AOPROPER_MF')
-CBS           rewind LUPROP
-              write(LUPROP)  iCenter
-              write(LUPROP)  xa,numbofsym,(nrtofiperIR(I),
-     *        i=1,numbofsym),
-     *        ikeeporb,(Loffunction(ikeeplist(i)),i=1,ikeeporb),
-     *        (Moffunction(ikeeplist(i)),I=1,ikeeporb),
-     *        Lhigh,((NContrac(I)-icore(I)),I=0,Lhigh)
-              write(LUPROP) (work(iocax3+irun),irun=0,length3-1)
-              write(LUPROP)  Ya
-              write(LUPROP) (work(iocay3+irun),irun=0,length3-1)
-              write(LUPROP)  Za
-              write(LUPROP) (work(iocaz3+irun),irun=0,length3-1)
-      Call GetMem('OCAX3','free','Real',iocax3,3*length3)
+CBS   Luprop=19
+CBS   OPEN(LUPROP,STATUS='UNKNOWN',FORM='UNFORMATTED',
+CBS  *     FILE='AOPROPER_MF')
+CBS   rewind LUPROP
+*
+      write(LUPROP)  iCenter
+      write(LUPROP)  xa,numbofsym,(nrtofiperIR(I),
+     *               i=1,numbofsym),
+     *               ikeeporb,(Loffunction(ikeeplist(i)),i=1,ikeeporb),
+     *               (Moffunction(ikeeplist(i)),I=1,ikeeporb),
+     *               Lhigh,((NContrac(I)-icore(I)),I=0,Lhigh)
+      write(LUPROP) (oca3(irun,1),irun=1,length3)
+      write(LUPROP)  Ya
+      write(LUPROP) (oca3(irun,2),irun=1,length3)
+      write(LUPROP)  Za
+      write(LUPROP) (oca3(irun,3),irun=1,length3)
+*
+      Call mma_deallocate(OCA3)
 CBS   close(luprop)
       endif
 cbs
 cbs   that is it!!
 cbs
-      Call GetMem('OCAX','free','Real',iocax,6*length3+MxContL*MxContL)
+      Call mma_deallocate(OCA2)
+      Call mma_deallocate(OCA)
+      Call mma_deallocate(Dummy)
       return
 c Avoid unused argument warnings
       if (.false.) call Unused_logical(makemean)
