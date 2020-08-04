96bddd6c16855b0e9b9e2a7785f310ece4f91554
Oskar Weser <oskar.weser@gmail.com>
2020-08-04 19:07:24 +0200

    Added unit tests

--- unit_tests/linalg_mod/test_linalg.F90
+++ unit_tests/linalg_mod/test_linalg.F90
@@ -1,19 +1,30 @@
 module test_linalg_mod
     use fruit
     use definitions, only: wp
+    use linalg_mod, only: mult
     implicit none
     private
-
-
+    public :: test_mult
 
 contains
 
-end module test_gasci_mod
+    subroutine test_mult()
+        real(wp) :: A(2, 2), B(2, 2), C(2, 2)
 
-program test_gasci_program
+        call random_number(A)
+        call random_number(B)
 
-    use mpi
+        call mult(A, B, C)
+        call assert_equals(C, matmul(A, B), 2, 2)
+
+    end subroutine
+
+end module test_linalg_mod
+
+program test_linalg
+
     use fruit
+    use test_linalg_mod, only: test_mult
 
     implicit none
     integer :: failed_count, err
@@ -29,11 +40,12 @@ program test_gasci_program
         call fruit_finalize()
         call get_failed_count(failed_count)
 
-        if (failed_count /= 0) call stop_all('test_linalg_program', 'failed_tests')
+        if (failed_count /= 0)  error stop
     end block
 
 contains
 
     subroutine test_linalg_driver()
+        call run_test_case(test_mult, "test_mult")
     end subroutine
-end program test_linalg_mod
+end program test_linalg
