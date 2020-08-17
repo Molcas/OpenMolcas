a6bf9d491deb2536077f5b47d978e64cace16fe1
Oskar Weser <oskar.weser@gmail.com>
2020-08-17 14:57:10 +0200

    Removed debug writes

--- unit_tests/linalg_mod/test_linalg.F90
+++ unit_tests/linalg_mod/test_linalg.F90
@@ -132,30 +132,8 @@ contains
             call canonicalize(V, lambdas)
 
             do i_test = 1, test_size
-
-! TODO(@Oskar): Delete the following
-!                 write(*, *) '===== V'
-!                 do i = 1, size(V, 1)
-!                     write(*, *) V(i, :)
-!                 end do
-!                 write(*, *) '====='
-!
                 call create_test_V(V, dimension_E, test_V)
-!
-!                 write(*, *) '===== test_V'
-!                 do i = 1, size(test_V, 1)
-!                     write(*, *) test_V(i, :)
-!                 end do
-!                 write(*, *) '====='
-!
                 call canonicalize(test_V, lambdas)
-!
-!                 write(*, *) '===== transformed test_V'
-!                 do i = 1, size(test_V, 1)
-!                     write(*, *) test_V(i, :)
-!                 end do
-!                 write(*, *) '====='
-
                 call assert_true(all(test_V .isclose. V))
             end do
         end block test_canonical_basis
@@ -176,30 +154,8 @@ contains
             call canonicalize(V, lambdas, ref)
 
             do i_test = 1, test_size
-
-! TODO(@Oskar): Delete the following
-!                 write(*, *) '====='
-!                 do i = 1, size(V, 1)
-!                     write(*, *) V(i, :)
-!                 end do
-!                 write(*, *) '====='
-!
                 call create_test_V(V, dimension_E, test_V)
-!
-!                 write(*, *) '===== test_V'
-!                 do i = 1, size(test_V, 1)
-!                     write(*, *) test_V(i, :)
-!                 end do
-!                 write(*, *) '====='
-!
                 call canonicalize(test_V, lambdas, ref)
-!
-!                 write(*, *) '===== transformed test_V'
-!                 do i = 1, size(test_V, 1)
-!                     write(*, *) test_V(i, :)
-!                 end do
-!                 write(*, *) '====='
-
                 call assert_true(all(test_V .isclose. V))
             end do
         end block test_general_basis
