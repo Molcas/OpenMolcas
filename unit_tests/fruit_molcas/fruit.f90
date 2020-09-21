fbe5df6a8e756b7a3ccd696a88f38d714ba7dac3
Oskar Weser <oskar.weser@gmail.com>
2020-09-21 10:36:24 +0200

    Removed redundant licensing information

--- unit_tests/fruit_molcas/fruit.f90
+++ unit_tests/fruit_molcas/fruit.f90
@@ -7,10 +7,6 @@
 !***********************************************************************
 
 
-! Copyright (c) 2005-2010, 2012-2013, Andrew Hang Chen and contributors,
-! All rights reserved.
-! Licensed under the 3-clause BSD license.
-
 !------------------------
 ! FORTRAN unit test utility
 !
