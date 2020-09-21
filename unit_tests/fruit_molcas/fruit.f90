215a2773d84ba25cc17beda3b456861ff1cdbe50
Oskar Weser <oskar.weser@gmail.com>
2020-09-21 10:28:17 +0200

    Added headers to fruit files

--- unit_tests/fruit_molcas/fruit.f90
+++ unit_tests/fruit_molcas/fruit.f90
@@ -1,3 +1,11 @@
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! fruit.f90 is licensed under the 3-clause BSD license.                *
+!                                                                      *
+! Copyright (C) 2005-2010,2012-2013, Andrew Hang Chen and contributors.*
+!***********************************************************************
+
 
 ! Copyright (c) 2005-2010, 2012-2013, Andrew Hang Chen and contributors,
 ! All rights reserved.
