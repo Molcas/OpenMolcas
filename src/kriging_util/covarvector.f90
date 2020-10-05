e3205844813f02f58cbc76be050196ecd168a602
roland <roland.lindh@kemi.uu.se>
2020-10-05 07:38:45 +0200

    More generalizations.

--- src/kriging_util/covarvector.f90
+++ src/kriging_util/covarvector.f90
@@ -21,39 +21,39 @@ SUBROUTINE covarVector(gh)
   nPoints=nPoints_save
   nInter =nInter_save
 
-  Call mma_Allocate(diffx,nPoints,npx,label="diffx")
-  Call mma_Allocate(diffx0,nPoints,npx,label="diffx0")
-  Call mma_Allocate(diffxk,nPoints,npx,label="diffxk")
+  Call mma_Allocate(diffx,nPoints_v,npx,label="diffx")
+  Call mma_Allocate(diffx0,nPoints_v,npx,label="diffx0")
+  Call mma_Allocate(diffxk,nPoints_v,npx,label="diffxk")
 !
   cv = 0
   i0 = 0
-  call defdlrl(nPoints,nInter)
+  call defdlrl()
 !
 ! Covariant Vector in kriging - First part of eq (4) in ref.
 !
   if (gh.eq.0) then
 !
-    call matern(dl, cv(1:nPoints,:,1,1), nPoints, npx)
-    call matderiv(1, dl, cvMatFDer, nPoints, npx)
+    call matern(dl, cv(1:nPoints_v,:,1,1), nPoints_v, npx)
+    call matderiv(1, dl, cvMatFDer, nPoints_v, npx)
     do i=1,nInter
 !     1st derivatives second part of eq. (4)
       diffx(:,:) = 2.0D0*rl(:,:,i)/l(i)
-      i0 = i*nPoints + 1
-      i1 = i0 + nPoints - 1
+      i0 = nPoints_v + 1 + (i-1)*nPoints_g
+      i1 = i0 + nPoints_g - 1
       cv(i0:i1,:,1,1) = cvMatFder * diffx
     enddo
 ! Covariant vector in Gradient Enhanced Kriging
 !
   else if(gh.eq.1) then
 !
-    call matderiv(1, dl, cvMatFder, nPoints, npx)
-    call matderiv(2, dl, cvMatSder, nPoints, npx)
+    call matderiv(1, dl, cvMatFder, nPoints_v, npx)
+    call matderiv(2, dl, cvMatSder, nPoints_v, npx)
     do i=1,nInter
       diffx(:,:) = 2.0D0*rl(:,:,i)/l(i)
-      cv(1:nPoints,:,i,1) = -cvMatFder * diffx
+      cv(1:nPoints_v,:,i,1) = -cvMatFder * diffx
       do j = 1,nInter
-        j0 = j*nPoints + 1
-        j1 = j0+nPoints - 1
+        j0 = nPoints_v + 1 + (j-1)*nPoints_g
+        j1 = j0 + nPoints_g - 1
         diffx0(:,:) = -2.0D0*rl(:,:,j)/l(j)
         if (i.eq.j) Then
          cv(j0:j1,:,i,1) = cvMatSder * diffx*diffx0 - cvMatFder*(2/(l(i)*l(j)))
@@ -66,9 +66,9 @@ SUBROUTINE covarVector(gh)
   else if(gh.eq.2) then
 !
       !    print *,'covar vector calling deriv(3) for Kriging Hessian'
-    call matderiv(1, dl, cvMatFder, nPoints, npx)
-    call matderiv(2, dl, cvMatSder, nPoints, npx)
-    call matderiv(3, dl, cvMatTder, nPoints, npx)
+    call matderiv(1, dl, cvMatFder, nPoints_v, npx)
+    call matderiv(2, dl, cvMatSder, nPoints_v, npx)
+    call matderiv(3, dl, cvMatTder, nPoints_v, npx)
     do i = 1, nInter
       diffx(:,:) = 2.0D0*rl(:,:,i)/l(i)
       sdiffx = 2.0D0/l(i)**2
@@ -83,8 +83,8 @@ SUBROUTINE covarVector(gh)
         do k = 1, nInter
           diffxk(:,:) = 2.0D0*rl(:,:,k)/l(k)
           sdiffxk = 2.0D0/l(k)**2
-          k0 = k*nPoints + 1
-          k1 = k0+nPoints - 1
+          k0 = nPoints_v + 1 + (k-1)*nPoints_g
+          k1 = k0 + nPoints_g - 1
           if (i.eq.j.and.j.eq.k) then
             cv(k0:k1,:,i,j) = cvMatTder*diffx*diffx0*diffxk + 3.0D0*cvMatSder*diffx*sdiffx0
           else if (i.eq.j) then
@@ -110,12 +110,15 @@ SUBROUTINE covarVector(gh)
 !
 contains
 !
-SUBROUTINE defdlrl(nPoints,nInter)
+SUBROUTINE defdlrl()
   use kriging_mod
-  integer i,j,nPoints,nInter
+  integer i,j,nInter
+
+  nInter=nInter_save
+
   dl=0
   do i=1,nInter
-    do j=1,nPoints
+    do j=1,nPoints_v
       do k=1,int(npx)
         rl(j,k,i) = (x(i,j) - nx(i,k))/l(i)
       enddo
