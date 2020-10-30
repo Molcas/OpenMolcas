89505b6bb178529d50cafa2531ae6f68a9f0b898
roland <roland.lindh@kemi.uu.se>
2020-10-30 14:15:40 +0100

    Updates.

--- src/mclr/rdciv.f
+++ src/mclr/rdciv.f
@@ -23,40 +23,44 @@
 ************************************************************************
       use negpre
       Implicit Real*8 (a-h,o-z)
-
 #include "Input.fh"
 #include "Pointers.fh"
-#include "WrkSpc.fh"
+#include "stdalloc.fh"
 #include "Files_mclr.fh"
+      Real*8, Allocatable:: OCIvec(:), Tmp(:)
       Call DaName(LuCIV,'ROOTS')
       iDisk=0
 *----------------------------------------------------------------------*
 *     Load the CI vector for all roots                            *
 *----------------------------------------------------------------------*
+
+      Call mma_Allocate(OCIvec,nConf,Label='OCIvec')
+      ipTmp = ip_of_Work(OCIvec)
+
       iDisk=iToc(4)
       idisk1=0
       Do i=1,lroots
-       Call GetMem('OCIvec','Allo','Real',ipTmp,nConf)
-       Call dDaFile(LuJob,2,Work(ipTmp),nConf,iDisk)
+       Call dDaFile(LuJob,2,OCIvec,nConf,iDisk)
        Call Gugactl_MCLR(ipTmp,1)
-       Call dDafile(LuCIV,1,Work(ipTmp),nconf,iDisk1)
-       Call GetMem('OCIVEC','Free','Real',ipTmp,nConf)
+       Call dDafile(LuCIV,1,OCIvec,nconf,iDisk1)
       End Do
+
+      Call mma_deAllocate(OCIvec)
 *----------------------------------------------------------------------*
 *     Load state energy                                                *
 *----------------------------------------------------------------------*
 
-      Call GetMem('Temp2','Allo','Real',ipTmp,mxRoot*mxIter)
+      Call mma_allocate(Tmp,mxRoot*mxIter,Label='Tmp')
       iDisk=iToc(6)
-      Call dDaFile(LuJob,2,Work(ipTmp),mxRoot*mxIter,iDisk)
+      Call dDaFile(LuJob,2,Tmp,mxRoot*mxIter,iDisk)
       Do i=0,lroots-1
       ERAS(i+1)=0.0d0
-      Do  iter=0,mxIter-1
-         Temp=Work(ipTmp+iter*mxRoot+i)
+      Do  iter=1,mxIter
+         Temp=Tmp(iter*mxRoot+i)
          If ( Temp.ne.0.0D0 ) ERAS(i+1)=Temp
       End Do
       End Do
-      Call GetMem('Temp2','Free','Real',ipTmp,mxRoot*mxiter)
+      Call mma_deallocate(Tmp)
 *----------------------------------------------------------------------*
 *     exit                                                             *
 *----------------------------------------------------------------------*
