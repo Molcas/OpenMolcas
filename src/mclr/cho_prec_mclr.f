5f4e3fd599eb77db828d7782d0559697cc3efd16
roland <roland.lindh@kemi.uu.se>
2020-11-06 11:28:37 +0100

    More update.

--- src/mclr/cho_prec_mclr.f
+++ src/mclr/cho_prec_mclr.f
@@ -243,7 +243,7 @@
            ntp=0      ! do not allocate those
            labatch=0
            iptpuq=-1
-           write(6,*) 'Batching loop i'
+*          write(6,*) 'Batching loop i'
         Else
 *
 **        Batching T loop
