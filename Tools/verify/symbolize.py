8d587ade4f8d31bcf6f70b6491a8c0dad7200502
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2020-11-22 15:26:01 +0100

    Convert scripts from Perl to Python
    And remove those that were only useful for the old molcas repo.

--- Tools/verify/symbolize.py
+++ Tools/verify/symbolize.py
@@ -1,4 +1,6 @@
-#!/usr/bin/perl
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 #***********************************************************************
 # This file is part of OpenMolcas.                                     *
 #                                                                      *
@@ -13,17 +15,28 @@
 # script to convert stack trace with pc addresses
 # to source code files and line numbers
 
-print "symbolized stack trace:\n";
-while (<STDIN>) {
-    if (/^(\s+#\d+)\s+0x[0-9a-f]+\s+\(([^\(\)]*)\)/) {
-        my $counter = $1;
-        my $address = $2;
-        my ($exe,$pc) = split(/\+/, $address);
-        my $source = `addr2line -f -p -e $exe $pc`;
-        if ($source =~ /\?\?:\?/) {
-            print "$counter (?) $exe $pc\n";
-        } else {
-            print "$counter $source";
-        }
-    }
-}
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+import sys
+import re
+import subprocess
+
+print('symbolized stack trace:')
+
+for line in sys.stdin:
+  match = re.match('(\s+#\d+)\s+0x[0-9a-f]+\s+\(([^\(\)]*)\)', line)
+  if match:
+    counter = match.group(1)
+    address = match.group(2)
+    exe, pc = address.split('+')
+    source = subprocess.check_output(['addr2line', '-f', '-p', '-e', exe, pc])
+    if re.search('\?\?:\?', source):
+      print('{} (?) {} {}'.format(counter, exe, pc))
+    else:
+      print('{} {}'.format(counter, source))
+  else:
+    match = re.match('(\s+#\d+)\s+0x[0-9a-f]+\s+(in\s+.*)', line)
+    if match:
+      counter = match.group(1)
+      address = match.group(2)
+      print('{} {}'.format(counter, address))
