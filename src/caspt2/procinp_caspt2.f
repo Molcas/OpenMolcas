0f2305c0de04f525f48b40f8b67da6ccdef7ea79
Stefano Battaglia <stefano.battaglia@kemi.uu.se>
2021-02-04 15:49:13 +0100

    adapted to F90 style

--- src/caspt2/procinp_caspt2.f
+++ src/caspt2/procinp_caspt2.f
@@ -1,57 +1,55 @@
-************************************************************************
-* This file is part of OpenMolcas.                                     *
-*                                                                      *
-* OpenMolcas is free software; you can redistribute it and/or modify   *
-* it under the terms of the GNU Lesser General Public License, v. 2.1. *
-* OpenMolcas is distributed in the hope that it will be useful, but it *
-* is provided "as is" and without any express or implied warranties.   *
-* For more details see the full text of the license in the file        *
-* LICENSE or in <http://www.gnu.org/licenses/>.                        *
-************************************************************************
-      Subroutine ProcInp_Caspt2
-CSVC: process CASPT2 input based on the data in the input table, and
-C initialize global common-block variables appropriately.
-      Use InputData
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!***********************************************************************
+      subroutine ProcInp_Caspt2
+!SVC: process CASPT2 input based on the data in the input table, and
+! initialize global common-block variables appropriately.
+      use InputData
+      use definitions, only:iwp
       use output, only:silent,terse,usual,verbose,debug,insane,iPrGlb
       use output, only:cmpThr,cntThr,dnmThr
 #ifdef _MOLCAS_MPP_
       Use Para_Info, Only: Is_Real_Par
 #endif
-* NOT TESTED
+! NOT TESTED
 #if 0
       use OFembed, only: Do_OFemb
 #endif
-      Implicit None
+      implicit none
 #include "rasdim.fh"
-! #include "warnings.fh"
 #include "caspt2.fh"
 #include "pt2_guga.fh"
 #include "WrkSpc.fh"
 #include "stdalloc.fh"
 #include "SysDef.fh"
-
-      Integer iDummy
-
-* Number of non-valence orbitals per symmetry
-      Integer nCore(mxSym)
-      Integer nDiff, NFI, NSD
-* Geometry-determining root
-      Logical Is_iRlxRoot_Set
-* Environment
-      Character(Len=180) Env
-
-      Integer I, J, M, N
-      Integer iSym
-* State selection
-      Integer iGroup, IOFF
-
 #include "chocaspt2.fh"
 
+      Integer(kind=iwp) :: iDummy
 
-* Hzero and Focktype are merged together into Hzero. We keep the
-* variable Focktype not to break the input keyword which is documented
-* in the manual. However, eventually we will have to keep only Hzero
-* and remove Focktype.
+! Number of non-valence orbitals per symmetry
+      Integer(kind=iwp) :: nCore(mxSym)
+      Integer(kind=iwp) :: nDiff, NFI, NSD
+! Geometry-determining root
+      Logical(kind=iwp) :: Is_iRlxRoot_Set
+! Environment
+      Character(Len=180) Env
+
+      Integer(kind=iwp) :: I, J, M, N
+      Integer(kind=iwp) :: iSym
+! State selection
+      Integer(kind=iwp) :: iGroup, iOff
+
+! Hzero and Focktype are merged together into Hzero. We keep the
+! variable Focktype not to break the input keyword which is documented
+! in the manual. However, eventually we will have to keep only Hzero
+! and remove Focktype.
       Hzero = input%Hzero
       if (Hzero.ne.'STANDARD'.and.Hzero.ne.'CUSTOM') then
         call WarningMessage(2,
@@ -59,7 +57,7 @@ C initialize global common-block variables appropriately.
         call Quit_OnUserError
       end if
 
-* Choose Focktype, reset IPEA shift to 0 for non-standard fock matrices
+! Choose Focktype, reset IPEA shift to 0 for non-standard fock matrices
       Focktype = input%Focktype
       if (Focktype.ne.'STANDARD') then
         if (IfChol) then
@@ -68,13 +66,13 @@ C initialize global common-block variables appropriately.
           WRITE(6,*)'be used with the standard FOCKtype!'
           Call Quit_OnUserError
         end if
-* If both Hzero and Focktype are not standard, quit
+! If both Hzero and Focktype are not standard, quit
         if (Hzero.ne.'STANDARD') then
           Call WarningMessage(2,'Requested combination of FOCKtype'//
      &                          ' and HZERo not possible.')
           Call Quit_OnUserError
         end if
-* IPEA different from zero only for standard Focktype
+! IPEA different from zero only for standard Focktype
         if (BSHIFT.gt.0.0d0.or.BSHIFT.lt.0.0d0) then
           BSHIFT = 0.0d0
           if (IPRGLB.ge.TERSE) then
@@ -82,11 +80,11 @@ C initialize global common-block variables appropriately.
           end if
         end if
       else
-* user-specified IPEA shift or not?
+! user-specified IPEA shift or not?
         if (input%IPEA) then
           BSHIFT = input%BSHIFT
         else
-* Set default IPEA to 0.25 Eh or 0.0
+! Set default IPEA to 0.25 Eh or 0.0
           call getenvf('MOLCAS_NEW_DEFAULTS', Env)
           call upcase(Env)
           if (Env.eq.'YES') then
@@ -97,44 +95,44 @@ C initialize global common-block variables appropriately.
         end if
       end if
 
-* Copy over to Hzero the content of Focktype, if Hzero is not CUSTOM
+! Copy over to Hzero the content of Focktype, if Hzero is not CUSTOM
       if (Hzero.ne.'CUSTOM') then
         Hzero = Focktype
       end if
 
-* print warnings if deviating from the default
+! print warnings if deviating from the default
       if (Hzero.ne.'STANDARD') then
         call warningmessage(1,'User-modified 0th-order Hamiltonian!')
       end if
 
-* real/imaginary shifts
+! real/imaginary shifts
       SHIFT = Input % Shift
       SHIFTI = Input % ShiftI
 
-* RHS algorithm selection
+! RHS algorithm selection
 #ifdef _MOLCAS_MPP_
 #  ifdef _GA_
-C     The RHS on-demand algorithm doesn't handle serial calculations
-C     because it's not adapted for use with regular Work arrays, only
-C     global arrays, and needs to be switched off (using rhsall instead)
+!     The RHS on-demand algorithm doesn't handle serial calculations
+!     because it's not adapted for use with regular Work arrays, only
+!     global arrays, and needs to be switched off (using rhsall instead)
       RHSDIRECT=(Is_Real_Par().AND.Input%RHSD)
 #  else
-C     Without the Global Arrays library, we can't use the RHSALL2
-C     and ADDRHS algorithms in parallel. Here we force the use of
-C     RHS on-demand instead, depending on if the calculation is
-C     really parallel or not.
+!     Without the Global Arrays library, we can't use the RHSALL2
+!     and ADDRHS algorithms in parallel. Here we force the use of
+!     RHS on-demand instead, depending on if the calculation is
+!     really parallel or not.
       RHSDIRECT=Is_Real_Par()
 #  endif
 #else
       RHSDIRECT=.False.
 #endif
 
-* Cholesky: set defaults if it was not called during input
+! Cholesky: set defaults if it was not called during input
       If (.NOT.(Input % ChoI .or. Input % Chol)) Then
         Call Cho_caspt2_rdInp(.True.,iDummy)
       End If
 
-*---  Initialize
+!---  Initialize
       IDCIEX=0
       IEOF1M=0
       DO I=1,64
@@ -155,53 +153,53 @@ C     really parallel or not.
 
       DoCumulant = Input % DoCumulant
 
-************************************************************************
-*
-* Root selection
-*
-************************************************************************
-*SB: create a flat array, putting (X)MS states with increasing
-* group number. Do not allow to run both MS and XMS in the same
-* calculation, it can lead to catastrophic results.
-* For MS, put one state per group, for XMS put all states in
-* a single group.
-* Example: lets say the input is:
-* MULTistate = 3 3 4 5
-*
-* Then the arrays that are created will be:
-* nstate = 3
-* mstate(nstate): 3 4 5
-* nGroup = 3
-* nGroupState(nGroup): 1 1 1
-*
-* On the other hand, if the input is
-* XMULtistate = 4 1 2 3 4
-*
-* Then the arrays that are created will be:
-* nstate = 4
-* mstate(nstate): 1 2 3 4
-* nGroup = 1
-* nGroupState(nGroup): 4
+!***********************************************************************
+!
+! Root selection
+!
+!***********************************************************************
+!SB: create a flat array, putting (X)MS states with increasing
+! group number. Do not allow to run both MS and XMS in the same
+! calculation, it can lead to catastrophic results.
+! For MS, put one state per group, for XMS put all states in
+! a single group.
+! Example: lets say the input is:
+! MULTistate = 3 3 4 5
+!
+! Then the arrays that are created will be:
+! nstate = 3
+! mstate(nstate): 3 4 5
+! nGroup = 3
+! nGroupState(nGroup): 1 1 1
+!
+! On the other hand, if the input is
+! XMULtistate = 4 1 2 3 4
+!
+! Then the arrays that are created will be:
+! nstate = 4
+! mstate(nstate): 1 2 3 4
+! nGroup = 1
+! nGroupState(nGroup): 4
       NSTATE = 0
       MSTATE = 0
       NGROUP = 0
       NGROUPSTATE = 0
-* This is the case for MS-CASPT2 and DW-CASPT2
+! This is the case for MS-CASPT2 and DW-CASPT2
       If (Input%MULT) Then
         If (Input%XMUL) Then
           Call WarningMessage(2,'Keyword MULTistate cannot be used '//
      &                          'together with keyword XMULtistate.')
           Call Quit_OnUserError
         End If
-* Either the states were specified manually or the keyword "all"
-* was used, so first we check the keyword all
+! Either the states were specified manually or the keyword "all"
+! was used, so first we check the keyword all
         If (Input%AllMult) Then
           NSTATE = NROOTS
           MSTATE = IROOT
           NGROUP = NSTATE
           NGROUPSTATE(1:NGROUP)=1
         Else
-* Save the states that need to be computed
+! Save the states that need to be computed
           Do I=1,Input%nMultState
             MSTATE(I) = Input%MultGroup%State(I)
             NSTATE = NSTATE + 1
@@ -211,17 +209,17 @@ C     really parallel or not.
         End If
       End If
       IOFF=NSTATE
-* This is the case for XMS-CASPT2 and XDW-CASPT2
+! This is the case for XMS-CASPT2 and XDW-CASPT2
       if (Input%XMUL) then
         if (Input%MULT) then
           call WarningMessage(2,'Keyword XMULtistate cannot be used '//
      &                          'together with keyword MULTistate.')
           call Quit_OnUserError
         end if
-* This is a XDW-CASPT2 calculation. It is actually more similar to
-* a MS-CASPT2 one since we need to put one state per group and thus
-* have as many groups as states. Nevertheless, it makes more sense
-* from the user point of view to ask for it through XMUL and DWMS
+! This is a XDW-CASPT2 calculation. It is actually more similar to
+! a MS-CASPT2 one since we need to put one state per group and thus
+! have as many groups as states. Nevertheless, it makes more sense
+! from the user point of view to ask for it through XMUL and DWMS
         if (Input%DWMS) then
           if (Input%AllXMult) Then
             NSTATE = NROOTS
@@ -236,7 +234,7 @@ C     really parallel or not.
             NGROUP = Input%nXMulState
             NGROUPSTATE(1:NGROUP) = 1
           end if
-* This is a XMS-CASPT2: one group with all the states
+! This is a XMS-CASPT2: one group with all the states
         else
           if (Input%AllXMult) Then
             NSTATE = NROOTS
@@ -253,8 +251,8 @@ C     really parallel or not.
           end if
         end if
       End If
-* After parsing mult or xmult, check that no two equal states where
-* given in the input
+! After parsing mult or xmult, check that no two equal states where
+! given in the input
       If (Input%MULT.OR.Input%XMUL) Then
         Do I=1,NSTATE
           Do J=I+1,NSTATE
@@ -266,8 +264,8 @@ C     really parallel or not.
           End Do
         End Do
       End If
-* The LROOt keyword specifies a single root to be used. It should not be
-* used together with either MULT or XMUL keywords.
+! The LROOt keyword specifies a single root to be used. It should not be
+! used together with either MULT or XMUL keywords.
       If (Input%LROO) Then
         If (Input%MULT.OR.Input%XMUL) Then
           Call WarningMessage(2,'Keyword LROO cannot be used together'//
@@ -279,15 +277,15 @@ C     really parallel or not.
         NGROUP=1
         NGROUPSTATE(1)=1
       End If
-* If still nothing was selected we should default to compute all the
-* roots that were part of the rasscf orbital optimization.
+! If still nothing was selected we should default to compute all the
+! roots that were part of the rasscf orbital optimization.
       IF (NSTATE.EQ.0) THEN
         NSTATE=NROOTS
         MSTATE=IROOT
         NGROUP=NSTATE
         NGROUPSTATE(1:NGROUP)=1
       END IF
-* Find the group number for OnlyRoot
+! Find the group number for OnlyRoot
       If (NLYROOT.ne.0) Then
         IOFF=0
         Do IGROUP=1,NGROUP
@@ -297,21 +295,21 @@ C     really parallel or not.
           IOFF=IOFF+NGROUPSTATE(IGROUP)
         End Do
       End If
-* Finally, some sanity checks.
+! Finally, some sanity checks.
       IF(NSTATE.LE.0.OR.NSTATE.GT.MXROOT) Then
         Call WarningMessage(2,'Number of states is <0 or too large.')
         WRITE(6,'(a,i8)')' NSTATE = ',NSTATE
         WRITE(6,*)' Check usage of keywords MULT/XMUL.'
         Call Quit_OnUserError
       END IF
-* setup root to state translation
+! setup root to state translation
       ROOT2STATE = 0
       DO I=1,NSTATE
         ROOT2STATE(MSTATE(I))=I
       END DO
-*
-* Relax root selection
-*
+!
+! Relax root selection
+!
       iRlxRoot=-1
       Call Qpg_iScalar('NumGradRoot',Is_iRlxRoot_Set)
       If (Is_iRlxRoot_Set) Then
@@ -327,14 +325,14 @@ C     really parallel or not.
         iRlxRoot=NSTATE
       End If
 
-************************************************************************
-*
-* Determine number of Frozen/Deleted orbitals
-*
-************************************************************************
-* The number of Frozen orbitals is initially read from the reference
-* wavefunction. Here, we modify that number to be the larger of what
-* was in the reference and the non-valence orbitals.
+!***********************************************************************
+!
+! Determine number of Frozen/Deleted orbitals
+!
+!***********************************************************************
+! The number of Frozen orbitals is initially read from the reference
+! wavefunction. Here, we modify that number to be the larger of what
+! was in the reference and the non-valence orbitals.
       Call Get_iArray('Non valence orbitals',nCore,nSym)
       Do iSym = 1, nSym
         If (nCore(iSym).gt.nFro(iSym)) Then
@@ -344,9 +342,9 @@ C     really parallel or not.
           nISh(iSym)=nISh(iSym)-nDiff
         End If
       End Do
-* If a user specified the number of frozen orbitals explicitely, then
-* that number overwrites the automatically chosen numbers, but it we
-* warn the user and report what the computed number was.
+! If a user specified the number of frozen orbitals explicitely, then
+! that number overwrites the automatically chosen numbers, but it we
+! warn the user and report what the computed number was.
       IF(Input % FROZ) THEN
         IF(IPRGLB.GE.TERSE) THEN
           Call WarningMessage(1,'User changed nr of frozen orbitals.')
@@ -362,7 +360,7 @@ C     really parallel or not.
           NISH(I)=NFI-nFro(I)
         END DO
       ENDIF
-* Set user-specified number of deleted orbitals.
+! Set user-specified number of deleted orbitals.
       IF(Input % DELE) THEN
         DO I=1,NSYM
           NSD=NSSH(I)+NDEL(I)
@@ -376,13 +374,13 @@ C     really parallel or not.
         ENDDO
       ENDIF
 
-* NOT TESTED
+! NOT TESTED
 #if 0
-************************************************************************
-*
-* Orbital-free embedding.
-*
-************************************************************************
+!***********************************************************************
+!
+! Orbital-free embedding.
+!
+!***********************************************************************
       If (Input % OFEmbedding) Then
         Do_OFemb=.true.
         write(6,*)
@@ -392,12 +390,12 @@ C     really parallel or not.
         write(6,*)
       End If
 #endif
-************************************************************************
-*
-* Determine what kind of calculations are needed/requested.
-*
-************************************************************************
-* flags for enabling/disabling sections
+!***********************************************************************
+!
+! Determine what kind of calculations are needed/requested.
+!
+!***********************************************************************
+! flags for enabling/disabling sections
       IFPROP = Input % Properties
       IFDENS = Input % DENS
       IFMIX  = .NOT.Input % NoMix
@@ -405,7 +403,7 @@ C     really parallel or not.
      &           .AND.(.NOT.Input % NoMult)
       IFXMS = Input % XMUL
       IFDW = Input % DWMS
-* Set exponent for DWMS
+! Set exponent for DWMS
       if (IFDW) then
         zeta = Input % zeta
       end if
@@ -420,28 +418,28 @@ C     really parallel or not.
         end if
       end if
 
-* Choice? of preprocessing route
+! Choice? of preprocessing route
       ORBIN='TRANSFOR'
 
-*---  Expectation values of H0 needed or not? Spectral resolution?
+!---  Expectation values of H0 needed or not? Spectral resolution?
       BMATRIX='YES     '
       BTRANS ='YES     '
       BSPECT ='YES     '
-*---  Overlap matrix needed? Linear dependence removal needed?
+!---  Overlap matrix needed? Linear dependence removal needed?
       SMATRIX='YES     '
       SDECOM ='YES     '
-C Presently, we always need the ON transformation/Lindep removal.
-C Consistency of these demands:
+! Presently, we always need the ON transformation/Lindep removal.
+! Consistency of these demands:
       IF(BSPECT.NE.'NO      ') BTRANS ='YES     '
       IF(BTRANS.NE.'NO      ') BMATRIX='YES     '
       IF(BTRANS.NE.'NO      ') SDECOM ='YES     '
       IF(SDECOM.NE.'NO      ') SMATRIX='YES     '
 
-************************************************************************
-*
-* Thresholds
-*
-************************************************************************
+!***********************************************************************
+!
+! Thresholds
+!
+!***********************************************************************
       If (Input%THRE) Then
         If (IPRGLB.GE.TERSE) THEN
           Call WarningMessage(1,
@@ -459,7 +457,7 @@ C Consistency of these demands:
       CMPTHR=Input%CMPTHR
       CNTTHR=Input%CNTTHR
 
-*---  Create the symmetry multiplication table
+!---  Create the symmetry multiplication table
       MUL(1,1)=1
       M=1
       DO N=1,3
@@ -472,7 +470,7 @@ C Consistency of these demands:
         END DO
         M=2*M
       END DO
-*
-*---  Exit
+!
+!---  Exit
       Return
       End
