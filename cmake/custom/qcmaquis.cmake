e8e957285a0bfda4490de99d23518cacea7f1148
Leon Freitag <lefreita@ethz.ch>
2021-02-26 15:49:55 +0100

    Copy QCMaquis interface Fortran modules to the main module dir
    - simplifies module handling

--- cmake/custom/qcmaquis.cmake
+++ cmake/custom/qcmaquis.cmake
@@ -47,7 +47,7 @@ set(OPENMOLCAS_TOOLS_DIR ${CMAKE_BINARY_DIR}/Tools/qcmaquis)
 if(SINGLE_MOD_DIR)
   set(mod_dir ${MAIN_MOD_DIR}/_single)
 else()
-  set(mod_dir ${MAIN_MOD_DIR}/qcmaquis)
+  set(mod_dir ${MAIN_MOD_DIR}) # making a subdirectory for qcmaquis dirs doesn't work
 endif()
 set (CMAKE_DISABLE_SOURCE_CHANGES ON)
 
