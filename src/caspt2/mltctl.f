36381f66e95d4040a344112f9fcb61179d2d5aaf
Stefano Battaglia <battaglia1803@gmail.com>
2021-04-22 14:37:11 +0200

    More compact if-block for CASPT2 variant printout

--- src/caspt2/mltctl.f
+++ src/caspt2/mltctl.f
@@ -27,6 +27,7 @@
       INTEGER  Cho_X_GetTol
       EXTERNAL Cho_X_GetTol
       CHARACTER(LEN=8) INLAB
+      character(len=3) variant
       DIMENSION HEFF(NSTATE,NSTATE),EIGVEC(NSTATE,NSTATE)
       real(8) U0(Nstate,Nstate)
       real(8),allocatable :: Utmp(:,:)
@@ -115,41 +116,23 @@ C Use a symmetrized matrix, in triangular storage:
 
       IF(IPRGLB.GE.TERSE) THEN
         If (IFRMS) Then
-          WRITE(6,*)
-          WRITE(6,'(6X,A)')' Total RMS-CASPT2 energies:'
-          DO I=1,NSTATE
-            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',
-     &      'RMS-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
-          END DO
+          variant = 'RMS'
         Else if (IFXMS.and.IFDW) then
-          WRITE(6,*)
-          WRITE(6,'(6X,A)')' Total XDW-CASPT2 energies:'
-          DO I=1,NSTATE
-            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',
-     &      'XDW-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
-          END DO
+          variant = 'XDW'
         Else if (IFXMS) then
-          WRITE(6,*)
-          WRITE(6,'(6X,A)')' Total XMS-CASPT2 energies:'
-          DO I=1,NSTATE
-            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',
-     &      'XMS-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
-          END DO
+          variant = 'XMS'
         Else if (IFDW) then
-          WRITE(6,*)
-          WRITE(6,'(6X,A)')' Total DW-CASPT2 energies:'
-          DO I=1,NSTATE
-            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',
-     &      'DW-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
-          END DO
+          variant = 'DW '
         Else
-          WRITE(6,*)
-          WRITE(6,'(6X,A)')' Total MS-CASPT2 energies:'
-          DO I=1,NSTATE
-            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',
-     &      'MS-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
-          END DO
+          variant = 'MS '
         End If
+          WRITE(6,*)
+          WRITE(6,'(6X,A,A)')' Total ',trim(variant)//
+     &      '-CASPT2 energies:'
+          DO I=1,NSTATE
+            Call PrintResult(6,'(6x,A,I3,5X,A,F16.8)',trim(variant)//
+     &      '-CASPT2 Root',I,'Total energy:',ENERGY(I),1)
+          END DO
       END IF
 
       IF(IPRGLB.GE.USUAL) THEN
