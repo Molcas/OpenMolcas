2b0cd96db8e12342aac966d8b3c0d165c1a46a49
roland <roland.lindh@kemi.uu.se>
2021-05-03 12:56:23 +0200

    Updates.

--- src/fock_util/cho_lr_mos.f
+++ src/fock_util/cho_lr_mos.f
@@ -35,19 +35,21 @@
       Implicit Real*8 (a-h,o-z)
       Integer  iOK, nDen, nSym
       Integer  nBas(nSym), nIsh(nSym), ISLT(nSym), ISK(nSym)
-      Type (DSBA_Type)  CM(nDen), MSQ(nDen)
+      Type (DSBA_Type)  CM(nDen)
+      Type (DSBA_Type), Target::  MSQ(nDen)
 
-#include "WrkSpc.fh"
 #include "stdalloc.fh"
 
       Real*8, Allocatable, Target:: DMat0(:), PMat0(:)
-      Real*8, Pointer:: DMat(:,:)=>Null()
+      Real*8, Pointer:: DMat(:,:)    =>Null()
       Real*8, Pointer:: PMat(:,:,:,:)=>Null()
+      Real*8, Pointer:: V(:,:)       =>Null()
 
 ************************************************************************
 
       irc=0
       ikc=0
+      i = isk(1)
 
       nBm=nBas(1)
       Do iSym=2,nSym
@@ -56,9 +58,6 @@
       Call mma_allocate(DMat0,nBm**2,Label='DMat0')
       If (nDen.gt.1) Then
          Call mma_allocate(PMat0,2*(nDen*nBm)**2,Label='PMat0')
-         ipV = ip_of_Work(PMat0(1+(nDen*nBm)**2))
-      Else
-         ipV = ip_of_Work(MSQ(1)%A0(1))
       EndIf
 
       iSym=1
@@ -104,23 +103,26 @@ C --- Inactive P[kl](a,b) = sum_i C[k](a,i)*C[l](b,i)
          end do
          Thr=1.0d-13*Ymax
 
-         If (nDen.eq.1) ipV = ip_of_Work(MSQ(1)%A0(1)) + ISK(iSym)
+         If (nDen.eq.1) Then
+            V(1:,1:) => MSQ(1)%SB(iSym)%A2(:,:)
+         Else
+            V(1:n2b,1:n2b) => PMat0((nDen*nBm)**2+1 :
+     &                              (nDen*nBm)**2+n2b**2)
+         End If
 
-         CALL CD_InCore(PMat,n2b,Work(ipV),n2b,NumV,Thr,irc)
+         CALL CD_InCore(PMat,n2b,V,n2b,NumV,Thr,irc)
 
          If (NumV.ne.nIsh(iSym)) ikc=1
 
          If (nDen.gt.1) Then
             Do jden=1,nDen
-               iMSQ = ip_of_Work(MSQ(jden)%A0(1)) + ISK(iSym)
                Do jv=1,NumV
-                  ifr=ipV+n2b*(jv-1)+nBas(iSym)*(jden-1)
-                  ito=iMSQ+nBas(iSym)*(jv-1)
-                  call dcopy_(nBas(iSym),Work(ifr),1,Work(ito),1)
+                  iv = 1 + nBas(iSym)*(jden-1)
+                  call dcopy_(nBas(iSym),V(iv:,jv),1,
+     &                                   MSQ(jDen)%SB(iSym)%A2(:,jv),1)
                End Do
             End Do
          End If
-
 
         EndIf
 
@@ -128,11 +130,11 @@ C --- Inactive P[kl](a,b) = sum_i C[k](a,i)*C[l](b,i)
 
         iSym=iSym+1
 
+        V    => Null()
         DMat => Null()
         PMat => Null()
 
       End Do
-
 
       If (nDen.gt.1) Call mma_deallocate(PMat0)
       Call mma_deallocate(DMat0)
