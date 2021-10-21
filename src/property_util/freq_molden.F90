Molcas/OpenMolcas

commit 1e82061ad3257b2738dd2af011951a6b8f6002b7

Remove Work pointers, allocate scratch inside, etc.

src/property_util/freq_molden.F90
@@ -19,12 +19,11 @@ use Definitions, only: u6
 #endif
 
 implicit none
-#include "Molcas.fh"
 integer(kind=iwp), intent(in) :: nFreq, nVectors, nSym, mDisp(nSym)
 real(kind=wp), intent(in) :: Freq(nFreq), Vectors(nVectors), Intens(nFreq), RedMas(nFreq)
 integer(kind=iwp) :: iCoor, iCoord, iFreq, Lu_9, nAll_Atoms, nCoord, nUnique_Atoms
-character(len=2) :: Element(MxAtom*8)
 real(kind=wp), allocatable :: Coord(:,:), NMode(:,:,:)
+character(len=2), allocatable :: Element(:)
 integer(kind=iwp), external :: isFreeUnit
 
 !                                                                      *
@@ -74,7 +73,7 @@ call Get_Coord_All(Coord,nCoord)
 #ifdef _DEBUGPRINT_
 call RecPrt('Coord(all)',' ',Coord,3,nCoord)
 #endif
-call Get_Name_All(Element)
+call mma_allocate(Element,nCoord,label='Element')
 write(Lu_9,*) '[NATOM]'
 write(Lu_9,*) nCoord
 
@@ -83,6 +82,7 @@ do iCoord=1,nCoord
   write(Lu_9,*) Element(iCoord),Coord(:,iCoord)
 end do
 call mma_deallocate(Coord)
+call mma_deallocate(Element)
 !                                                                      *
 !***********************************************************************
 !                                                                      *

src/cpf/alloc_cpf.f
@@ -231,9 +231,8 @@ C     OCCUPATION NUMBERS FOR ALL POSSIBLE ORBITALS
       LW(90)=LW(89)+NBMAX*NBMAX
 C     OVERLAP MATRIX IN CHARGE
       LW(91)=LW(90)+NTOT
-*     Scratch for property calculation
-      LW(92)=LW(91)+LCIN
 C     ADDRESSES NOT USED
+      LW(92)=LW(91)
       LW(93)=LW(92)
       LIM=LW(93)
 CPAM96      WRITE(6,349)LIM

src/cpf/natct.f
@@ -112,8 +112,7 @@
       End If
       Call Charge(nSym,nBas,Name,H(LW(87)),H(LW(90)),H(LW(91)),2,.True.,
      &            .True.)
-      Call Prpt_old(nSym,nBas,nSum,n2Sum,H(LW(87)),H(LW(90)),
-     *          Lic-LW(92)+1,H(LW(92)))
+      Call Prpt_old(nSym,nBas,nSum,n2Sum,H(LW(87)),H(LW(90)))
 *
       If (iCPF.eq.1) Then
          Header='* CPF NO COEFS'

src/integral_util/oneel.f
@@ -25,9 +25,7 @@
       External Kernel, KrnlMm
 #include "stdalloc.fh"
 #include "real.fh"
-      Real*8, Dimension(:), Allocatable :: Out, Nuc, TMat, Temp, El,
-     &                                     Array
-      Character*16, Dimension(:), Allocatable :: plabs
+      Real*8, Dimension(:), Allocatable :: Out, Nuc, El, Array
       Character Label*8, LBL*4
       Character L_Temp*8
       Real*8 CCoor(3,nComp), rNuc(nComp), PtChrg(nGrid)
@@ -198,9 +196,6 @@ c               Close(28)
                Else If (LBL.eq.'VELO') Then
                    lpole = 1
                End If
-               Call mma_allocate(plabs,nComp,label='plabs')
-               Call mma_allocate(TMat,nComp**2,label='TMat')
-               Call mma_allocate(Temp,nComp,label='Temp')
                If (nComp.eq.1) Then
                   ipC2 = 1 ! dummy
                Else
@@ -208,7 +203,7 @@ c               Close(28)
                End If
                Call Prop(Short,Label,Ccoor(1,1),Ccoor(1,ipC2),
      &                   nIrrep,nBas,mDim,Occ,Thrs,
-     &                   Out,Nuc,lpole,plabs,TMat,Temp,ifallorb)
+     &                   Out,Nuc,lpole,ifallorb)
 *
 * For a properties calculation, save the values of EF or CNT operators,
 * they will be used to write the sum through Add_Info in Drv1El
@@ -237,9 +232,6 @@ c               Close(28)
                  Call mma_deallocate(El)
                End If
 *
-               Call mma_deallocate(Temp)
-               Call mma_deallocate(TMat)
-               Call mma_deallocate(plabs)
                Call mma_deallocate(Nuc)
                Call mma_deallocate(Out)
             End If

src/linalg_util/not_dgeev.F90
@@ -9,7 +9,7 @@
 ! LICENSE or in <http://www.gnu.org/licenses/>.                        *
 !***********************************************************************
 
-subroutine not_DGeEV(iOpt,a,lda,w,z,ldz,n,aux)
+subroutine not_DGeEV(iOpt,a,lda,w,z,ldz,n)
 
 use stdalloc, only: mma_allocate, mma_deallocate
 use Constants, only: Zero, One
@@ -18,9 +18,9 @@ use Definitions, only: wp, iwp, u6
 implicit none
 integer(kind=iwp), intent(in) :: iOpt, lda, ldz, n
 real(kind=wp), intent(inout) :: a(lda,n)
-real(kind=wp), intent(out) :: w(2,n), z(2*ldz*n), aux(n,2)
+real(kind=wp), intent(out) :: w(2,n), z(2*ldz*n)
 integer(kind=iwp) :: i, iErr, iOff
-real(kind=wp), allocatable :: w1(:)
+real(kind=wp), allocatable :: aux(:,:), w1(:)
 
 if (iOpt == 2) then
   write(u6,*) 'not_DGeEV: iOpt=2 is not implemented yet!'
@@ -48,6 +48,7 @@ end if
 
 !-----Order eigenvalues to ESSL standard
 
+call mma_allocate(aux,n,2,label='aux')
 call dcopy_(n,w,1,aux(:,1),1)
 do i=1,n
   w(1,i) = aux(i,1)  ! Real
@@ -79,6 +80,7 @@ do while (i >= 1)
   end if
   i = i-1
 end do
+call mma_deallocate(aux)
 
 return
 

src/mclr/freq.f
@@ -8,9 +8,9 @@
 * For more details see the full text of the license in the file        *
 * LICENSE or in <http://www.gnu.org/licenses/>.                        *
 ************************************************************************
-      Subroutine FREQ(nX,H,nDeg,nrvec,TmpAux,Tmp3,EVec,EVal,RedM,iNeg)
+      Subroutine FREQ(nX,H,nDeg,nrvec,Tmp3,EVec,EVal,RedM,iNeg)
       Implicit Real*8 (a-h,o-z)
-      Real*8 H(*), TmpAux(*),Tmp3(nX,nX),
+      Real*8 H(*), Tmp3(nX,nX),
      &       EVec(2*nX,nX),
      &       EVal(2*nX),
      &       RedM(nX)
@@ -52,7 +52,7 @@
       If ( nX.gt.0 ) then
         Call Not_DGeEv(iOpt,Tmp3,nX,
      &             EVal,EVec,nX,
-     &             nX,TmpAux)
+     &             nX)
       End If
 *
 *-----Compute the harmonic frequencies

src/mclr/freqanal.f
@@ -18,7 +18,7 @@
       logical Do_Molden
       Integer nrvec(*),nDeg(*),iel(3),ldisp(nsym)
       Real*8, Allocatable:: NMod(:), EVec(:), EVal(:), Intens(:),
-     &                      RedMas(:), Tmp2(:), Tmp3(:), Temp(:)
+     &                      RedMas(:), Tmp3(:), Temp(:)
 #include "temperatures.fh"
 *
       Call mma_allocate(NMod,nDisp**2,Label='NMod')
@@ -70,12 +70,10 @@
 *
             If (converged(isym))  Then
                naux=Max(nx*2,nX**2)
-               Call mma_allocate(Tmp2,naux,Label='Tmp2')
                Call mma_allocate(Tmp3,nX**2,Label='Tmp3')
                Call FREQ(nX,H(i3),nDeg(i1),nrvec(i1),
-     &                   Tmp2,Tmp3,EVec,EVal(i1),RedMas,iNeg)
+     &                   Tmp3,EVec,EVal(i1),RedMas,iNeg)
                Call mma_deallocate(Tmp3)
-               Call mma_deallocate(Tmp2)
 *
                iCtl=0
                ll=0

commit d683169f5df7a10f044cb8dbb30cbb4d59eba482

Fix bug in freq.molden files
This was introduced in 1e82061a.

src/property_util/freq_molden.F90
@@ -74,6 +74,7 @@ call Get_Coord_All(Coord,nCoord)
 call RecPrt('Coord(all)',' ',Coord,3,nCoord)
 #endif
 call mma_allocate(Element,nCoord,label='Element')
+call Get_Name_All(Element)
 write(Lu_9,*) '[NATOM]'
 write(Lu_9,*) nCoord
 