001e07ffff86b084c86acf653fde0bb6e33467d3
roland <roland.lindh@kemi.uu.se>
2022-01-07 09:22:17 +0100

    Make VWN_III with the new interface.

---
+++ src/raw_functional_util/vwn_III.f90
@@ -0,0 +1,48 @@
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!                                                                      *
+! Copyright (C) 2022, Roland Lindh                                     *
+!***********************************************************************
+      Subroutine VWN_III(mGrid,nD,F_xc,Coeff)
+      use xc_f03_lib_m
+      use nq_Grid, only: Rho
+      use libxc
+      implicit none
+      integer :: mGrid, nD, iGrid, nRho
+      Real*8 :: F_xc(mGrid)
+      Real*8 :: Coeff
+
+      ! xc functional
+      TYPE(xc_f03_func_t) :: xc_func
+      ! xc functional info
+      TYPE(xc_f03_func_info_t) :: xc_info
+
+      ! VWN_III correlation
+      integer*4, parameter :: func_id = 8
+
+      nRho=SIZE(Rho,1)
+      ! Initialize libxc functional: nRho = 2 means spin-polarized
+      call xc_f03_func_init(xc_func, func_id, int(nRho, 4))
+
+      ! Get the functional's information
+      xc_info = xc_f03_func_get_info(xc_func)
+
+      If (nD.eq.1) Rho(:,:)=2.0D0*Rho(:,:)
+
+      call libxc_interface(xc_func,xc_info,mGrid,nD,F_xc,Coeff)
+
+      call xc_f03_func_end(xc_func)
+
+      If (nD.eq.1) Rho(:,:)=0.5D0*Rho(:,:)
+!     Call RecPrt('F_xc',' ',F_xc,1,mGrid)
+!     Stop 123
+      Return
+
+    End Subroutine VWN_III
