8242019c9e395b73547c8b517a08768b91460677
roland <roland.lindh@kemi.uu.se>
2022-01-10 12:13:49 +0100

    Make cwig.f90.

--- src/raw_functional_util/cwig.f90
+++ src/raw_functional_util/cwig.f90
@@ -1,3 +1,63 @@
+#define _NEWCODE_
+#ifdef _NEWCODE_
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!                                                                      *
+! Copyright (C) 2022, Roland Lindh                                     *
+!***********************************************************************
+      Subroutine cwig(mGrid,Coeff,nD,F_xc)
+      use xc_f03_lib_m
+      use nq_Grid, only: Rho, Sigma
+      use nq_Grid, only: vSigma
+      use libxc
+      implicit none
+      integer :: mGrid, nD, nRho
+      Real*8 :: F_xc(mGrid)
+      Real*8 :: Coeff
+
+      ! xc functional
+      TYPE(xc_f03_func_t) :: xc_func
+      ! xc functional info
+      TYPE(xc_f03_func_info_t) :: xc_info
+
+      ! PBE correlation
+      integer*4, parameter :: func_id = 573
+
+      nRho=SIZE(Rho,1)
+      ! Initialize libxc functional: nRho = 2 means spin-polarized
+      call xc_f03_func_init(xc_func, func_id, int(nRho, 4))
+
+      ! Get the functional's information
+      xc_info = xc_f03_func_get_info(xc_func)
+
+      If (nD.eq.1) Then
+         Rho(:,1:mGrid)=2.00D0*Rho(:,1:mGrid)
+         Sigma(:,1:mGrid)=4.00D0*Sigma(:,1:mGrid)
+         vSigma(:,1:mGrid)=0.50D0*vSigma(:,1:mGrid)
+      End If
+
+      call libxc_interface(xc_func,xc_info,mGrid,nD,F_xc,Coeff)
+
+      call xc_f03_func_end(xc_func)
+
+      If (nD.eq.1) Then
+         Rho(:,1:mGrid)=0.50D0*Rho(:,1:mGrid)
+         Sigma(:,1:mGrid)=0.25D0*Sigma(:,1:mGrid)
+         vSigma(:,1:mGrid)=2.00D0*vSigma(:,1:mGrid)
+      End If
+
+      Return
+
+    End Subroutine cwig
+#else
+
 ************************************************************************
 * This file is part of OpenMolcas.                                     *
 *                                                                      *
@@ -130,3 +190,4 @@ c Avoid unused argument warnings
          Call Unused_real(d2Fdrab)
       End If
       End
+#endif
