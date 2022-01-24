c02662990002060c55c324868acdcc385c38ddf0
roland <roland.lindh@kemi.uu.se>
2022-01-24 18:34:08 +0100

    Start cleanup loops, if statements next.

--- src/nq_util/symadp_full.f
+++ src/nq_util/symadp_full.f
@@ -28,10 +28,12 @@
       use Basis_Info,    only: nBas
       Implicit Real*8 (A-H,O-Z)
 #include "real.fh"
+#include "stdalloc.fh"
       Real*8 AOIntegrals(nBfn,nBfn), SOIntegrals(nSOInt), Fact(ndc,ndc)
       Integer list_s(2,nlist_s)
       Integer nOp(2)
       Integer, Parameter:: iTwoj(0:7)=[1,2,4,8,16,32,64,128]
+      Integer, Allocatable:: BasList(:,:)
 *                                                                      *
 ************************************************************************
 *                                                                      *
@@ -40,38 +42,53 @@
 *                                                                      *
 ************************************************************************
 *                                                                      *
+      Call mma_Allocate(BasList,2,nBfn,Label='BasList')
       loper=1
       Do j1 = 0, nIrrep-1
          iPnt = iPntSO(j1,j1,lOper,nbas)
 
+         ! Pick up only basis functions which contribute to (j1,j1)
+         mBfn=0
          Do iBfn = 1, nBfn
+            ilist_s = iBfn_Index(2,iBfn)
+            iCmp    = iBfn_Index(3,iBfn)
+            iSkal   = list_s(1,ilist_s)
+            iAO     = iSD( 7,iSkal)
+            iSO1=iAOtSO(iAO+iCmp,j1)
+            If (iSO1<0) Cycle
+            mBfn=mBfn+1
+            BasList(1,mBfn)=iBfn
+            BasList(2,mBfn)=iSO1
+         End Do
+
+         Do iBfn_ = 1, mBfn
+            iBfn=BasList(1,iBfn_)
+            iSO1=BasList(2,iBfn_)
+
             ilist_s = iBfn_Index(2,iBfn)
             iCmp    = iBfn_Index(3,iBfn)
             indAO1  = iBfn_Index(6,iBfn)
             iSkal   = list_s(1,ilist_s)
             kDCRE   = list_s(2,ilist_s)
-            iAO     = iSD( 7,iSkal)
             mdci    = iSD(10,iSkal)
             iShell  = iSD(11,iSkal)
             nOp(1) = NrOpr(kDCRE)
             xa = DBLE(iChTbl(j1,nOp(1)))
-            If (iAOtSO(iAO+iCmp,j1)<0) Cycle
-            iSO1=iAOtSO(iAO+iCmp,j1)
 
-            Do jBfn = 1, nBfn
+            Do jBfn_= 1, mBfn
+               jBfn=BasList(1,jBfn_)
+               iSO2=BasList(2,jBfn_)
+
                jlist_s = iBfn_Index(2,jBfn)
                If (jlist_s<ilist_s) Cycle
                jCmp    = iBfn_Index(3,jBfn)
                indAO2  = iBfn_Index(6,jBfn)
                jSkal   = list_s(1,jlist_s)
                kDCRR   = list_s(2,jlist_s)
-               jAO     = iSD( 7,jSkal)
                mdcj    = iSD(10,jSkal)
                jShell  = iSD(11,jSkal)
                nOp(2) = NrOpr(kDCRR)
                xb = DBLE(iChTbl(j1,nOp(2)))
-               If (iAOtSO(jAO+jCmp,j1)<0) Cycle
-               iSO2=iAOtSO(jAO+jCmp,j1)
 
                If (iShell.eq.jShell .and. iCmp<jCmp .and.
      &             nOp(1).eq.nOp(2)) Cycle
@@ -97,6 +114,7 @@
             End Do ! jBfn
          End Do    ! iBfn
       End Do       ! j1
+      Call mma_deAllocate(BasList)
 *
       Return
       End
