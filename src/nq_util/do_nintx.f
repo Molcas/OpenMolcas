b843971750382c72343bfbef919d7bc105935d4c
roland <roland.lindh@kemi.uu.se>
2022-02-01 15:37:34 +0100

    Use dgemm_tri where appropriate.

--- src/nq_util/do_nintx.f
+++ src/nq_util/do_nintx.f
@@ -20,7 +20,7 @@
 #include "nq_info.fh"
       Real*8 AOInt(nBfn,nBfn,nD), TabAO1(nFn,mGrid,nBfn,nD),
      &       TabAO2(mAO,mGrid,nBfn)
-      Real*8, Allocatable:: A1(:,:), A2(:,:)
+      Real*8, Allocatable:: A1(:,:), A2(:,:), A_tri(:)
 *                                                                      *
 ************************************************************************
 ************************************************************************
@@ -44,7 +44,7 @@
          End Do
          Total = Total / DBLE(nBfn)
          Write (6,*) 'Sparsity analysis, iD, iFn', iD, iFn
-         Write (6,*) ' Total parcity in %:', 1.0D2 * Total
+         Write (6,*) ' Total sparcity in %:', 1.0D2 * Total
          Write (6,*) ' Complete Bfn sparcity in %:',
      &                1.0D2*DBLE(lBfn)/DBLE(nBfn)
          Write (6,*)
@@ -65,7 +65,7 @@
          End Do
          Total = Total / DBLE(nBfn)
          Write (6,*) 'Sparsity analysis, iAO', iAO
-         Write (6,*) ' Total parcity in %:', 1.0D2 * Total
+         Write (6,*) ' Total sparcity in %:', 1.0D2 * Total
          Write (6,*) ' Complete Bfn sparcity in %:',
      &                1.0D2*DBLE(lBfn)/DBLE(nBfn)
          Write (6,*)
@@ -78,6 +78,13 @@
       Call mma_allocate(A1,mGrid,nBfn,Label='A1')
       Call mma_allocate(A2,mGrid,nBfn,Label='A2')
 
+*                                                                      *
+************************************************************************
+************************************************************************
+*                                                                      *
+      Select Case (Functional_type)
+*                                                                      *
+      Case (LDA_type)
       Do iD = 1, nD
       Call DCopy_(mGrid*nBfn,TabAO1(1,1,1,iD),nFn,A1,1)
       Call DCopy_(mGrid*nBfn,TabAO2(1,1,1)   ,mAO,A2,1)
@@ -90,16 +97,15 @@
 ************************************************************************
 ************************************************************************
 *                                                                      *
-      Select Case (Functional_type)
-*                                                                      *
-      Case (LDA_type) ! Already take care of above.
-*                                                                      *
-************************************************************************
-************************************************************************
-*                                                                      *
       Case (GGA_type)
 *                                                                      *
       Do iD = 1, nD
+      Call DCopy_(mGrid*nBfn,TabAO1(1,1,1,iD),nFn,A1,1)
+      Call DCopy_(mGrid*nBfn,TabAO2(1,1,1)   ,mAO,A2,1)
+      Call DGEMM_('T','N',nBfn,nBfn,mGrid,
+     &             One,A1,mGrid,
+     &                 A2,mGrid,
+     &             Zero,AOInt(1,1,iD),nBfn)
       Do iBfn = 1, nBfn
          Do jBfn = 1, iBfn
             AOInt_Sym = AOInt(iBfn,jBfn,iD) +  AOInt(jBfn,iBfn,iD)
@@ -115,6 +121,12 @@
       Case (meta_GGA_type1,meta_GGA_type2)
 *                                                                      *
       Do iD = 1, nD
+      Call DCopy_(mGrid*nBfn,TabAO1(1,1,1,iD),nFn,A1,1)
+      Call DCopy_(mGrid*nBfn,TabAO2(1,1,1)   ,mAO,A2,1)
+      Call DGEMM_('T','N',nBfn,nBfn,mGrid,
+     &             One,A1,mGrid,
+     &                 A2,mGrid,
+     &             Zero,AOInt(1,1,iD),nBfn)
       Do iBfn = 1, nBfn
          Do jBfn = 1, iBfn
             AOInt_Sym = AOInt(iBfn,jBfn,iD) +  AOInt(jBfn,iBfn,iD)
@@ -129,6 +141,7 @@
       Call mma_allocate(A1,3*mGrid,nBfn,Label='A1')
       Call mma_allocate(A2,3*mGrid,nBfn,Label='A2')
 
+      Call mma_Allocate(A_tri,nBfn*(nBfn+1)/2,Label='A_tri')
       Do iD = 1, nD
       Call DCopy_(mGrid*nBfn,TabAO1(2,1,1,iD),nFn,A1(1,1),3)
       Call DCopy_(mGrid*nBfn,TabAO2(2,1,1)   ,mAO,A2(1,1),3)
@@ -136,11 +149,24 @@
       Call DCopy_(mGrid*nBfn,TabAO2(3,1,1)   ,mAO,A2(2,1),3)
       Call DCopy_(mGrid*nBfn,TabAO1(4,1,1,iD),nFn,A1(3,1),3)
       Call DCopy_(mGrid*nBfn,TabAO2(4,1,1)   ,mAO,A2(3,1),3)
-      Call DGEMM_('T','N',nBfn,nBfn,3*mGrid,
-     &             One,A1,3*mGrid,
-     &                 A2,3*mGrid,
-     &             One ,AOInt(1,1,iD),nBfn)
+      Call DGEMM_Tri('T','N',nBfn,nBfn,3*mGrid,
+     &               One,A1,3*mGrid,
+     &                   A2,3*mGrid,
+     &               Zero,A_Tri,nBfn)
+      ijBfn = 0
+      Do iBfn = 1, nBfn
+         Do jBfn = 1, iBfn-1
+            ijBfn = ijBfn + 1
+            AOInt_Sym = A_tri(ijBfn)
+            AOInt(iBfn,jBfn,iD) = AOInt(iBfn,jBfn,iD) + AOInt_Sym
+            AOInt(jBfn,iBfn,iD) = AOInt(jBfn,iBfn,iD) + AOInt_Sym
+         End Do
+         ijBfn = ijBfn + 1
+         AOInt_Sym = A_tri(ijBfn)
+         AOInt(iBfn,iBfn,iD) = AOInt(iBfn,iBfn,iD) + AOInt_Sym
       End Do
+      End Do
+      Call mma_deAllocate(A_tri)
 *                                                                      *
 ************************************************************************
 ************************************************************************
