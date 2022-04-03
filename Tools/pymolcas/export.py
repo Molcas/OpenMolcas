d645ed0b98adaf9191c6583f9b1154fc23c3823e
Ignacio Fdez. Galván <Ignacio.Fernandez@kemi.uu.se>
2022-04-03 18:06:08 +0200

    Pass XC_POLARIZED to Libxc
    Also pass CMAKE_BUILD_TYPE when building.
    Fixes #340

--- Tools/pymolcas/export.py
+++ Tools/pymolcas/export.py
@@ -11,7 +11,7 @@
 # For more details see the full text of the license in the file        *
 # LICENSE or in <http://www.gnu.org/licenses/>.                        *
 #                                                                      *
-# Copyright (C) 2015,2017,2018,2022 Ignacio Fdez. Galván               *
+# Copyright (C) 2015,2017,2018,2022, Ignacio Fdez. Galván              *
 #***********************************************************************
 
 '''
