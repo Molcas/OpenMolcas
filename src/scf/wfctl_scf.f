d39e60428ea22be69ff5a1950c9bed8ce79514a1
roland <roland.lindh@kemi.uu.se>
2022-04-26 13:15:02 +0200

    No convergence on energy increase.

--- src/scf/wfctl_scf.f
+++ src/scf/wfctl_scf.f
@@ -882,7 +882,7 @@
 *
 *        EmConv is true.
 *
-         If (EDiff>0.0.and..Not.Reset) EDiff=Ten*EDif
+         If (EDiff>0.0.and..Not.Reset) EDiff=Ten*EThr
          If (iter.ne.1             .AND.
      &       (Abs(EDiff).le.EThr)  .AND.
      &       (Abs(FMOMax).le.FThr) .AND.
