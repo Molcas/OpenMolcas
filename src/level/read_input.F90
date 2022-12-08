fb285873df1b40186c48e37a1cb9b87bf4c6de2d
Nike Dattani <dattani.nike@gmail.com>
2022-12-08 12:58:13 -0500

    Pushed after: - it seems that this version of the code shows what the problem is: all keywords are read in correclty, but LEVEL tries reading the input file a second time, spuriously.

--- src/level/read_input.F90
+++ src/level/read_input.F90
@@ -34,12 +34,12 @@ logical(kind=iwp) :: skip, exists
 character(len=4) :: word
 character(len=80) :: Title1(10), Title2(10)
 character(len=180) :: Line, l84, l84x
-integer(kind=iwp), parameter :: ntab = 37
+integer(kind=iwp), parameter :: ntab = 40
 integer(kind=iwp) :: LuIn
 character(len=*), parameter :: tabinp(ntab) = ['IAN1','IMN1','IAN2','IMN2','CHAR','NUMP','RH  ','RMIN','PRV ','ARV ', &
                                                'EPS ','NTP ','LPPO','IOME','VLIM','IPOT','PPAR','QPAR','NSR ','NLR ', &
-                                               'IBOB','DSCM','REQ ','RREF','NCMM','IVSR','TDST','RHOA', &
-                                               'NLEV','AUTO','LCDC','LXPC','NJM ','JDJR','IWR ','LPRW','END ']
+                                               'IBOB','DSCM','REQ ','RREF','NCMM','IVSR','TDST','RHOA','MMLR','CMM ', &
+                                               'PARM','NLEV','AUTO','LCDC','LXPC','NJM ','JDJR','IWR ','LPRW','END ']
 character(len=180), external :: Get_Ln, Get_Ln_EOF
 integer(kind=iwp), external :: IsFreeUnit
 
@@ -81,18 +81,20 @@ NCMM = 3                      ! Number of long-range constants included in u(r) 
 IVSR  = -2                    !
 TDSTT = 1                     ! 
 RHOAB = 0.54                  ! Constant used for damping functions in the MLR model.
+write(6,*) 'RHOAB=',RHOAB
 
-MMLR(1) = 6                   ! Inverse-power for the first long-range u(r) term for an MLR model
-CMM(1) = 6.719d+06            ! Numerator for the first long-range u(r) term for an MLR model
-MMLR(2) = 8                   ! Inverse-power for the second long-range u(r) term for an MLR model
-CMM(2) = 1.12635d+08          ! Numerator for the second long-range u(r) term for an MLR model
-MMLR(3) = 10                  ! Inverse-power for the third long-range u(r) term for an MLR model  
-CMM(3) = 2.78694d+09          ! Numerator for the third long-range u(r) term for an MLR model
-
-PARM(1) = -5.156803528943D-01 ! Beta_0 for an MLR potential
-PARM(2) = -9.585070416286D-02 ! Beta_1 for an MLR potential
-PARM(3) =  1.170797201140D-01 ! Beta_2 for an MLR potential
-PARM(4) = -2.282814434665D-02 ! Beta_3 for an MLR potential
+!MMLR(1) = 6                   ! Inverse-power for the first long-range u(r) term for an MLR model
+!CMM(1) = 6.719d+06            ! Numerator for the first long-range u(r) term for an MLR model
+!MMLR(2) = 8                   ! Inverse-power for the second long-range u(r) term for an MLR model
+!CMM(2) = 1.12635d+08          ! Numerator for the second long-range u(r) term for an MLR model
+!MMLR(3) = 10                  ! Inverse-power for the third long-range u(r) term for an MLR model  
+!CMM(3) = 2.78694d+09          ! Numerator for the third long-range u(r) term for an MLR model
+write(6,*) 'MMLR=',MMLR
+!
+!PARM(1) = -5.156803528943D-01 ! Beta_0 for an MLR potential
+!PARM(2) = -9.585070416286D-02 ! Beta_1 for an MLR potential
+!PARM(3) =  1.170797201140D-01 ! Beta_2 for an MLR potential
+!PARM(4) = -2.282814434665D-02 ! Beta_3 for an MLR potential
 
 NLEV1 = -999                  !
 AUTO1 = 1                     !
@@ -171,145 +173,168 @@ input: do
    case (tabinp(11)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,EPS)
+     write(6,*) 'EPS=',EPS
 
      ! Read NTP, LPPOT, IOMEG, VLIM
    case (tabinp(12)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,NTP)
+     write(6,*) 'NTP=',NTP
 
    case (tabinp(13)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,LPPOT)
+     write(6,*) 'LPPOT=',LPPOT
 
    case (tabinp(14)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,IOMEG1)
+     write(6,*) 'IOMEG1=',IOMEG1
 
    case (tabinp(15)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,VLIM)
+     write(6,*) 'VLIM=',VLIM
 
      ! Read  IPOTL, PPAR, QPAR, NSR, NLR, IBOB
    case (tabinp(16)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,IPOTL)
+     write(6,*) 'IPOTL=',IPOTL
 
    case (tabinp(17)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,PPAR)
+     write(6,*) 'PPAR=',PPAR
 
    case (tabinp(18)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,QPAR)
+     write(6,*) 'QPAR=',QPAR
 
    case (tabinp(19)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,NSR)
+     write(6,*) 'NSR=',NSR
 
    case (tabinp(20)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,NLR)
+     write(6,*) 'NLR=',NLR
 
    case (tabinp(21)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,IBOB)
+     write(6,*) 'IBOB=',IBOB
 
      ! Read DSCM, REQ, Rref
    case (tabinp(22)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,DSCM)
+     write(6,*) 'DSCM=',DSCM
 
    case (tabinp(23)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,REQ)
+     write(6,*) 'REQ=',REQ
 
    case (tabinp(24)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,RREF)
+     write(6,*) 'RREF=',RREF
 
      ! Read NCMM, IVSR, TDSTT, rhoAB
    case (tabinp(25)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,NCMM)
+     write(6,*) 'NCMM=',NCMM
 
    case (tabinp(26)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,IVSR)
+     write(6,*) 'IVSR=',IVSR
 
    case (tabinp(27)) 
      Line = Get_Ln(LuIn)
      call Get_I1(1,TDSTT)
+     write(6,*) 'TDSTT=',TDSTT
 
    case (tabinp(28)) 
      Line = Get_Ln(LuIn)
      call Get_F1(1,RHOAB)
+     write(6,*) 'RHOAB=',RHOAB
+     write(6,*) ''
 
-!   ! Read MMLR(1), CMM(1), MMLR(2), CMM(2), MMLR(3), CMM(3)
-!  case (tabinp(29)) 
-!    Line = Get_Ln(LuIn)
-!    call Get_I1(1,MMLR(1))
-!    call Get_I1(2,MMLR(2))
-!    call Get_I1(3,MMLR(3))
-
-!  case (tabinp(30)) 
-!    Line = Get_Ln(LuIn)
-!    call Get_F1(1,CMM(1))
-!    call Get_F1(2,CMM(2))
-!    call Get_F1(3,CMM(3))
-
-!    ! Read PARM(1), PARM(2), PARM(3),PARM(4)
-!  case (tabinp(31)) 
-!    Line = Get_Ln(LuIn)
-!    call Get_F1(1,PARM(1))
-!    call Get_F1(2,PARM(2))
-!    call Get_F1(3,PARM(3))
-!    call Get_F1(4,PARM(4))
-
-    ! Read NLEV1, AUTO1, LCDC, LXPCT, NJM, JDJR, IWR, LPRWF
-  case (tabinp(29)) 
+   ! Read MMLR(1), CMM(1), MMLR(2), CMM(2), MMLR(3), CMM(3)
+  case (tabinp(29))  
     Line = Get_Ln(LuIn)
-    call Get_I1(1,NLEV1)
+    call Get_I(1,MMLR,3)
+    write(6,*) 'MMLR=',MMLR
 
   case (tabinp(30)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,AUTO1)
+    call Get_F(1,CMM,3)
+    write(6,*) 'CMM=',CMM
 
+    ! Read PARM(1), PARM(2), PARM(3),PARM(4)
   case (tabinp(31)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,LCDC)
+    call Get_F(1,PARM,4)
+    write(6,*) 'PARM=',PARM
 
+    ! Read NLEV1, AUTO1, LCDC, LXPCT, NJM, JDJR, IWR, LPRWF
   case (tabinp(32)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,LXPCT)
+    call Get_I1(1,NLEV1)
+    write(6,*) 'NLEV1=',NLEV1
 
   case (tabinp(33)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,NJM)
+    call Get_I1(1,AUTO1)
+     write(6,*) 'AUTO1=',AUTO1
 
   case (tabinp(34)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,JDJR)
+    call Get_I1(1,LCDC)
+     write(6,*) 'LCDC=',LCDC
 
   case (tabinp(35)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,IWR)
+    call Get_I1(1,LXPCT)
+     write(6,*) 'LXPCT=',LXPCT
 
   case (tabinp(36)) 
     Line = Get_Ln(LuIn)
-    call Get_I1(1,LPRWF)
+    call Get_I1(1,NJM)
+     write(6,*) 'NJM=',NJM
 
-   case (tabinp(37))
+  case (tabinp(37)) 
+    Line = Get_Ln(LuIn)
+    call Get_I1(1,JDJR)
+     write(6,*) 'JDJR=',JDJR
+
+  case (tabinp(38)) 
+    Line = Get_Ln(LuIn)
+    call Get_I1(1,IWR)
+     write(6,*) 'IWR=',IWR
+
+  case (tabinp(39)) 
+    Line = Get_Ln(LuIn)
+    call Get_I1(1,LPRWF)
+     write(6,*) 'LPRWF=',LPRWF
+
+   case (tabinp(40))
      exit input
 
-   case default
-     write(6,*)
-     write(6,*) '******************************************'
-     write(6,*) ' LEVEL Error: Input line not recognized.'
-     write(6,*) ' Input line, in upper case:'
-     write(6,'(a)') line
-     write(6,*) ' Extracted keyword: ',word
-     write(6,*) '******************************************'
-     call Quit_OnUserError()
+!  case default
+!    write(6,*)
+!    write(6,*) '******************************************'
+!    write(6,*) ' LEVEL Error: Input line not recognized.'
+!    write(6,*) ' Input line, in upper case:'
+!    write(6,'(a)') line
+!    write(6,*) ' Extracted keyword: ',word
+!    write(6,*) '******************************************'
+!    call Quit_OnUserError()
 end select
 end do input
 
