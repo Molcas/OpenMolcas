5c92b2aa2b9b775be401c2edae03e58c68ad2571
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2023-03-17 09:57:26 -0500

    fix appos

--- src/mcpdft/readvc.f
+++ src/mcpdft/readvc.f
@@ -443,7 +443,7 @@ CSVC: read the L2ACT and LEVEL arrays from the jobiph file
          END IF
       Else If (InVec.eq.1) then
         IF(IPRLEV.ge.VERBOSE) Write(LF,'(6X,2A)')
-     &  'MC-PDFT shouldnt be guessing orbitals...something wrong',
+     &  "MC-PDFT shouldn't be guessing orbitals...something wrong",
      &  'with your calculation or this module..'
         call abend()
       Else
