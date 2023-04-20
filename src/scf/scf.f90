c88f1e1424312699dcb041416af9aebfbcc9e698
roland <roland.lindh@kemi.uu.se>
2023-04-20 22:18:04 +0200

    Update to f90 format.

--- src/scf/scf.f90
+++ src/scf/scf.f90
@@ -1,90 +1,75 @@
-************************************************************************
-* This file is part of OpenMolcas.                                     *
-*                                                                      *
-* OpenMolcas is free software; you can redistribute it and/or modify   *
-* it under the terms of the GNU Lesser General Public License, v. 2.1. *
-* OpenMolcas is distributed in the hope that it will be useful, but it *
-* is provided "as is" and without any express or implied warranties.   *
-* For more details see the full text of the license in the file        *
-* LICENSE or in <http://www.gnu.org/licenses/>.                        *
-*                                                                      *
-* Copyright (C) 1992, Per-Olof Widmark                                 *
-*               1992, Markus P. Fuelscher                              *
-*               1992, Piotr Borowski                                   *
-*               2003-2005, Valera Veryazov                             *
-*               2017, Roland Lindh                                     *
-************************************************************************
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!                                                                      *
+! Copyright (C) 1992, Per-Olof Widmark                                 *
+!               1992, Markus P. Fuelscher                              *
+!               1992, Piotr Borowski                                   *
+!               2003-2005, Valera Veryazov                             *
+!               2017, Roland Lindh                                     *
+!***********************************************************************
       subroutine SCF(ireturn)
-************************************************************************
-*                                                                      *
-*     purpose: perform RHF calculations                                *
-*                                                                      *
-*     calls to: OpnFls_SCF,ReadIn_SCF,SOrb,WrInp_SCF,WfCtl_SCF,Final,  *
-*               ClsFls_SCF                                             *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     written by:                                                      *
-*     P.O. Widmark, P. Borowski, M. Schuetz, and R. Lindh              *
-*     University of Lund, Sweden, 1992-1998                            *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     history: none                                                    *
-*                                                                      *
-************************************************************************
+!***********************************************************************
+!                                                                      *
+!     purpose: perform RHF and UHF calculations                        *
+!                                                                      *
+!                                                                      *
+!***********************************************************************
       Use SCF_Arrays, only: CMO, HDiag, OccNo
       Use Interfaces_SCF, Only: OccDef
       use OFembed, only: Do_OFemb
-      use InfSCF, only: DSCF, nDisc, nCore, iUHF, AufB, nBB, mOV,
-     &                  OnlyProp, iStatPrn, Atom, KSDFT, Name, nnB,
-     &                  Type
+      use InfSCF, only: DSCF, nDisc, nCore, iUHF, AufB, nBB, mOV, OnlyProp, iStatPrn, Atom, KSDFT, Name, nnB, Type
       use stdalloc, only: mma_allocate, mma_deallocate
-      use Files
+      use Files, only: LuInp
       Implicit None
-*
+!
 #include "twoswi.fh"
 #include "warnings.h"
-*
+!
       Integer iReturn
 
       Character(LEN=8) EMILOOP
       Logical FstItr, Semi_Direct
       Real*8 SIntTh,TCPU1, TCPU2, TWALL1, TWALL2
       Integer iTerm, LUOrb, MemLow, MemSew, nD, LthH
-*
-*----------------------------------------------------------------------*
-*     Start                                                            *
-*----------------------------------------------------------------------*
-*
+!
+!----------------------------------------------------------------------*
+!     Start                                                            *
+!----------------------------------------------------------------------*
+!
       Call CWTime(TCPU1,TWall1)
       Call SCF_Init()
       iTerm=0
-*
+!
       Call OpnFls_SCF()
       Call ReadIn_SCF(SIntTh)
       LuOrb=LuInp
-*
-      Semi_Direct = DSCF .and. (nDisc.ne.0 .or. (nCore.ne.0
-     &      .and. nDisc.eq.0))
+!
+      Semi_Direct = DSCF .and. (nDisc.ne.0 .or. (nCore.ne.0 .and. nDisc.eq.0))
       If (Semi_Direct) Then
          Call mma_MaxDBLE(MemSew)
          MemLow=Min(MemSew/2,1024*1024)
          MemSew=Max(MemSew/10,MemLow)
          Call xsetmem_ints(MemSew)
       End If
-*
+!
       Call Init_SCF()
-*                                                                      *
-************************************************************************
-*                                                                      *
-*     Pick up the starting orbitals and set the occupation numbers.
-*
+!                                                                      *
+!***********************************************************************
+!                                                                      *
+!     Pick up the starting orbitals and set the occupation numbers.
+!
       LuOrb=LuInp
       nD = iUHF + 1
       Call SOrb(LuOrb,SIntTh,iTerm)
       Call OccDef(OccNo,nnB,nD,CMO,nBB)
-*
+!
       Call mma_deallocate(HDiag)
       If (Aufb) Then
          lthH = nBB*nD
@@ -92,9 +77,9 @@
          lthH = mOV
       End If
       Call mma_allocate(HDiag,lthH,Label='HDiag')
-*                                                                      *
-************************************************************************
-*                                                                      *
+!                                                                      *
+!***********************************************************************
+!                                                                      *
       Call WrInp_SCF(SIntTh)
 
       Call Cre_SCFWfn()
@@ -108,15 +93,15 @@
       Call mma_deallocate(Type)
       Call mma_deallocate(Atom)
       Call mma_deallocate(Name)
-*
+!
       Call CWTime(TCPU2,TWall2)
-*
+!
       Call GMFree()
       Call ClsFls_SCF()
       If (Semi_Direct) Call xRlsMem_Ints()
-*
-*     Call MolDen Interface
-*
+!
+!     Call MolDen Interface
+!
       If(iUHF.eq.0) Then
          Call Molden_Interface(iUHF,'SCFORB','MD_SCF')
       Else
@@ -125,11 +110,11 @@
       if(iStatPRN.gt.0) then
        Call FastIO('STATUS')
       endif
-*
-*.... Everything has to come to an end...
-*
+!
+!.... Everything has to come to an end...
+!
       iReturn=iTerm
-*
+!
       If (Do_OFemb) Then
          Call GetEnvF('EMIL_InLoop',EMILOOP)
          If (EMILOOP.eq.' ') EMILOOP='0'
@@ -141,16 +126,16 @@
             Call Check_FThaw(iReturn)
          EndIf
       EndIf
-*
-*----------------------------------------------------------------------*
-*     Exit                                                             *
-*----------------------------------------------------------------------*
+!
+!----------------------------------------------------------------------*
+!     Exit                                                             *
+!----------------------------------------------------------------------*
       return
-*
-      End
-************************************************************************
+!
+      End subroutine SCF
+!***********************************************************************
       SubRoutine IniLLs()
-*     initialize the diverse linked lists
+!     initialize the diverse linked lists
       use LnkLst, only: LLlist,LLGrad,LLdGrd,LLDelt,LLy,LLx,Init_LLs
       use MxDM, only: MxOptm
       Implicit None
@@ -163,9 +148,9 @@
       Call IniLst(LLy,20)
       Call IniLst(LLx,MxOptm)
       Init_LLs=.True.
-*
+!
       End subroutine IniLLs
-*----------------------------------------------------------------------*
+!----------------------------------------------------------------------*
 #define _NOTUSED_
 #ifdef _NOTUSED_
       Subroutine StatLLS()
@@ -183,11 +168,11 @@
          Write (6,*) ' Linked lists are not there!'
       End If
       Return
-      End
+      End Subroutine StatLLS
 #endif
-*----------------------------------------------------------------------*
-      SubRoutine KiLLs
-*     dispose the diverse linked lists
+!----------------------------------------------------------------------*
+      SubRoutine KiLLs()
+!     dispose the diverse linked lists
       use LnkLst, only: LLGrad,LLdGrd,LLDelt,LLy,LLx,Init_LLs
       Implicit None
       If (Init_LLs) Then
@@ -202,8 +187,8 @@
          Write (6,*) ' Linked list already killed!'
       End If
       Return
-      End
-*----------------------------------------------------------------------*
+      End SubRoutine KiLLs
+!----------------------------------------------------------------------*
       Subroutine RclLLs(iDskPt)
       use InfSO, only: MemRsv
       use LnkLst, only: LLGrad,LLdGrd,LLDelt,LLy,LLx,Init_LLs
@@ -216,17 +201,17 @@
       Call RclLst(LLy   ,Lux  ,iDskPt(4),MemRsv)
       Call RclLst(LLx   ,Luy  ,iDskPt(5),MemRsv)
       Init_LLs=.True.
-*     Call StatLLs()
+!     Call StatLLs()
       Return
-      End
-*----------------------------------------------------------------------*
+      End Subroutine RclLLs
+!----------------------------------------------------------------------*
       Subroutine DmpLLs(iDskPt)
       use LnkLst, only: LLGrad,LLdGrd,LLDelt,LLy,LLx,Init_LLs
       use Files
       Implicit None
       Integer iDskPt(5)
       If (Init_LLs) Then
-*        Call StatLLs()
+!        Call StatLLs()
          Call DmpLst(LLGrad,LuGrd,iDskPt(1))
          Call DmpLst(LLDgrd,LuDGd,iDskPt(2))
          Call DmpLst(LLDelt,LuDel,iDskPt(3))
@@ -237,13 +222,13 @@
          Write (6,*) ' Linked list already killed!'
       End If
       Return
-      End
-*----------------------------------------------------------------------*
+      End Subroutine DmpLLs
+!----------------------------------------------------------------------*
       Subroutine StlLst(LLink)
       use LnkLst, only: nLList
       Implicit None
       Integer LLink, iRoot
-*     return
+!     return
        Write (6,*)
        Write (6,*) '*********** Status of Linked List *************'
        Write (6,*)
@@ -275,21 +260,21 @@
        Write (6,*) '************ End of Status Report *************'
        Write (6,*)
       Return
-      End
-*----------------------------------------------------------------------*
+      End Subroutine StlLst
+!----------------------------------------------------------------------*
       Subroutine Free_TLists()
       use InfSCF, Only: DSCF
       Implicit None
-*
+!
       If (DSCF) Then
          Call Free_TList()
          Call Free_PPList()
          Call Free_GTList()
       End If
-*
+!
       Return
-      End
-*----------------------------------------------------------------------*
+      End Subroutine Free_TLists
+!----------------------------------------------------------------------*
       Subroutine Reduce_Thresholds(EThr_,SIntTh)
       use InfSO, only: DltNTh
       use InfSCF, only: EThr, DThr, FThr
@@ -298,7 +283,7 @@
       Implicit None
       Real*8 EThr_, SIntTh, Relax
       Real*8, External:: Get_ThrInt
-*
+!
       Write (6,*)
       Write (6,*) 'Temporary increase of thresholds...'
       Write (6,*)
@@ -307,12 +292,12 @@
       DThr_old=DThr
       DltNTh_old=DltNTh
       FThr_old=FThr
-*
-*---- Get threshold used in connection of products of integrals and
-*     densities
-*
+!
+!---- Get threshold used in connection of products of integrals and
+!     densities
+!
       ThrInt_Old=Get_ThrInt()
-*
+!
       EThr=EThr_
       If (EThr_old.eq.Zero) Then
          Relax=One
@@ -324,15 +309,15 @@
       DltNTh=100.0D0*EThr
       FThr=FThr*Relax
       Call xSet_ThrInt(ThrInt_Old*Relax)
-*
+!
       Return
-      End
+      End Subroutine Reduce_Thresholds
       Subroutine Reset_Thresholds()
       use InfSO, only: DltNTh
       use InfSCF, only: EThr, DThr, FThr
       use Save_Stuff
       Implicit None
-*
+!
       Write (6,*)
       Write (6,*) 'Restore thresholds...'
       Write (6,*)
@@ -341,6 +326,6 @@
       DltNTh=DltNTh_old
       FThr=FThr_old
       Call xSet_ThrInt(ThrInt_Old)
-*
+!
       Return
-      End
+      End Subroutine Reset_Thresholds
