6b386d6269d0cb5d70daade67169864e6f7b553c
roland <roland.lindh@kemi.uu.se>
2023-05-16 11:13:57 +0200

    More cleanup.

--- src/scf/cre_scfwfn.f90
+++ src/scf/cre_scfwfn.f90
@@ -16,10 +16,10 @@
 !     exists, it will be overwritten.
 #ifdef _HDF5_
       use mh5, only: mh5_create_file, mh5_init_attr, mh5_create_dset_real, mh5_create_dset_str
-#endif
       use InfSCF, only: KSDFT, nBas, nBB, nD, nnB, nSym
       use SCFWFn, only: wfn_mocoef, wfn_mocoef_a,  wfn_mocoef_b, wfn_occnum, wfn_occnum_a, wfn_occnum_b,                 &
                         wfn_orbene, wfn_orbene_a, wfn_orbene_b, wfn_tpidx, wfn_tpidx_a, wfn_tpidx_b, wfn_energy, wfn_fileid
+#endif
       implicit none
 #ifdef _HDF5_
 
