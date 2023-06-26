8cd98c6d381ce618e8851e6a033dc408c9c2859c
Arta Safari <a.safari@fkf.mpg.de>
2023-06-26 16:05:53 +0200

    introduce disk pointer for easy book keeping

--- src/rasscf/putrlx.f
+++ src/rasscf/putrlx.f
@@ -21,6 +21,7 @@
       Parameter (ROUTINE='PUTRLX  ')
       Real*8 D(*),DS(*),P(*),DAO(*),C(*)
       Dimension rdum(1)
+      logical :: disk_pointer_moved
 
       IPRLEV=IPRLOC(3)
       IF(IPRLEV.ge.DEBUG) THEN
@@ -32,119 +33,116 @@
 
       jDisk = IADR15(3)
       NZ=NTOT2
-      ! Do i=1,iRlxRoot-1
-      !   Call DDaFile(JOBIPH,0,rdum,NACPAR,jDisk)
-      !   Call DDaFile(JOBIPH,0,rdum,NACPAR,jDisk)
-      !   Call DDaFile(JOBIPH,0,rdum,NACPR2,jDisk)
-      !   Call DDaFile(JOBIPH,0,rdum,NACPR2,jDisk)
-      ! End Do
 
       Call GetMem('TEMP','ALLO','REAL',ipDA,NZ)
       Call GetMem('TEMP','ALLO','REAL',ipDI,NZ)
       Call GetMem('TEMP','ALLO','REAL',ipDS,NZ)
 
       do i = 1, LRoots
+        disk_pointer_moved = .False.
         do j = 1, i
-        if (iroot(j) == i) then
+          if (iroot(j) == i) then
+            ! jDisk pointer now at position j
+            call ddafile(JOBIPH, 2, D, NACPAR, jDisk)
+            call ddafile(JOBIPH, 2, DS, NACPAR, jDisk)
+            call ddafile(JOBIPH, 2, P, NACPR2, jDisk)
+            call ddafile(JOBIPH, 0, rdum, NACPR2, jDisk)
+            disk_pointer_moved = .True.
+*
+*           Construc D-ACTIVE AND D-INACTIVE IN AO BASIS
+*
+*
+            Call Get_D1I_RASSCF(C,Work(ipDI))
+*
+            Call GetMem('TEMP','ALLO','REAL',ipD,NACPAR)
+*
+            CALL DCOPY_(NACPAR,DS,1,Work(ipD),1)
+            Call DBLOCK(Work(ipD))
+            CALL Get_D1A_RASSCF(C,Work(ipD),Work(ipDS))
+*
+            CALL DCOPY_(NACPAR,D,1,Work(ipD),1)
+            Call DBLOCK(Work(ipD))
+            CALL Get_D1A_RASSCF(C,Work(ipd),Work(ipDA))
+*
+*           Construct the Fock matrix used for the connection term.
+*
+            NFSIZE=MAX(NACPAR,NTOT4)
+            Call GetMem('TEMP','ALLO','REAL',ipF,NFSIZE)
+            Call GetMem('TEMP','ALLO','REAL',ipB,NZ)
+            Call GetMem('TEMP','ALLO','REAL',ipQ,NZ)
+            Call GetMem('TEMP','ALLO','REAL',ipFA,NZ)
+            Call GetMem('TEMP','ALLO','REAL',ipFI,NZ)
+            Call GetMem('TEMP','ALLO','REAL',ipPUVX,NFINT)
+            Call GetMem('TEMP','ALLO','REAL',iptuvx,nacpr2)
+            IPR=0
+            IF(IPRLEV.EQ.3) IPR=1
+            IF(IPRLEV.EQ.4) IPR=5
+            IF(IPRLEV.EQ.5) IPR=10
+            Call FZero(Work(ipPUVX),NFINT)
+            Call TraCtl2(C,Work(ipPUVX),Work(ipTUVX),
+     &                   Work(ipDI),Work(ipFI),
+     &                   Work(ipDA),Work(ipFA),ipr,lsquare,ExFac)
+            CALL SGFCIN(C,WORK(ipF),Work(ipFI),Work(ipDI),Work(ipDA),
+     &                                                    Work(ipDS))
+            call dcopy_(ntot4,[0.0d0],0,Work(ipF),1)
+            call dcopy_(ntot4,[0.0d0],0,Work(ipB),1)
+*
+*           Prevent FMAT from changing Active fock matrix
+*
+            iTmp=newfock
+            newFock=-99999
+*
+            Call Fmat(C,Work(ipPUVX),Work(ipD),Work(ipDA),Work(ipFI),
+     &                Work(ipFA))
+            newFock=itmp
+            IFINAL=1
+            rTmp=   CBLBM
+            itmp= IBLBM
+            jtmp= JBLBM
+            istmp= ISYMBB
 
-        Call DDaFile(JOBIPH,2,D,NACPAR,jDisk)
-        Call DDaFile(JOBIPH,2,DS,NACPAR,jDisk)
-        Call DDaFile(JOBIPH,2,P,NACPR2,jDisk)
-        ! Call DDaFile(JOBIPH,0,rdum,NACPR2,jDisk)
+            IF(ISTORP(NSYM+1).GT.0) THEN
+                CALL GETMEM('ISTRP','ALLO','REAL',ipP,ISTORP(NSYM+1))
+                CALL PMAT_RASSCF(P,WORK(ipP))
+            Else
+                ipP = ip_Dummy
+            END IF
 *
-*       Construc D-ACTIVE AND D-INACTIVE IN AO BASIS
-*
-*
-        Call Get_D1I_RASSCF(C,Work(ipDI))
-*
-        Call GetMem('TEMP','ALLO','REAL',ipD,NACPAR)
-*
-        CALL DCOPY_(NACPAR,DS,1,Work(ipD),1)
-        Call DBLOCK(Work(ipD))
-        CALL Get_D1A_RASSCF(C,Work(ipD),Work(ipDS))
-*
-        CALL DCOPY_(NACPAR,D,1,Work(ipD),1)
-        Call DBLOCK(Work(ipD))
-        CALL Get_D1A_RASSCF(C,Work(ipd),Work(ipDA))
-*
-*       Construct the Fock matrix used for the connection term.
-*
-        NFSIZE=MAX(NACPAR,NTOT4)
-        Call GetMem('TEMP','ALLO','REAL',ipF,NFSIZE)
-        Call GetMem('TEMP','ALLO','REAL',ipB,NZ)
-        Call GetMem('TEMP','ALLO','REAL',ipQ,NZ)
-        Call GetMem('TEMP','ALLO','REAL',ipFA,NZ)
-        Call GetMem('TEMP','ALLO','REAL',ipFI,NZ)
-        Call GetMem('TEMP','ALLO','REAL',ipPUVX,NFINT)
-        Call GetMem('TEMP','ALLO','REAL',iptuvx,nacpr2)
-        IPR=0
-        IF(IPRLEV.EQ.3) IPR=1
-        IF(IPRLEV.EQ.4) IPR=5
-        IF(IPRLEV.EQ.5) IPR=10
-        Call FZero(Work(ipPUVX),NFINT)
-        Call TraCtl2(C,Work(ipPUVX),Work(ipTUVX),
-     &               Work(ipDI),Work(ipFI),
-     &               Work(ipDA),Work(ipFA),ipr,lsquare,ExFac)
-        CALL SGFCIN(C,WORK(ipF),Work(ipFI),Work(ipDI),Work(ipDA),
-     &                                                Work(ipDS))
-        call dcopy_(ntot4,[0.0d0],0,Work(ipF),1)
-        call dcopy_(ntot4,[0.0d0],0,Work(ipB),1)
-*
-*       Prevent FMAT from changing Active fock matrix
-*
-        iTmp=newfock
-        newFock=-99999
-*
-        Call Fmat(C,Work(ipPUVX),Work(ipD),Work(ipDA),Work(ipFI),
-     &            Work(ipFA))
-        newFock=itmp
-        IFINAL=1
-        rTmp=   CBLBM
-        itmp= IBLBM
-        jtmp= JBLBM
-        istmp= ISYMBB
+            Call FOCK(Work(ipF),Work(ipB),Work(ipFI),
+     &                Work(ipFA),Work(ipd),WORK(ipP),Work(ipQ),
+     &                Work(ipPUVX), IFINAL,C)
+            CBLBM=rtmp
+            IBLBM=itmp
+            JBLBM=jtmp
+            ISYMBB=istmp
+            IF(ISTORP(NSYM+1).GT.0)
+     &          CALL GETMEM('ISTRP','FREE','REAL',ipP,ISTORP(NSYM+1))
 
-        IF(ISTORP(NSYM+1).GT.0) THEN
-            CALL GETMEM('ISTRP','ALLO','REAL',ipP,ISTORP(NSYM+1))
-            CALL PMAT_RASSCF(P,WORK(ipP))
-        Else
-            ipP = ip_Dummy
-        END IF
+            write(6,'(6x,a,i3,5x,f12.10)')
+     &          "Norm of electronic gradient for root ", i,
+     &          DNRM2_(NSXS,Work(ipB),1)
 *
-        Call FOCK(Work(ipF),Work(ipB),Work(ipFI),
-     &            Work(ipFA),Work(ipd),WORK(ipP),Work(ipQ),Work(ipPUVX),
-     &            IFINAL,C)
-        CBLBM=rtmp
-        IBLBM=itmp
-        JBLBM=jtmp
-        ISYMBB=istmp
-        IF(ISTORP(NSYM+1).GT.0)
-     &      CALL GETMEM('ISTRP','FREE','REAL',ipP,ISTORP(NSYM+1))
+            Call GetMem('TEMP','FREE','REAL',ipD,NACPAR)
+            Call GetMem('TEMP','FREE','REAL',iptuvx,idum)
+            Call GetMem('TEMP','FREE','REAL',ippuvx,idum)
+            Call GetMem('TEMP','FREE','REAL',ipFI,idum)
+            Call GetMem('TEMP','FREE','REAL',ipFA,idum)
+            Call GetMem('TEMP','FREE','REAL',ipQ,idum)
+            Call GetMem('TEMP','FREE','REAL',ipB,idum)
+            Call GetMem('TEMP','FREE','REAL',ipF,NFSIZE)
 
-        write(6,'(6x,a,i3,5x,f12.10)')
-     &      "Norm of electronic gradient for root ", i,
-     &      DNRM2_(NSXS,Work(ipB),1)
-*
-        Call GetMem('TEMP','FREE','REAL',ipD,NACPAR)
-        Call GetMem('TEMP','FREE','REAL',iptuvx,idum)
-        Call GetMem('TEMP','FREE','REAL',ippuvx,idum)
-        Call GetMem('TEMP','FREE','REAL',ipFI,idum)
-        Call GetMem('TEMP','FREE','REAL',ipFA,idum)
-        Call GetMem('TEMP','FREE','REAL',ipQ,idum)
-        Call GetMem('TEMP','FREE','REAL',ipB,idum)
-        Call GetMem('TEMP','FREE','REAL',ipF,NFSIZE)
+            ! For outctl the electronic gradient is set to the root
+            ! specified in the input, not the average!
+            if (iroot(j) == iRlxRoot) RlxGrd = DNRM2_(NSXS,Work(ipB),1)
 
-        ! For outctl the electronic gradient is set to the root
-        ! specified in the input, not the average!
-        if (iroot(j) == iRlxRoot) RlxGrd = DNRM2_(NSXS,Work(ipB),1)
-
-        else
-         Call DDaFile(JOBIPH,0,rdum,NACPAR,jDisk)
-         Call DDaFile(JOBIPH,0,rdum,NACPAR,jDisk)
-         Call DDaFile(JOBIPH,0,rdum,NACPR2,jDisk)
-         ! Call DDaFile(JOBIPH,0,rdum,NACPR2,jDisk)
-        end if
+          end if
         end do
+        if (.not. disk_pointer_moved) then
+          call ddafile(JOBIPH, 0, rdum, NACPAR, jDisk)
+          call ddafile(JOBIPH, 0, rdum, NACPAR, jDisk)
+          call ddafile(JOBIPH, 0, rdum, NACPR2, jDisk)
+          call ddafile(JOBIPH, 0, rdum, NACPR2, jDisk)
+        end if
       end do
 
 
