82c3e11f9a283c66c8ce94c3d9cba9d245514615
roland <roland.lindh@kemi.uu.se>
2023-10-26 22:31:02 +0200

    Implement implicit none.

--- src/integral_util/trgrd_alaska.f
+++ src/integral_util/trgrd_alaska.f
@@ -9,60 +9,66 @@
 ! LICENSE or in <http://www.gnu.org/licenses/>.                        *
 !                                                                      *
 ! Copyright (C) 1996, Anders Bernhardsson                              *
+!               2006, Giovanni Ghigo                                   *
 !***********************************************************************
-      SubRoutine TrGrd_Alaska(GradIn,nGrad)
-!*******************************************************************
-!                                                                  *
-!      Transforms a symmetry adapted gradient to unsymmetric  form *
-!                                                                  *
-!       Written by Anders Bernhardsson                             *
-!       960427                                                     *
-!*******************************************************************
-      use Basis_Info
-      use Center_Info
+      SubRoutine TrGrd_Alaska_(CGrad,CNames,GradIn,nGrad,iCen)
+!***********************************************************************
+!                                                                      *
+!      Transforms a symmetry adapted gradient to unsymmetric form.     *
+!                                                                      *
+!       Written by Anders Bernhardsson                                 *
+!       Adapted by Giovanni Ghigo                                      *
+!       University of Torino, July 2006                                *
+!***********************************************************************
+      Use Basis_Info
+      Use Center_Info
       use Symmetry_Info, only: nIrrep
       use Constants
       use Disp
       Implicit Real*8(a-h,o-z)
-      parameter (tol=1d-8)
+      Parameter (tol=1d-8)
 #include "Molcas.fh"
 #include "SysDef.fh"
       Real*8 CGrad(3,MxAtom)
       Real*8 GradIn(nGrad)
+      Character CNames(MxAtom)*(LENIN5)
       Logical, External :: TF
-      Integer, External :: iPrmt
 !
       mdc=0
       iIrrep=0
 !
       call dcopy_(3*MxAtom,[Zero],0,CGrad,1)
       iCen=0
-      nCnttp_Valence=0
-      Do iCnttp = 1, nCnttp
-         If (dbsc(iCnttp)%Aux) Go To 999
-         nCnttp_Valence = nCnttp_Valence+1
-      End Do
- 999  Continue
+!     nCnttp_Valence=0
+!     Do iCnttp = 1, nCnttp
+!        If (dbsc(iCnttp)%Aux) Go To 999
+!        nCnttp_Valence = nCnttp_Valence+1
+!     End Do
+!999  Continue
 !
-      Do iCnttp=1,nCnttp_Valence
-         Do iCnt=1,dbsc(iCnttp)%nCntr
-            mdc=mdc+1
-            Do iCo=0,nIrrep/dc(mdc)%nStab-1
-               kop=dc(mdc)%iCoSet(iCo,0)
-               nDispS = IndDsp(mdc,iIrrep)
-               iCen=iCen+1
-               Do iCar=0,2
-                  iComp = 2**iCar
-                  If ( TF(mdc,iIrrep,iComp)) Then
-                     nDispS = nDispS + 1
-                     XR=DBLE(iPrmt(NrOpr(kop),icomp))
-                     CGrad(iCar+1,iCen)=XR*GradIn(nDispS)
-                  End If
+      Do iCnttp=1,nCnttp
+         If (.Not.(dbsc(iCnttp)%pChrg.or.
+     &             dbsc(iCnttp)%Frag.or.
+     &             dbsc(iCnttp)%Aux)) Then
+            Do iCnt=1,dbsc(iCnttp)%nCntr
+               mdc=mdc+1
+               Do iCo=0,nIrrep/dc(mdc)%nStab-1
+                  kop=dc(mdc)%iCoSet(iCo,0)
+                  nDispS = IndDsp(mdc,iIrrep)
+                  iCen=iCen+1
+                  Do iCar=0,2
+                     iComp = 2**iCar
+                     If ( TF(mdc,iIrrep,iComp)) Then
+                        nDispS = nDispS + 1
+                        XR=DBLE(iPrmt(NrOpr(kop),icomp))
+                        CGrad(iCar+1,iCen)=XR*GradIn(nDispS)
+                     End If
+                  End Do
+                  CNames(iCen)=dc(mdc)%LblCnt
                End Do
             End Do
-         End Do
+         End If
       End Do
 !
-!     Call RecPrt('CGrad',' ',CGrad,3,iCen)
       Return
       End
