3e9a361228a3f3baaf3fc1e2c67c8142b0e7c01d
Roland Lindh <roland.lindh@kemi.uu.se>
2024-02-02 14:43:15 +0100

    Remove GETMEMs.

--- src/lucia_util/adddia_term.f
+++ src/lucia_util/adddia_term.f
@@ -32,18 +32,14 @@
 #include "cecore.fh"
 #include "cprnt.fh"
 *
-
-*#include "cstate.fh"
-
-
-
-
 *. Input
       DIMENSION CVEC(*)
 *. Output
       DIMENSION SVEC(*)
       Integer, Allocatable:: LASTR(:), LBSTR(:)
       Real*8, Allocatable:: LSCR(:), LSCR2(:)
+      Real*8, Allocatable:: LJ(:), LK(:), LXA(:), LXB(:), LRJKA(:),
+     &                      LH1D(:)
 *
       NTEST = 000
       NTEST = MAX(NTEST,IPRDIA)
@@ -54,6 +50,7 @@
       NBEL   = NELEC(IBTP)
       NOCTPA = NOCTYP(IATP)
 *
+#ifdef _DEBUGPRINT_
       IF(NTEST.GE.10) THEN
         WRITE(6,*) ' ========================='
         WRITE(6,*) '   ADDDIA_TERM for BK is speaking '
@@ -61,50 +58,50 @@
         WRITE(6,*) ' NAEL NBEL =', NAEL,NBEL
         WRITE(6,*) ' IASPGP, IBSPGP = ', IASPGP, IBSPGP
       END IF
+#endif
 *. A bit of scracth
-      CALL GETMEM('KLH1D ','ALLO','REAL',KLH1D ,NTOOB)
-      CALL GETMEM('KLJ   ','ALLO','REAL',KLJ   ,NTOOB**2)
-      CALL GETMEM('KLK   ','ALLO','REAL',KLK   ,NTOOB**2)
+      CALL mma_allocate(LH1D ,NTOOB,Label='LH1D')
+      CALL mma_allocate(LJ   ,NTOOB**2,Label='LJ')
+      CALL mma_allocate(LK   ,NTOOB**2,Label='LK')
       Call mma_allocate(LSCR2,2*NTOOB**2,Label='LSCR2')
-      CALL GETMEM('KLXA  ','ALLO','REAL',KLXA  ,NACOB)
-      CALL GETMEM('KLXB  ','ALLO','REAL',KLXB  ,NACOB)
+      CALL mma_allocate(LXA  ,NACOB,Label='LXA')
+      CALL mma_allocate(LXB  ,NACOB,Label='LXB')
       Call mma_allocate(LSCR,2*NACOB,Label='LSCR')
 *. Space for blocks of strings
       Call mma_allocate(LASTR,MXNSTR*NAEL,Label='LASTR')
       Call mma_allocate(LBSTR,MXNSTR*NBEL,Label='LBSTR')
 
       MAXA = IMNMX(IWORK(KNSTSO(IATP)),NSMST*NOCTPA,2)
-      CALL GETMEM('KLRJKA','ALLO','REAL',KLRJKA,MAXA)
+      CALL mma_allocate(LRJKA,MAXA,Label='LRJKA')
 *. Diagonal of one-body integrals and coulomb and exchange integrals
 *. Integrals assumed in place so :
-      CALL GT1DIA(WORK(KLH1D))
-      CALL GTJK(WORK(KLJ),WORK(KLK),NTOOB,LSCR2,IREOTS,IREOST)
+      CALL GT1DIA(LH1D)
+      CALL GTJK(LJ,LK,NTOOB,LSCR2,IREOTS,IREOST)
 *. Core energy not included
       ECOREP = 0.0D0
-      CALL GTJK(WORK(KLJ),WORK(KLK),NTOOB,LSCR2,IREOTS,IREOST)
+      CALL GTJK(LJ,LK,NTOOB,LSCR2,IREOTS,IREOST)
 *
       SHIFT = ECORE_ORIG-ECORE
       FACTORX = FACTOR + SHIFT
 *
       CALL ADDDIA_TERMS(NAEL,LASTR,NBEL,LBSTR,
-     &             NACOB,CVEC,SVEC,NSMST,WORK(KLH1D),
-     &             WORK(KLXA),WORK(KLXB),LSCR,WORK(KLJ),
-     &             WORK(KLK),IWORK(KNSTSO(IATP)),IWORK(KNSTSO(IBTP)),
+     &             NACOB,CVEC,SVEC,NSMST,LH1D,
+     &             LXA,LXB,LSCR,LJ,
+     &             LK,IWORK(KNSTSO(IATP)),IWORK(KNSTSO(IBTP)),
      &             ECOREP,
      &             IPRDIA,NTOOB,
-     &             WORK(KLRJKA),
+     &             LRJKA,
      &             IASPGP,IASM,IBSPGP,IBSM,FACTORX)
 *.Flush local memory
-      CALL GETMEM('KLH1D ','FREE','REAL',KLH1D ,NTOOB)
-      CALL GETMEM('KLJ   ','FREE','REAL',KLJ   ,NTOOB**2)
-      CALL GETMEM('KLK   ','FREE','REAL',KLK   ,NTOOB**2)
+      CALL mma_deallocate(LH1D)
+      CALL mma_deallocate(LJ)
+      CALL mma_deallocate(LK)
       Call mma_deallocate(LSCR2)
-      CALL GETMEM('KLXA  ','FREE','REAL',KLXA  ,NACOB)
-      CALL GETMEM('KLXB  ','FREE','REAL',KLXB  ,NACOB)
+      CALL mma_deallocate(LXA)
+      CALL mma_deallocate(LXB)
       Call mma_deallocate(LSCR)
       Call mma_deallocate(LASTR)
       Call mma_deallocate(LBSTR)
-      CALL GETMEM('KLRJKA','FREE','REAL',KLRJKA,MAXA)
+      CALL mma_deallocate(LRJKA)
 
-      RETURN
       END
