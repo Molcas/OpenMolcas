1263271534b05067508a02384862962f6e242da4
Roland Lindh <roland.lindh@kemi.uu.se>
2024-02-14 15:30:15 +0100

    Prep.

--- src/rasscf/rd2int_rasscf.f
+++ src/rasscf/rd2int_rasscf.f
@@ -10,7 +10,7 @@
 *                                                                      *
 * Copyright (C) 1993, Markus P. Fuelscher                              *
 ************************************************************************
-      Subroutine Rd2Int_RASSCF
+      Subroutine Rd2Int_RASSCF()
 ************************************************************************
 *                                                                      *
 *     Read header of the two-electron integral file                    *
@@ -26,50 +26,47 @@
 *     history: none                                                    *
 *                                                                      *
 ************************************************************************
-      Implicit Real*8 (A-H,O-Z)
-#include "rasdim.fh"
+      use Definitions, only: LF => u6
+      use UnixInfo, only: ProgName
+      use General_data
+      Implicit None
 #include "rasscf.fh"
-#include "general.fh"
-#include "output_ras.fh"
-      Integer nSymX,nBasX(8)
+      Integer nSymX,nBasX(8),iRc,i,IERR,iSym
 *----------------------------------------------------------------------*
 *     Start                                                            *
 *----------------------------------------------------------------------*
       iRc=-1
       Call GetOrd(iRc,lSquare,nSymX,nBasX,nSkipX)
-      If (iRc.ne.0) Then
+      If (iRc/=0) Then
         Write(LF,*)'RD2INT Error: Failed to read from ORDINT file.'
-        Write(LF,*)'RASSCF tried to read two-electron integrals from'
+        Write(LF,*) Progname,
+     &            ' tried to read two-electron integrals from'
         Write(LF,*)'the ORDINT file, but failed. Something is wrong'
         Write(LF,*)'with the file. Perhaps it is missing?'
-*        Call Quit(20)
         Call Quit_OnUserError()
       End If
-      If ( nSymX.ne.nSym ) Then
+      If ( nSymX/=nSym ) Then
         Write(LF,*)'RD2INT Error: Wrong size of symmetry group.'
-        Write(LF,*)'RASSCF tried to use two-electron integrals from'
+        Write(LF,*)ProgName,' tried to use two-electron integrals from'
         Write(LF,*)'a file that was evidently created for some other'
         Write(LF,*)'program run.'
         Write(LF,'(1x,a,2i8)')'nSymX,nSym:',nSymX,nSym
-*        Call Quit(20)
         Call Quit_OnUserError()
       End If
       IERR=0
       Do iSym=1,nSym
          If ( nBas(iSym).ne.nBasX(iSym) ) IERR=1
       End Do
-      If ( IERR.eq.1 ) Then
+      If ( IERR==1 ) Then
         Write(LF,*)'RD2INT Error: Wrong nr of basis functions.'
         Write(LF,*)'RASSCF tried to use two-electron integrals from'
         Write(LF,*)'a file that was evidently created for some other'
         Write(LF,*)'program run.'
         Write(LF,'(1x,a,8i8)')'nBas :',(nBas(i),i=1,nSym)
         Write(LF,'(1x,a,8i8)')'nBasX:',(nBasX(i),i=1,nSym)
-*        Call Quit(20)
         Call Quit_OnUserError()
       End If
 *----------------------------------------------------------------------*
 *     Exit                                                             *
 *----------------------------------------------------------------------*
-      Return
-      End
+      End Subroutine Rd2Int_RASSCF
