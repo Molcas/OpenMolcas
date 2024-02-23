2e49be79c1a5e29f0f2dc7c91a4f46155120524d
Roland Lindh <roland.lindh@kemi.uu.se>
2024-02-23 10:27:13 +0100

    Cleanup.

--- src/rassi/sginit.F90
+++ src/rassi/sginit.F90
@@ -1,27 +1,28 @@
-************************************************************************
-* This file is part of OpenMolcas.                                     *
-*                                                                      *
-* OpenMolcas is free software; you can redistribute it and/or modify   *
-* it under the terms of the GNU Lesser General Public License, v. 2.1. *
-* OpenMolcas is distributed in the hope that it will be useful, but it *
-* is provided "as is" and without any express or implied warranties.   *
-* For more details see the full text of the license in the file        *
-* LICENSE or in <http://www.gnu.org/licenses/>.                        *
-************************************************************************
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!***********************************************************************
       Subroutine SGInit(nSym,nActEl,iSpin,nRasPrt,nRas,nRasEl,SGS)
       use stdalloc, only: mma_allocate, mma_deallocate
       use Struct, only: LEVEL, SGStruct
-      IMPLICIT REAL*8 (A-H,O-Z)
+      IMPLICIT None
 #include "rassi.fh"
       Integer nSym, nActEl, iSpin, nRasPrt
       Type (SGStruct) SGS
       Integer nRas(8,nRasPrt),nRasEl(nRasPrt)
 
-      Integer, Allocatable:: DRT0(:), Down0(:), Tmp(:), Lim(:), NWV(:),
-     &                       DAW(:), RAW(:)
+      Integer, Allocatable:: DRT0(:), Down0(:), Tmp(:), Lim(:), NWV(:), DAW(:), RAW(:)
+      Integer IA0,IB0,IC0,IAC,iErr,iLev,iRO,iSy,iSym,it,iTabs,MidLev,  &
+     &        MVSta,MVEnd,NDown0,nDrt0,nLev,nTmp,nVert0,Lev,nVert
 
       NLEV=NASHT
-C Allocate Level to Symmetry table ISm:
+! Allocate Level to Symmetry table ISm:
       Call mma_allocate(SGS%ISm,nLev,Label='SGS%ISm')
       ITABS=0
       IT=0
@@ -33,7 +34,7 @@ C Allocate Level to Symmetry table ISm:
         END DO
       END DO
 
-C Compute size of unrestricted DRT table:
+! Compute size of unrestricted DRT table:
       ib0=ispin-1
       ia0=(nActEl-ib0)/2
       ic0=nLev-ia0-ib0
@@ -55,21 +56,20 @@ C Compute size of unrestricted DRT table:
 
       IAC=MIN(IA0,IC0)
       NVERT0=((IA0+1)*(IC0+1)*(2*IB0+IAC+2))/2-(IAC*(IAC+1)*(IAC+2))/6
-CTEST      write(*,*)' NVERT0:',NVERT0
       NDRT0=5*NVERT0
       NDOWN0=4*NVERT0
 
-C Compute unrestricted DRT tables:
+! Compute unrestricted DRT tables:
       CALL mma_allocate(DRT0,NDRT0,Label='DRT0')
       CALL mma_allocate(DOWN0,NDOWN0,Label='DOWN0')
       NTMP=((NLEV+1)*(NLEV+2))/2
       CALL mma_allocate(TMP,NTMP,Label='TMP')
       CALL mkDRT0 (IA0,IB0,IC0,NVERT0,DRT0,DOWN0,NTMP,TMP)
 
-C Construct a restricted graph.
+! Construct a restricted graph.
       Call mma_allocate(Lim,nLev,Label='Lim')
       Lim(:)=0
-C Fill in the occupation limit table:
+! Fill in the occupation limit table:
       Lev=0
       Do iRO=1,nRasPrt
         Do iSy=1,nSym
@@ -88,27 +88,26 @@ C Fill in the occupation limit table:
       Call mma_deallocate(DRT0)
       Call mma_deallocate(Down0)
 
-C Direct Arc Weights table and Level-To-Vertex table:
+! Direct Arc Weights table and Level-To-Vertex table:
       Call mma_allocate(DAW,5*nVert,Label='DAW')
       Call mma_allocate(SGS%LTV,nLev+2,Label='SGS%LTV')
       Call MkDAW_RASSI(nLev,nVert,SGS%DRT,SGS%Down,DAW,SGS%LTV)
 
-C Upchain Index table:
+! Upchain Index table:
       Call mma_allocate(SGS%Up,4*nVert,Label='SGS%Up')
-C Reverse Arc Weights table:
+! Reverse Arc Weights table:
       Call mma_allocate(RAW,5*nVert,Label='RAW')
-C Modified Arc Weights table:
+! Modified Arc Weights table:
       Call mma_allocate(SGS%MAW,4*nVert,Label='SGS%MAW')
-      Call MkMAW(nLev,nVert,SGS%Down,DAW,SGS%Up,
-     &           RAW,SGS%MAW,SGS%LTV,MidLev)
+      Call MkMAW(nLev,nVert,SGS%Down,DAW,SGS%Up,RAW,SGS%MAW,SGS%LTV,MidLev)
       MVSta=SGS%LTV(2+MidLev)
       MVEnd=SGS%LTV(1+MidLev)-1
-C The DAW, RAW tables are no longer needed:
+! The DAW, RAW tables are no longer needed:
       CALL mma_deallocate(RAW)
       CALL mma_deallocate(DAW)
       CALL mma_deallocate(TMP)
 
-C Put sizes and addresses in structure SGS:
+! Put sizes and addresses in structure SGS:
 
       SGS%nLev   =nLev
       SGS%nVert  =nVert
