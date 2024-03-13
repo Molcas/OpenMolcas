1835fb2ac15a9400ff5fe394b0425da6b15d4a25
Roland Lindh <roland.lindh@kemi.uu.se>
2024-03-13 15:33:48 +0100

    Error?.

--- src/molcas_ci_util/check_invec.F90
+++ src/molcas_ci_util/check_invec.F90
@@ -1,17 +1,20 @@
-************************************************************************
-* This file is part of OpenMolcas.                                     *
-*                                                                      *
-* OpenMolcas is free software; you can redistribute it and/or modify   *
-* it under the terms of the GNU Lesser General Public License, v. 2.1. *
-* OpenMolcas is distributed in the hope that it will be useful, but it *
-* is provided "as is" and without any express or implied warranties.   *
-* For more details see the full text of the license in the file        *
-* LICENSE or in <http://www.gnu.org/licenses/>.                        *
-************************************************************************
-* PAM2009 Who wrote this? What purpose?
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!***********************************************************************
+! PAM2009 Who wrote this? What purpose?
       Subroutine Check_InVec(InVec)
 #ifdef _MOLCAS_MPP_
       Use Para_Info, Only: nProcs, Is_Real_Par
+      Implicit None
+      Integer, Intent(in):: InVec
+
       If (.Not.Is_Real_Par()) Return
       InVec_Tot = InVec
       Call GAIGOP_SCAL(InVec_Tot,'+')
@@ -29,7 +32,7 @@
          End If
       End If
 #else
-c Avoid unused argument warnings
+! Avoid unused argument warnings
 #ifdef _WARNING_WORKAROUND_
       If (.False.) Call Unused_integer(InVec)
 #endif
