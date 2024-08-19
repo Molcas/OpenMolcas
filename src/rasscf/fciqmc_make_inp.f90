240999da3ad47d3888806e05e2b23af09fe6cbb2
Oskar Weser <oskar.weser@gmail.com>
2024-08-19 11:10:04 +0200

    trying to fix license header

--- src/rasscf/fciqmc_make_inp.f90
+++ src/rasscf/fciqmc_make_inp.f90
@@ -1,16 +1,16 @@
 !***********************************************************************
 ! This file is part of OpenMolcas.                                     *
 !                                                                      *
-! OpenMolcas is free software; you can redistribute it and / or modify *
-! it under the terms of the GNU Lesser General Public License, v.2.1.  *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
 ! OpenMolcas is distributed in the hope that it will be useful, but it *
-! is provided "as is"and without any express or implied warranties.    *
+! is provided "as is" and without any express or implied warranties.   *
 ! For more details see the full text of the license in the file        *
-! LICENSE or in < http://www.gnu.org / licenses /  > .                 *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
 !                                                                      *
-! Copyright(C) 2015, Giovanni Li Manni                                 *
-!              2019, 2024, Oskar Weser                                 *
-!              2021, Werner Dobrautz                                   *
+! Copyright (C) 2015, Giovanni Li Manni                                *
+!               2019, 2024, Oskar Weser                                *
+!               2021, Werner Dobrautz                                  *
 !***********************************************************************
 
 module fciqmc_make_inp
