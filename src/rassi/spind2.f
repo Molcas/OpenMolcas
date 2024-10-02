fb3737dba0a26bbd34720190af7d5302b3d8c7ef
Roland Lindh <roland.lindh@kemi.uu.se>
2024-10-02 11:45:11 +0200

    Cleanup.

--- src/rassi/spind2.f
+++ src/rassi/spind2.f
@@ -10,27 +10,31 @@
 ************************************************************************
       SUBROUTINE SPIND2(ISYOP,MS2OP,IORBTAB,ISSTAB,IFSBTAB1,IFSBTAB4,
      &                 PSI1,PSI4,SPD2)
+      use stdalloc, only: mma_allocate, mma_deallocate
       IMPLICIT NONE
-      REAL*8 PSI1(*),PSI4(*),SPD2(*)
-C     REAL*8 COEFF,OVERLAP,OVLP,PRTHR
-      REAL*8 COEFF,OVERLAP_RASSI,OVLP
-      INTEGER IORBTAB(*),NASORB
+      INTEGER ISYOP,MS2OP
+      INTEGER IORBTAB(*)
       INTEGER ISSTAB(*)
       INTEGER IFSBTAB1(*),IFSBTAB4(*)
+      REAL*8 PSI1(*),PSI4(*),SPD2(*)
+
+      REAL*8 COEFF,OVLP
+      INTEGER NASORB
       INTEGER FSBOP,IMODE,ISORB
       INTEGER LFSBANN1,LFSBANN4
       INTEGER ND1,ND2,ND3,ND4
-      INTEGER JSORB,LANN1,LANN2
-      INTEGER ISYOP,MS2OP,KOINFO
+      INTEGER JSORB
+      INTEGER KOINFO
       INTEGER ISMLAB,ISPLAB,JSMLAB,JSPLAB
       INTEGER NASGEM,JISORB,LFSBANN2
-      INTEGER LFSBANN3,LANN3,LANN4,LSORB,KSORB
+      INTEGER LFSBANN3,LSORB,KSORB
       INTEGER KLSORB,KLSYM,KLMS2,LSMLAB,LSPLAB,KSMLAB
       INTEGER KSPLAB,IJKL
 #include "SysDef.fh"
 #include "WrkSpc.fh"
 #include "symmul.fh"
-      EXTERNAL OVERLAP_RASSI
+      Real*8, EXTERNAL:: OVERLAP_RASSI
+      Real*8, Allocatable:: ANN1(:), ANN2(:), ANN3(:), ANN4(:)
 
 C Nr of active spin-orbitals
       NASORB= IORBTAB(4)
@@ -47,13 +51,13 @@ C Annihilate a single spin orbital, ISORB:
        LFSBANN1=FSBOP(IMODE,ISORB,IORBTAB,ISSTAB,IFSBTAB1)
        ND1=IWORK(LFSBANN1+4)
        COEFF=1.0D0
-       CALL GETMEM('ANN1','Allo','Real',LANN1,ND1)
-       CALL DCOPY_(ND1,[0.0D0],0,WORK(LANN1),1)
+       CALL mma_allocate(ANN1,ND1,Label='ANN1')
+       ANN1(:)=0.0D0
        CALL PRIMSGM(IMODE,ISORB,IORBTAB,ISSTAB,IWORK(LFSBANN1),
-     &                   IFSBTAB1,COEFF,WORK(LANN1),PSI1)
+     &                   IFSBTAB1,COEFF,ANN1,PSI1)
 CTEST       WRITE(*,*)' The ANN1 wave function, with ISORB=',ISORB
 CTEST       PRTHR=0.01D0
-CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN1),PRTHR,WORK(LANN1))
+CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN1),PRTHR,ANN1)
        DO JSORB=1,ISORB-1
 C Symmetry properties:
         JSMLAB=IORBTAB(KOINFO+1+8*(JSORB-1))
@@ -64,13 +68,13 @@ C Annihilate once more, the spin orbital JSORB:
         IMODE=-1
         LFSBANN2=FSBOP(IMODE,JSORB,IORBTAB,ISSTAB,IWORK(LFSBANN1))
         ND2=IWORK(LFSBANN2+4)
-        CALL GETMEM('ANN2','Allo','Real',LANN2,ND2)
-        CALL DCOPY_(ND2,[0.0D0],0,WORK(LANN2),1)
+        CALL mma_allocate(ANN2,ND2,Label='ANN2')
+        ANN2(:)=0.0D0
         CALL PRIMSGM(IMODE,JSORB,IORBTAB,ISSTAB,IWORK(LFSBANN2),
-     &                IWORK(LFSBANN1),COEFF,WORK(LANN2),WORK(LANN1))
+     &                IWORK(LFSBANN1),COEFF,ANN2,ANN1)
 CTEST       WRITE(*,*)' The ANN2 wave function, with JSORB=',JSORB
 CTEST       PRTHR=0.01D0
-CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN2),PRTHR,WORK(LANN2))
+CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN2),PRTHR,ANN2)
 
         KLSYM=MUL(MUL(ISMLAB,JSMLAB),ISYOP)
         KLMS2=MS2OP+ISPLAB+JSPLAB
@@ -83,13 +87,13 @@ C Annihilate a single spin orbital, LSORB:
          LFSBANN4=FSBOP(IMODE,LSORB,IORBTAB,ISSTAB,IFSBTAB4)
          ND4=IWORK(LFSBANN4+4)
          COEFF=1.0D0
-         CALL GETMEM('ANN4','Allo','Real',LANN4,ND4)
-         CALL DCOPY_(ND4,[0.0D0],0,WORK(LANN4),1)
+         CALL mma_allocate(ANN4,ND4,Label='ANN4')
+         ANN4(:)=0.0D0
          CALL PRIMSGM(IMODE,LSORB,IORBTAB,ISSTAB,IWORK(LFSBANN4),
-     &                   IFSBTAB4,COEFF,WORK(LANN4),PSI4)
+     &                   IFSBTAB4,COEFF,ANN4,PSI4)
 CTEST       WRITE(*,*)' The ANN4 wave function, with LSORB=',LSORB
 CTEST       PRTHR=0.01D0
-CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN4),PRTHR,WORK(LANN4))
+CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN4),PRTHR,ANN4)
          DO KSORB=1,LSORB-1
           OVLP=0.0D0
 C Symmetry properties:
@@ -103,29 +107,29 @@ C Annihilate once more, the spin orbital KSORB:
           IMODE=-1
           LFSBANN3=FSBOP(IMODE,KSORB,IORBTAB,ISSTAB,IWORK(LFSBANN4))
           ND3=IWORK(LFSBANN3+4)
-          CALL GETMEM('ANN3','Allo','Real',LANN3,ND3)
-          CALL DCOPY_(ND3,[0.0D0],0,WORK(LANN3),1)
+          CALL mma_allocate(ANN3,ND3,Label='ANN3')
+          ANN3(:)=0.0D0
           CALL PRIMSGM(IMODE,KSORB,IORBTAB,ISSTAB,IWORK(LFSBANN3),
-     &                IWORK(LFSBANN4),COEFF,WORK(LANN3),WORK(LANN4))
+     &                IWORK(LFSBANN4),COEFF,ANN3,ANN4)
 CTEST       WRITE(*,*)' The ANN3 wave function, with KSORB=',KSORB
 CTEST       PRTHR=0.01D0
-CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN3),PRTHR,WORK(LANN3))
+CTEST       CALL PRWVF(IORBTAB,ISSTAB,IWORK(LFSBANN3),PRTHR,ANN3)
 C Compute the spin transition density matrix element:
           OVLP=OVERLAP_RASSI(IWORK(LFSBANN2),
-     &                  IWORK(LFSBANN3),WORK(LANN2),WORK(LANN3))
+     &                  IWORK(LFSBANN3),ANN2,ANN3)
 CTEST       write(*,*)' Their overlap:',OVLP
             IJKL=JISORB+NASGEM*(KLSORB-1)
             SPD2(IJKL)=SPD2(IJKL)+OVLP
-            CALL GETMEM('ANN3','Free','Real',LANN3,ND3)
+            Call mma_deallocate(ANN3)
             CALL KILLOBJ(LFSBANN3)
          END DO
-         CALL GETMEM('ANN4','Free','Real',LANN4,ND4)
+         Call mma_deallocate(ANN4)
          CALL KILLOBJ(LFSBANN4)
         END DO
-        CALL GETMEM('ANN2','Free','Real',LANN2,ND2)
+        Call mma_deallocate(ANN2)
         CALL KILLOBJ(LFSBANN2)
        END DO
-       CALL GETMEM('ANN1','Free','Real',LANN1,ND1)
+       Call mma_deallocate(ANN1)
        CALL KILLOBJ(LFSBANN1)
       END DO
 
