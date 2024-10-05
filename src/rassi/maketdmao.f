b0f8731b8d64a76c6036c8f20796d634fc3897ee
Roland Lindh <roland.lindh@kemi.uu.se>
2024-10-05 17:08:39 +0200

    Cleanup.

--- src/rassi/maketdmao.f
+++ src/rassi/maketdmao.f
@@ -31,7 +31,6 @@
 #include "rassi.fh"
 #include "symmul.fh"
 #include "Files.fh"
-#include "WrkSpc.fh"
       CHARACTER(LEN=8) CHARTYPE!,LABEL
       Integer NSS
       Real*8 USOR(NSS,NSS),USOI(NSS,NSS)
@@ -52,6 +51,7 @@
       End Type A2_Array
       Type (A2_array)  :: pZMR(3), pZMI(3)
       Type (A2_array)  :: pZMR2(3), pZMI2(3)
+      Real*8, allocatable:: TMPR(:), TMPI(:), SCR(:), TDMZZ(:)
 
       nbsts=nbst**2
 
@@ -99,9 +99,9 @@ C (see prprop.f and others)
 c Allocate some arrays
 c SDMXR, etc      DM/TDM for this iteration
 c SDMXR2, etc     Accumulated DM/TDM
-c LTMPR,I          Temporary array for U*AU multiplication
-c LTDMZZ           DM/TDM as read from file
-c LSCR             Scratch for expansion of LTDMZZ
+c TMPR,I          Temporary array for U*AU multiplication
+c TDMZZ           DM/TDM as read from file
+c SCR             Scratch for expansion of TDMZZ
       CALL mma_allocate(SDMXR,nbsts,Label='SDMXR')
       CALL mma_allocate(SDMXI,nbsts,Label='SDMXI')
       CALL mma_allocate(SDMYR,nbsts,Label='SDMYR')
@@ -140,17 +140,16 @@ c LSCR             Scratch for expansion of LTDMZZ
       pZMI2(2)%A2=>SDMYI2(:)
       pZMI2(3)%A2=>SDMZI2(:)
 
-      CALL GETMEM('TSDMTMPR','ALLO','REAL',LTMPR,nbsts)
-      CALL GETMEM('TSDMTMPI','ALLO','REAL',LTMPI,nbsts)
-      CALL DCOPY_(nbsts,[0.0D00],0,WORK(LTMPR),1)
-      CALL DCOPY_(nbsts,[0.0D00],0,WORK(LTMPI),1)
+      CALL mma_allocate(TMPR,nbsts,Label='TMPR')
+      CALL mma_allocate(TMPI,nbsts,Label='TMPI')
+      TMPR(:)=0.0D0
+      TMPI(:)=0.0D0
 
-      CALL GETMEM('TDMSCR','ALLO','REAL',LSCR,nbsts)
+      CALL mma_allocate(SCR,nbsts,Label='SCR')
 c zeroed inside the loop
-c      CALL DCOPY_(nbsts,0.0D00,0,WORK(LSCR),1)
 
-      CALL GETMEM('TDMZZ','ALLO','REAL',LTDMZZ,NTDMZZ)
-      CALL DCOPY_(NTDMZZ,[0.0D00],0,WORK(LTDMZZ),1)
+      CALL mma_allocate(TDMZZ,NTDMZZ,Label='TDMZZ')
+      TDMZZ(:)=0.0D0
 
 CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC
 C MAIN LOOP OVER KSF/LSF
@@ -189,7 +188,7 @@ CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCc
 C Transition density matrices, TDMZZ, in AO basis.
 C WDMZZ similar, but WE-reduced 'triplet' densities.
 C TDMZZ will store either, depending on the type
-        CALL DCOPY_(NTDMZZ,[0.0D00],0,WORK(LTDMZZ),1)
+        TDMZZ(:)=0.0D0
 
 
 ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc
@@ -201,25 +200,25 @@ c so swap if needed
         iOpt=2
         IF (ITYPE.GE.3) Then
            iGo=4
-           CALL dens2file(Work(LTDMZZ),Work(LTDMZZ),Work(LTDMZZ),
+           CALL dens2file(TDMZZ,TDMZZ,TDMZZ,
      &                    nTDMZZ,LUTDM,IDISK,iEmpty,iOpt,iGo,KSF,LSF)
 C NOTE-the TD matrix as read in has an incorrect sign
-           CALL DSCAL_(NTDMZZ,-1.0d0,WORK(LTDMZZ),1)
+           CALL DSCAL_(NTDMZZ,-1.0d0,TDMZZ,1)
         Else
            iGo=1
-           CALL dens2file(Work(LTDMZZ),Work(LTDMZZ),Work(LTDMZZ),
+           CALL dens2file(TDMZZ,TDMZZ,TDMZZ,
      &                    nTDMZZ,LUTDM,IDISK,iEmpty,iOpt,iGo,KSF,LSF)
         END IF
 
 
 c Anti-hermitian properties need a little fixing
         IF((ITYPE.EQ.2.OR.ITYPE.EQ.4).AND.(KSF.LE.LSF))
-     &          CALL DSCAL_(NTDMZZ,-1.0d0,WORK(LTDMZZ),1)
+     &          CALL DSCAL_(NTDMZZ,-1.0d0,TDMZZ,1)
 
 
 C CALCULATE THE SYMMETRIC AND ANTISYMMETRIC FOLDED TRANS D MATRICES
 C AND SIMILAR WE-REDUCED SPIN DENSITY MATRICES
-        CALL DCOPY_(nbsts,[0.0D00],0,WORK(LSCR),1)
+        SCR(:)=0.0D0
 cccccccccccccc
 c nbsq and NTDMZZ are the same, but they are the
 c sizes of the symmetry-adapted matrices
@@ -245,10 +244,9 @@ cc Expand into C1
               Do J=NB2_i+1,NB2_f
                 Do I=NB1_i+1,NB1_f
                   ITD=ITD+1
-c                  TDM=WORK(LTDMZZSCR-1+ITD)
-                  TDM=WORK(LTDMZZ-1+ITD)
+                  TDM=TDMZZ(ITD)
                   IJ=I+NBST*(J-1)
-                  WORK(LSCR-1+IJ)=WORK(LSCR-1+IJ)+TDM
+                  SCR(IJ)=SCR(IJ)+TDM
                 Enddo
               Enddo
             Endif
@@ -283,24 +281,24 @@ c ie, see how AMFI is processed in soeig.f
         IF((ITYPE.EQ.1.OR.ITYPE.EQ.2)
      &          .AND.MPLETK.EQ.MPLETL
      &          .AND.MSPROJK.EQ.MSPROJL) THEN
-          CALL DAXPY_(nbsts,1.0d0,WORK(LSCR),1,SDMZR,1)
+          CALL DAXPY_(nbsts,1.0d0,SCR,1,SDMZR,1)
         ELSE IF(ITYPE.EQ.3.OR.ITYPE.EQ.4) THEN
           If (iOpt.eq.1) Then
-          CALL DAXPY_(nbsts,CGX*ROTMAT(1,1),WORK(LSCR),1,SDMXR,1)
-          CALL DAXPY_(nbsts,CGY*ROTMAT(2,1),WORK(LSCR),1,SDMXI,1)
-          CALL DAXPY_(nbsts,CG0*ROTMAT(3,1),WORK(LSCR),1,SDMXR,1)
+          CALL DAXPY_(nbsts,CGX*ROTMAT(1,1),SCR,1,SDMXR,1)
+          CALL DAXPY_(nbsts,CGY*ROTMAT(2,1),SCR,1,SDMXI,1)
+          CALL DAXPY_(nbsts,CG0*ROTMAT(3,1),SCR,1,SDMXR,1)
 
-          CALL DAXPY_(nbsts,CGX*ROTMAT(1,2),WORK(LSCR),1,SDMYR,1)
-          CALL DAXPY_(nbsts,CGY*ROTMAT(2,2),WORK(LSCR),1,SDMYI,1)
-          CALL DAXPY_(nbsts,CG0*ROTMAT(3,2),WORK(LSCR),1,SDMYR,1)
+          CALL DAXPY_(nbsts,CGX*ROTMAT(1,2),SCR,1,SDMYR,1)
+          CALL DAXPY_(nbsts,CGY*ROTMAT(2,2),SCR,1,SDMYI,1)
+          CALL DAXPY_(nbsts,CG0*ROTMAT(3,2),SCR,1,SDMYR,1)
 
-          CALL DAXPY_(nbsts,CGX*ROTMAT(1,3),WORK(LSCR),1,SDMZR,1)
-          CALL DAXPY_(nbsts,CGY*ROTMAT(2,3),WORK(LSCR),1,SDMZI,1)
-          CALL DAXPY_(nbsts,CG0*ROTMAT(3,3),WORK(LSCR),1,SDMZR,1)
+          CALL DAXPY_(nbsts,CGX*ROTMAT(1,3),SCR,1,SDMZR,1)
+          CALL DAXPY_(nbsts,CGY*ROTMAT(2,3),SCR,1,SDMZI,1)
+          CALL DAXPY_(nbsts,CG0*ROTMAT(3,3),SCR,1,SDMZR,1)
           Else
-          CALL DAXPY_(nbsts,CGX,WORK(LSCR),1,SDMXR,1)
-          CALL DAXPY_(nbsts,CGY,WORK(LSCR),1,SDMYI,1)
-          CALL DAXPY_(nbsts,CG0,WORK(LSCR),1,SDMZR,1)
+          CALL DAXPY_(nbsts,CGX,SCR,1,SDMXR,1)
+          CALL DAXPY_(nbsts,CGY,SCR,1,SDMYI,1)
+          CALL DAXPY_(nbsts,CG0,SCR,1,SDMZR,1)
           End If
         END IF
 
@@ -315,20 +313,20 @@ c when doing DAXPY
         UIL=USOI(KSS,ASS)
 
         DO IDIR=1,3
-          CALL DCOPY_(nbsts,[0.0D00],0,WORK(LTMPR),1)
-          CALL DCOPY_(nbsts,[0.0D00],0,WORK(LTMPI),1)
+          TMPR(:)=0.0D0
+          TMPI(:)=0.0D0
 
 C right side
-          CALL DAXPY_(nbsts,       URR,pZMR(IDIR)%A2,1,WORK(LTMPR),1)
-          CALL DAXPY_(nbsts,-1.0d0*UIR,pZMI(IDIR)%A2,1,WORK(LTMPR),1)
-          CALL DAXPY_(nbsts,       UIR,pZMR(IDIR)%A2,1,WORK(LTMPI),1)
-          CALL DAXPY_(nbsts,       URR,pZMI(IDIR)%A2,1,WORK(LTMPI),1)
+          CALL DAXPY_(nbsts,       URR,pZMR(IDIR)%A2,1,TMPR,1)
+          CALL DAXPY_(nbsts,-1.0d0*UIR,pZMI(IDIR)%A2,1,TMPR,1)
+          CALL DAXPY_(nbsts,       UIR,pZMR(IDIR)%A2,1,TMPI,1)
+          CALL DAXPY_(nbsts,       URR,pZMI(IDIR)%A2,1,TMPI,1)
 
 C left side
-         CALL DAXPY_(nbsts,       URL,WORK(LTMPR),1,pZMR2(IDIR)%A2,1)
-         CALL DAXPY_(nbsts,       UIL,WORK(LTMPI),1,pZMR2(IDIR)%A2,1)
-         CALL DAXPY_(nbsts,       URL,WORK(LTMPI),1,pZMI2(IDIR)%A2,1)
-         CALL DAXPY_(nbsts,-1.0d0*UIL,WORK(LTMPR),1,pZMI2(IDIR)%A2,1)
+         CALL DAXPY_(nbsts,       URL,TMPR,1,pZMR2(IDIR)%A2,1)
+         CALL DAXPY_(nbsts,       UIL,TMPI,1,pZMR2(IDIR)%A2,1)
+         CALL DAXPY_(nbsts,       URL,TMPI,1,pZMI2(IDIR)%A2,1)
+         CALL DAXPY_(nbsts,-1.0d0*UIL,TMPR,1,pZMI2(IDIR)%A2,1)
         END DO
 cccccccccccccccccccccc
 c END SPINORBIT STUFF
@@ -363,8 +361,8 @@ C Store this density to DENSOUT
       END IF
 
 c Free memory
-      CALL GETMEM('TDMSCR','FREE','REAL',LSCR,nbsts)
-      CALL GETMEM('TDMZZ','FREE','REAL',LTDMZZ,NTDMZZ)
+      CALL mma_deallocate(SCR)
+      CALL mma_deallocate(TDMZZ)
 
       pZMI(3)%A2=>Null()
       pZMI(2)%A2=>Null()
@@ -379,8 +377,8 @@ c Free memory
       Call mma_deallocate(SDMXI)
       Call mma_deallocate(SDMXR)
 
-      CALL GETMEM('TSDMTMPR','FREE','REAL',LTMPR,nbsts)
-      CALL GETMEM('TSDMTMPI','FREE','REAL',LTMPI,nbsts)
+      Call mma_deallocate(TMPI)
+      Call mma_deallocate(TMPR)
 
       pZMI2(3)%A2=>Null()
       pZMI2(2)%A2=>Null()
