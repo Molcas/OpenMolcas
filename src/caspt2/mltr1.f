f7be439172259fc3128fcac75f64eca70aec95d5
Roland Lindh <roland.lindh@kemi.uu.se>
2024-10-23 14:36:28 +0200

    Cleanup.

--- src/caspt2/mltr1.f
+++ src/caspt2/mltr1.f
@@ -241,3 +241,71 @@ C F(L2,p) := Add V*X(L1,p,q)*Y(L3,q)
 
       RETURN
       END
+
+      SUBROUTINE PMLTR1 (KOD,IMLTOP,LST1,
+     &                   X,NAS1,NIS1,JXOFF,
+     &                   F,NFI,NFJ,
+     &                   lg_Y,NAS2,NIS2)
+#ifdef _MOLCAS_MPP_
+      USE Para_Info, ONLY: Is_Real_Par
+#endif
+      use Sigma_data
+      use fake_GA, only: GA_Arrays
+      IMPLICIT REAL*8 (A-H,O-Z)
+#ifdef _MOLCAS_MPP_
+#include "global.fh"
+#include "mafdecls.fh"
+#endif
+      INTEGER KOD, IMLTOP
+      INTEGER LST1(4,NLST1)
+      REAL*8 X(*)
+      INTEGER NAS1,NIS1,JXOFF, NFI,NFJ, lg_Y, NAS2, NIS2
+      REAL*8 F(NFI,NFJ)
+
+#ifdef _MOLCAS_MPP_
+C SVC: Determine the index ranges of the local chunks of lg_X and lg_Y.
+C The boundaries and leading dimension are stored in a common block for
+C access inside the lower-level routines.
+C For now, only case H is handled as a distributed array, which is
+C always the Y array.
+      IF (Is_Real_Par()) THEN
+        CALL GA_Sync()
+        myRank = GA_NodeID()
+*     CALL GA_Distribution (lg_X,myRank,iXLo,iXHi,jXLo,jXHi)
+*     IF (iXLo.NE.0.AND.jXLo.NE.0) THEN
+*       CALL GA_Access (lg_X,iXLo,iXHi,jXLo,jXHi,mX,LDX)
+*     END IF
+        CALL GA_Distribution (lg_Y,myRank,iYLo,iYHi,jYLo,jYHi)
+        IF (iYLo.NE.0.AND.jYLo.NE.0) THEN
+          CALL GA_Access (lg_Y,iYLo,iYHi,jYLo,jYHi,mY,LDY)
+          IF (KOD.EQ.17 .OR. KOD.EQ.18) THEN
+            CALL MLTR1_EH(IMLTOP,LST1,
+     &                  X,NAS1,NIS1,JXOFF,
+     &                  F,NFI,NFJ,
+     &                  DBL_MB(mY),NAS2,jYLo,jYHi)
+          ELSE IF (KOD.EQ.21 .OR. KOD.EQ.22) THEN
+            CALL MLTR1_GH(IMLTOP,LST1,
+     &                  X,NAS1,NIS1,JXOFF,
+     &                  F,NFI,NFJ,
+     &                  DBL_MB(mY),NAS2,jYLo,jYHi)
+          END IF
+          CALL GA_Release_Update (lg_Y,iYLo,iYHi,jYLo,jYHi)
+        END IF
+        CALL GA_Sync()
+      ELSE
+#endif
+        IF (KOD.EQ.17 .OR. KOD.EQ.18) THEN
+          CALL MLTR1_EH(IMLTOP,LST1,
+     &                  X,NAS1,NIS1,JXOFF,
+     &                  F,NFI,NFJ,
+     &                  GA_Arrays(lg_Y)%A,NAS2,1,NIS2)
+        ELSE IF (KOD.EQ.21 .OR. KOD.EQ.22) THEN
+          CALL MLTR1_GH(IMLTOP,LST1,
+     &                  X,NAS1,NIS1,JXOFF,
+     &                  F,NFI,NFJ,
+     &                  GA_Arrays(lg_Y)%A,NAS2,1,NIS2)
+        END IF
+#ifdef _MOLCAS_MPP_
+      END IF
+#endif
+      END SUBROUTINE PMLTR1
--- src/caspt2/mltsca.f
+++ src/caspt2/mltsca.f
@@ -188,3 +188,61 @@ C     F(L12,L22) := Add V1*V2*X(L11,L21)*Y(L13,L23)
 *     NFSCA=NFSCA+4*NLST1*NLST2
       RETURN
       END
+
+      SUBROUTINE PMLTSCA(KOD,IMLTOP,LST1,LST2,
+     &                   X,NXI,NXA,F,NFI,NFA,
+     &                   lg_Y,NAS2,NIS2)
+#ifdef _MOLCAS_MPP_
+      USE Para_Info, ONLY: Is_Real_Par
+#endif
+      use Sigma_data
+      use fake_GA, only: GA_Arrays
+      IMPLICIT REAL*8 (A-H,O-Z)
+#ifdef _MOLCAS_MPP_
+#include "global.fh"
+#include "mafdecls.fh"
+#endif
+      DIMENSION X(NXI,NXA),F(NFI,NFA)
+      DIMENSION LST1(4,NLST1), LST2(4,NLST2)
+
+#ifdef _MOLCAS_MPP_
+C SVC: Determine the index ranges of the local chunks of lg_Y.
+C The boundaries and leading dimension are stored in a common block for
+C access inside the lower-level routines.
+C For now, only case H is handled as a distributed array, which is
+C always the Y array.
+      IF (Is_Real_Par()) THEN
+        CALL GA_Sync()
+        myRank = GA_NodeID()
+*     CALL GA_Distribution (lg_X,myRank,iXLo,iXHi,jXLo,jXHi)
+*     IF (iXLo.NE.0.AND.jXLo.NE.0) THEN
+*       CALL GA_Access (lg_X,iXLo,iXHi,jXLo,jXHi,mX,LDX)
+*     END IF
+        CALL GA_Distribution (lg_Y,myRank,iYLo,iYHi,jYLo,jYHi)
+        IF (iYLo.NE.0.AND.jYLo.NE.0) THEN
+          CALL GA_Access (lg_Y,iYLo,iYHi,jYLo,jYHi,mY,LDY)
+          IF (KOD.EQ.23 .OR. KOD.EQ.24) THEN
+            CALL MLTSCA_DH(IMLTOP,LST1,LST2,
+     &                   X,NXI,NXA,F,NFI,NFA,
+     &                   DBL_MB(mY),NAS2,jYLo,jYHi)
+          ELSE
+            WRITE(6,*) 'PMLTSCA: not supposed to be here'
+            CALL AbEnd()
+          END IF
+          CALL GA_Release_Update (lg_Y,iYLo,iYHi,jYLo,jYHi)
+        END IF
+        CALL GA_Sync()
+      ELSE
+#endif
+        IF (KOD.EQ.23 .OR. KOD.EQ.24) THEN
+          CALL MLTSCA_DH(IMLTOP,LST1,LST2,
+     &                   X,NXI,NXA,F,NFI,NFA,
+     &                   GA_Arrays(lg_Y)%A,NAS2,1,NIS2)
+        ELSE
+          WRITE(6,*) 'PMLTSCA: not supposed to be here'
+          CALL AbEnd()
+        END IF
+#ifdef _MOLCAS_MPP_
+      END IF
+#endif
+      END SUBROUTINE PMLTSCA
