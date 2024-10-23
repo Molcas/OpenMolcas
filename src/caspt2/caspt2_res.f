3d75c4637787564d6cc095c06813293be525815a
Roland Lindh <roland.lindh@kemi.uu.se>
2024-10-23 13:11:16 +0200

    Cleanup.

--- src/caspt2/caspt2_res.f
+++ src/caspt2/caspt2_res.f
@@ -118,54 +118,6 @@ C
 C
 C-----------------------------------------------------------------------
 C
-      !! RHS_SGMDIA
-      SUBROUTINE CASPT2_ResD(Mode,NIN,NIS,lg_W1,lg_W2,DIN,DIS)
-#ifdef _MOLCAS_MPP_
-      USE Para_Info, ONLY: Is_Real_Par
-#endif
-      use EQSOLV
-      IMPLICIT REAL*8 (A-H,O-Z)
-
-#include "rasdim.fh"
-#include "caspt2.fh"
-#include "WrkSpc.fh"
-      DIMENSION DIN(*),DIS(*)
-
-C Apply the resolvent of the diagonal part of H0 to an RHS array
-
-#ifdef _MOLCAS_MPP_
-#include "global.fh"
-#include "mafdecls.fh"
-#endif
-
-#ifdef _MOLCAS_MPP_
-      IF (Is_Real_Par()) THEN
-        CALL GA_Sync()
-        myRank = GA_NodeID()
-C-SVC: get the local vertical stripes of the lg_W vector
-      !! not yet, probably just repeat for lg_W2
-        CALL GA_Distribution (lg_W1,myRank,iLo,iHi,jLo,jHi)
-        IF (iLo.NE.0.AND.jLo.NE.0) THEN
-          NROW=iHi-iLo+1
-          NCOL=jHi-jLo+1
-          CALL GA_Access (lg_W1,iLo,iHi,jLo,jHi,mW,LDW)
-          CALL CASPT2_ResD2(MODE,NROW,NCOL,DBL_MB(mW),DBL_MB(mW),
-     &                      LDW,DIN(iLo),DIS(jLo))
-          CALL GA_Release_Update (lg_W,iLo,iHi,jLo,jHi)
-        END IF
-        CALL GA_Sync()
-      ELSE
-#endif
-        CALL CASPT2_ResD2(MODE,NIN,NIS,WORK(lg_W1),WORK(lg_W2),
-     *                    NIN,DIN,DIS)
-#ifdef _MOLCAS_MPP_
-      END IF
-#endif
-
-      END
-C
-C-----------------------------------------------------------------------
-C
       !! RESDIA
       subroutine CASPT2_ResD2(Mode,nRow,nCol,W1,W2,LDW,dIn,dIs)
 
