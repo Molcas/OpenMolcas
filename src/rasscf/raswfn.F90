4e7b72ce26d74a9146eeab9f7e49bc1d72f00566
Roland Lindh <roland.lindh@kemi.uu.se>
2024-10-30 14:39:43 +0100

    Cleanup.

--- src/rasscf/raswfn.F90
+++ src/rasscf/raswfn.F90
@@ -38,7 +38,7 @@ use sxci, only: IDXCI, IDXSX
 use stdalloc, only: mma_allocate, mma_deallocate
 use gas_data, only: iDoGAS, NGAS, NGSSH
 use input_ras, only: KeyTDM
-use rasscf_global
+use rasscf_global, only: DoDMRG, IROOT, IXSYM, LROOTS, NAC, NROOTS, WEIGHT
 
 
 implicit none
