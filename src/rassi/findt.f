ef0a9ad8f329df357563ee4dc3deb6f1ccc18842
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-03 22:27:46 +0100

    Cleanup.

--- src/rassi/findt.f
+++ src/rassi/findt.f
@@ -20,15 +20,15 @@
 *****************************************************************
       SUBROUTINE FINDT (CMO1,CMO2,TRA1,TRA2)
       use stdalloc, only: mma_allocate, mma_deallocate
-      use Cntrl
-      IMPLICIT REAL*8 (A-H,O-Z)
-      Real*8 CMO1(NCMO),CMO2(NCMO)
-      Real*8 TRA1(NTRA),TRA2(NTRA)
+      use Cntrl, only: PRSXY, PRTRA, PRORB
+      IMPLICIT None
 #include "rasdim.fh"
 #include "symmul.fh"
 #include "rassi.fh"
-
+      Real*8 CMO1(NCMO),CMO2(NCMO)
+      Real*8 TRA1(NTRA),TRA2(NTRA)
       Real*8, allocatable:: SXY(:), CXA(:), CYB(:)
+      Integer NCYB
 
 C BEGIN BY COMPUTING MO OVERLAPS, SXY.
 C MO OVERLAP MATRIX HAS SAME STRUCTURE AS TRA1,TRA2:
