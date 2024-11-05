04102637218ecaa178caf0134ff87875f91effa1
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-05 21:27:10 +0100

    Cleanup 'only'.

--- src/rassi/maketdmao.f
+++ src/rassi/maketdmao.f
@@ -25,8 +25,8 @@
       use rassi_aux, only : idisk_TDM
       use rassi_global_arrays, only: JBNUM
       use stdalloc, only: mma_allocate, mma_deallocate
-      use Cntrl, only: MXJOB, NSTATE, LSYM1, LSYM2, IRREP, MLTPLT
-      use Files
+      use Cntrl, only: NSTATE, LSYM1, LSYM2, IRREP, MLTPLT
+      use Files, only: LuTDM
 
       IMPLICIT NONE
 #include "rassi.fh"
