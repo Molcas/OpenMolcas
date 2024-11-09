5655ef1b734bfb55504c4b9bea6485bdf7309661
Matthew Hennefarth <matthew.hennefarth@gmail.com>
2024-11-09 15:48:17 -0600

    prep readvc

--- src/mcpdft/readvc.f
+++ src/mcpdft/readvc.f
@@ -1,49 +1,30 @@
-************************************************************************
-* This file is part of OpenMolcas.                                     *
-*                                                                      *
-* OpenMolcas is free software; you can redistribute it and/or modify   *
-* it under the terms of the GNU Lesser General Public License, v. 2.1. *
-* OpenMolcas is distributed in the hope that it will be useful, but it *
-* is provided "as is" and without any express or implied warranties.   *
-* For more details see the full text of the license in the file        *
-* LICENSE or in <http://www.gnu.org/licenses/>.                        *
-*                                                                      *
-* Copyright (C) 1998, Markus P. Fuelscher                              *
-************************************************************************
-      Subroutine ReadVC_m(CMO)
-************************************************************************
-*                                                                      *
-*     purpose:                                                         *
-*     get start MO-coefficients                                        *
-*     (if the CI is restarted from a previous calculation get          *
-*      also the density matrices)                                      *
-*                                                                      *
-*     calling arguments:                                               *
-*     CMO     : array of real*8                                        *
-*               MO-coefficients                                        *
-*     INVEC   : integer                                                *
-*               flag indicating orbital type                           *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     written by:                                                      *
-*     M. P. Fuelscher                                                  *
-*     University of Lund, Sweden, 1998                                 *
-*                                                                      *
-*----------------------------------------------------------------------*
-*                                                                      *
-*     history:                                                         *
-*     none                                                             *
-*                                                                      *
-************************************************************************
+!***********************************************************************
+! This file is part of OpenMolcas.                                     *
+!                                                                      *
+! OpenMolcas is free software; you can redistribute it and/or modify   *
+! it under the terms of the GNU Lesser General Public License, v. 2.1. *
+! OpenMolcas is distributed in the hope that it will be useful, but it *
+! is provided "as is" and without any express or implied warranties.   *
+! For more details see the full text of the license in the file        *
+! LICENSE or in <http://www.gnu.org/licenses/>.                        *
+!                                                                      *
+! Copyright (C) 1998, Markus P. Fuelscher                              *
+!***********************************************************************
 
+!> @brief get start MO coeffients
+!>
+!> @author M. P. Fuelscher
+!> @author Matthew R. Hennefarth
+!>
+!> @param[out] cmo mo coefficients
+      Subroutine ReadVC_m(CMO)
 #ifdef _HDF5_
       use mh5, only: mh5_open_file_r, mh5_fetch_dset, mh5_close_file
 #endif
-      use definitions, only: wp, iwp
+      use definitions, only: wp, iwp,u6
       use constants,only:zero
       use printlevel, only: terse, verbose, debug
-      use mcpdft_output, only: lf, iPrGlb, iPrLoc
+      use mcpdft_output, only: iPrGlb, iPrLoc
       use mcpdft_input, only: mcpdft_options
 
       implicit none
@@ -54,23 +35,17 @@
 #include "warnings.h"
 
       real(kind=wp), Dimension(*) :: CMO
-!     local data declarations
       integer(kind=iwp), dimension(30) :: IADR19
-      logical :: Found
+      logical(kind=iwp) :: Found
       integer(kind=iwp) :: i, iad19, ijob, iprlev
 
-
 #ifdef _HDF5_
-      integer mh5id
+      integer(kind=iwp) mh5id
 #endif
 
-!----------------------------------------------------------------------*
-!                                                                      *
-!----------------------------------------------------------------------*
-! Local print level (if any)
       IPRLEV=IPRLOC(1)
       IF(IPRLEV >= DEBUG) THEN
-        WRITE(LF,*)' Entering READVC'
+        WRITE(u6,*)' Entering READVC'
       END IF
 
 ! invec can either be 3 or 4 based off of proc_inpx
@@ -94,7 +69,7 @@
            end if
         Else
            If (IPRLEV >= TERSE) then
-              Write(LF,*) '  File JOBOLD not found -- use JOBIPH.'
+              Write(u6,*) '  File JOBOLD not found -- use JOBIPH.'
            End If
            If (JOBIPH > 0) Then
               JOBOLD=JOBIPH
@@ -115,13 +90,13 @@
         END IF
         IF(IPRLEV >= TERSE) THEN
          If (iJOB == 1) Then
-            Write(LF,'(6X,A)')
+            Write(u6,'(6X,A)')
      &      'The MO-coefficients are taken from the file:'
-            Write(LF,'(6X,A)') 'JOBOLD'
+            Write(u6,'(6X,A)') 'JOBOLD'
          Else
-            Write(LF,'(6X,A)')
+            Write(u6,'(6X,A)')
      &      'The MO-coefficients are taken from the file:'
-            Write(LF,'(6X,A)') trim(mcpdft_options%wfn_file)
+            Write(u6,'(6X,A)') trim(mcpdft_options%wfn_file)
          End If
         END IF
 
@@ -139,22 +114,22 @@
       Else If (InVec.eq.4) then
 #ifdef _HDF5_
         IF(IPRLEV.ge.TERSE) THEN
-          Write(LF,'(6X,A)')
+          Write(u6,'(6X,A)')
      &            'The MO-coefficients are taken from the file:'
-          Write(LF,'(6X,A)') trim(mcpdft_options%wfn_file)
+          Write(u6,'(6X,A)') trim(mcpdft_options%wfn_file)
         END IF
 
         mh5id = mh5_open_file_r(mcpdft_options%wfn_file)
         call mh5_fetch_dset(mh5id, 'MO_VECTORS', CMO)
         call mh5_close_file(mh5id)
 #else
-        write (6,*) 'Orbitals requested from HDF5, but this'
-        write (6,*) 'installation does not support that, abort!'
+        write (u6,*) 'Orbitals requested from HDF5, but this'
+        write (u6,*) 'installation does not support that, abort!'
         call abend
 #endif
       else if (invec .eq. 5) then
-        write(lf,*) "FileOrb specified, but does not point to hdf5 file"
-        write(lf,*) "This has not been implemented, aborting"
+        write(u6,*) "FileOrb specified, but does not point to hdf5 file"
+        write(u6,*) "This has not been implemented, aborting"
         call abend
       End If
 
