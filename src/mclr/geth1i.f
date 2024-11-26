88bae56490e8f6f13eefd8829757916895d6cad8
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-26 15:49:53 +0100

    Cleanup.

--- src/mclr/geth1i.f
+++ src/mclr/geth1i.f
@@ -8,16 +8,18 @@
 * For more details see the full text of the license in the file        *
 * LICENSE or in <http://www.gnu.org/licenses/>.                        *
 ************************************************************************
-       FUNCTION GETH1I_MCLR(I,J)
+       REAL*8 FUNCTION GETH1I_MCLR(I,J)
        use Arrays, only: FIMO
+       use MCLR_Data, only: ipCM
 *
 * Obtain one -electron integral H(IORB,JOB)
 *
-      IMPLICIT REAL*8 (A-H,O-Z)
+      IMPLICIT None
+      Integer I, J
 #include "detdim.fh"
 #include "Input.fh"
-#include "Pointers.fh"
 #include "orbinp_mclr.fh"
+      Integer ISM, JSM, I1, J1, IJ
 *
       ISM = ISMFTO(I)
       JSM = ISMFTO(J)
@@ -29,5 +31,4 @@
       GETH1I_MCLR = FIMO(IJ)
 *
 *
-      RETURN
-      END
+      END FUNCTION GETH1I_MCLR
