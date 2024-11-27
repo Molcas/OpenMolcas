779fe0be84fe752671e601d3f75a891263532469
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-27 17:00:43 +0100

    Cleanup.

--- src/mclr/rdjobiph_td.f
+++ src/mclr/rdjobiph_td.f
@@ -23,17 +23,23 @@
 ************************************************************************
       Use Arrays, only: CMO, G2t, G2sq, G1t
       use stdalloc, only: mma_allocate, mma_deallocate
-      Implicit Real*8 (a-h,o-z)
+      use MCLR_Data, only: nNA, nA
+      Implicit None
+      Real*8, Allocatable:: CIVec(:,:)
 #include "Input.fh"
 #include "Files_mclr.fh"
-#include "Pointers.fh"
 #include "SysDef.fh"
-      Character*72 Line
+      Character(LEN=72) Line
       Real*8 rdum(1)
       Character(Len=1), Allocatable:: TempTxt(:)
       Real*8, Allocatable::  Tmp2(:), G2tts(:), G2tta(:)
-      Real*8, Allocatable:: CIVec(:,:)
+      Integer kRoots,iDisk,Length,iSym,      i,    j,         jpCMO,
+     &             Iter,nAct,nAct2,nAct4,iS,jS,kS,lS,nG1,nG2,iB,jB,
+     &        iDij,kB,lB,iDkl,iIJKL,iDij2,iDkl2,iIJKL2
+      Real*8 Temp,PotNuc0,Fact,Factij,Factkl,Fact2
 
+
+      integer itri
       itri(i,j)=Max(i,j)*(Max(i,j)-1)/2+Min(i,j)
 
 *----------------------------------------------------------------------*
@@ -249,5 +255,4 @@
 *----------------------------------------------------------------------*
 *     exit                                                             *
 *----------------------------------------------------------------------*
-      Return
-      End
+      End Subroutine RdJobIph_td
