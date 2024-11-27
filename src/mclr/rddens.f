b5248a25fdc51dc0ca1cc74c21fc1fc1748550e0
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-27 16:44:55 +0100

    Cleanup.

--- src/mclr/rddens.f
+++ src/mclr/rddens.f
@@ -10,31 +10,33 @@
 ************************************************************************
       Subroutine RdDens(d1,nd1,d2,nd2)
       use stdalloc, only: mma_allocate, mma_deallocate
-      Implicit Real*8(a-h,o-z)
-
-#include "Input.fh"
-
-#include "SysDef.fh"
-#include "Pointers.fh"
-#include "Files_mclr.fh"
+      use Constants, only: Zero, One, Two
+      Implicit None
+      Integer nd1, nd2
       Real*8 D1(nd1),d2(nd2)
+#include "Input.fh"
+#include "SysDef.fh"
+#include "Files_mclr.fh"
       Real*8 rdum(1)
       Real*8, Allocatable:: G2tt(:), D2t(:), D1t(:)
+      Integer jDisk,i,j,iB,jB,iDij,kB,lB,iDkl,iIJKL
+      Real*8 W,Fact
 *                                                                      *
 ************************************************************************
 *                                                                      *
+      Integer itri
       itri(i,j)=Max(i,j)*(Max(i,j)-1)/2+Min(i,j)
 *                                                                      *
 ************************************************************************
 *                                                                      *
-      d1(:)=0.0d0
+      d1(:)=Zero
       Call mma_allocate(G2tt,nd2,Label='G2tt')
       Call mma_allocate(D2t,nd2,Label='D2t')
       Call mma_allocate(D1t,nd1,Label='D1t')
-      G2tt(:)=0.0d0
+      G2tt(:)=Zero
       jDisk = ITOC(3)
       Do i=1,lroots
-         W=0.0d0
+         W=Zero
          Do j=1,nroots
             If (iroot(j).eq.i) W=Weight(j)
          End Do
@@ -42,7 +44,7 @@
          Call dDaFile(LUJOB ,0,rdum,nd1,jDisk)
          Call dDaFile(LUJOB ,2,D2t,ND2,jDisk)
          Call dDaFile(LUJOB ,0,rdum,ND2,jDisk)
-         If (W.ne.0.0d0) Then
+         If (W.ne.Zero) Then
             call daxpy_(nd2,w,D2t,1,G2tt,1)
             call daxpy_(nd1,w,D1t,1,D1,1)
          End If
@@ -58,9 +60,9 @@
         Do kB=1,ib
          Do lB=1,kB
           iDkl=iTri(kB,lB)
-          fact=1.0d00
-          if(iDij.ge.iDkl .and. kB.eq.lB) fact=2.0d00
-          if(iDij.lt.iDkl .and. iB.eq.jB) fact=2.0d00
+          fact=One
+          if(iDij.ge.iDkl .and. kB.eq.lB) fact=Two
+          if(iDij.lt.iDkl .and. iB.eq.jB) fact=Two
           iijkl=itri(iDij,iDkl)
           D2(iijkl)=Fact*G2tt(iijkl)
          End Do
@@ -71,5 +73,4 @@ c
       Call mma_deallocate(G2tt)
       Call mma_deallocate(D2t)
       Call mma_deallocate(D1t)
-      Return
-      End
+      End Subroutine RdDens
