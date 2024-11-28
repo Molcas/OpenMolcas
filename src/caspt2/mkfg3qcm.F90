876caac50c05231c52d5f06bc3c270efa8d3afa7
Kalman Szenes <szenes.kalman@gmail.com>
2024-11-28 13:12:18 +0100

    format(dmrg-caspt2)

--- src/caspt2/mkfg3qcm.F90
+++ src/caspt2/mkfg3qcm.F90
@@ -12,106 +12,102 @@
 !***********************************************************************
 #ifdef _DMRG_
 
-subroutine mkfg3qcm(IFF,G1,F1,G2,F2,G3,F3,idxG3)
+subroutine mkfg3qcm(IFF, G1, F1, G2, F2, G3, F3, idxG3)
 
-  use stdalloc, only:mma_allocate,mma_deallocate
-  use qcmaquis_interface
-  use definitions, only:wp,iwp,i1
-  use gugx, only:SGS
-  use caspt2_global, only: CompressMPS
+   use stdalloc, only: mma_allocate, mma_deallocate
+   use qcmaquis_interface
+   use definitions, only: wp, iwp, i1
+   use gugx, only: SGS
+   use caspt2_global, only: CompressMPS
 
-  implicit none
+   implicit none
 
 #include "caspt2.fh"
 #include "pt2_guga.fh"
 
-  Integer(kind=iwp), intent(in)  :: IFF
-  Real(kind=wp),     intent(out) :: G1(nasht,nasht),G2(nasht,nasht,nasht,nasht)
-  Real(kind=wp),     intent(out) :: F1(nasht,nasht),F2(nasht,nasht,nasht,nasht)
-  Real(kind=wp),     intent(out) :: G3(*), F3(*)
-  Integer(kind=i1),  intent(in)  :: idxG3(6,*)
-  Real(kind=wp), allocatable :: G3tmp(:,:,:,:,:,:),TG3tmp(:,:,:,:,:,:)
-  ! Real(kind=wp), allocatable :: G4(:,:,:,:,:)
-  Real(kind=wp) :: val
-  Integer(kind=iwp) :: t,u,v,w,x,y,z,tu,vx
-  Integer(kind=iwp) :: i,n4
+   Integer(kind=iwp), intent(in)  :: IFF
+   Real(kind=wp), intent(out) :: G1(nasht, nasht), G2(nasht, nasht, nasht, nasht)
+   Real(kind=wp), intent(out) :: F1(nasht, nasht), F2(nasht, nasht, nasht, nasht)
+   Real(kind=wp), intent(out) :: G3(*), F3(*)
+   Integer(kind=i1), intent(in)  :: idxG3(6, *)
+   Real(kind=wp), allocatable :: G3tmp(:, :, :, :, :, :), TG3tmp(:, :, :, :, :, :)
+   ! Real(kind=wp), allocatable :: G4(:,:,:,:,:)
+   Real(kind=wp) :: val
+   Integer(kind=iwp) :: t, u, v, w, x, y, z, tu, vx
+   Integer(kind=iwp) :: i, n4
 
+   ! This might be memory hungry
+   ! call mma_allocate(G3tmp,nasht,nasht,nasht,nasht,nasht,nasht,Label='G3tmp')
+   allocate (G3tmp(nasht, nasht, nasht, nasht, nasht, nasht))
+   allocate (TG3tmp(nasht, nasht, nasht, nasht, nasht, nasht))
+   ! call mma_allocate(G4,n4,nasht,nasht,nasht,nasht,Label='G4')
 
-  ! number of elements in the contracted 4-index of the 4-RDM
-  n4 = (nasht*(nasht+1)*(nasht+2)*(nasht+3)/24)
+   call qcmaquis_interface_get_1rdm_full(G1)
+   call qcmaquis_interface_get_2rdm_full(G2)
+   call qcmaquis_interface_get_3rdm_full(G3tmp)
+   call qcmaquis_interface_get_fock_contracted_4rdm_full(TG3tmp, epsa, CompressMPS)
 
-  ! This might be memory hungry
-  ! call mma_allocate(G3tmp,nasht,nasht,nasht,nasht,nasht,nasht,Label='G3tmp')
-  allocate(G3tmp(nasht,nasht,nasht,nasht,nasht,nasht))
-  allocate(TG3tmp(nasht,nasht,nasht,nasht,nasht,nasht))
-  ! call mma_allocate(G4,n4,nasht,nasht,nasht,nasht,Label='G4')
+   ! number of elements in the contracted 4-index of the 4-RDM
+   ! n4 = (nasht*(nasht+1)*(nasht+2)*(nasht+3)/24)
+   ! call mma_allocate(G4,n4,nasht,nasht,nasht,nasht,Label='G4')
+   ! call qcmaquis_interface_get_4rdm_full(G4)
 
-
-  call qcmaquis_interface_get_1rdm_full(G1)
-  call qcmaquis_interface_get_2rdm_full(G2)
-  call qcmaquis_interface_get_3rdm_full(G3tmp)
-  call qcmaquis_interface_get_fock_contracted_4rdm_full(TG3tmp,epsa,CompressMPS)
-
-
-  if (iff > 0)then
-    do t = 1,nasht
-      do u = 1,t
-        if (mul(SGS%ism(t),SGS%ism(u)) == 1) then
-          do w = 1,nasht
-            F1(t,u) = F1(t,u) + G2(t,u,w,w) * epsa(w)
-          end do
-          F1(u,t) = F1(t,u)
-        end if
-      end do
-    end do
-  end if
-
-
-  if (iff > 0)then
-    do t = 1,nasht
-      do u = 1,nasht
-        do v = 1,nasht
-          do x = 1,nasht
-            if (mul(SGS%ism(x),mul(SGS%ism(v),mul(SGS%ism(u),SGS%ism(t)))) == 1) then
-              do w = 1,nasht
-                F2(t,u,v,x) = F2(t,u,v,x) + G3tmp(t,v,w,u,x,w) * epsa(w)
-              end do
+   if (iff > 0) then
+      do t = 1, nasht
+         do u = 1, t
+            if (mul(SGS%ism(t), SGS%ism(u)) == 1) then
+               do w = 1, nasht
+                  F1(t, u) = F1(t, u) + G2(t, u, w, w)*epsa(w)
+               end do
+               F1(u, t) = F1(t, u)
             end if
-          end do
-        end do
+         end do
       end do
-    end do
-  end if
+   end if
 
+   if (iff > 0) then
+      do t = 1, nasht
+         do u = 1, nasht
+            do v = 1, nasht
+               do x = 1, nasht
+                  if (mul(SGS%ism(x), mul(SGS%ism(v), mul(SGS%ism(u), SGS%ism(t)))) == 1) then
+                     do w = 1, nasht
+                        F2(t, u, v, x) = F2(t, u, v, x) + G3tmp(t, v, w, u, x, w)*epsa(w)
+                     end do
+                  end if
+               end do
+            end do
+         end do
+      end do
+   end if
 
+   do i = 1, ng3
+      t = idxG3(1, i)
+      u = idxG3(2, i)
+      v = idxG3(3, i)
+      x = idxG3(4, i)
+      y = idxG3(5, i)
+      z = idxG3(6, i)
 
-  do i = 1,ng3
-    t = idxG3(1,i)
-    u = idxG3(2,i)
-    v = idxG3(3,i)
-    x = idxG3(4,i)
-    y = idxG3(5,i)
-    z = idxG3(6,i)
+      G3(i) = G3tmp(t, v, y, u, x, z)
+      ! F_tvyuxz = <| e_{tv,yu,xz} E_ww f_ww |> - <| e_{tv,yu,xz} |> (f_uu + f_xx + f_zz)
+      ! Compute first term using transition RDM between <| and |'> = E_ww f_ww |>: <| e_{tv,yu,xz} |'>
+      F3(i) = TG3tmp(t, v, y, u, x, z) - G3(i)*(epsa(u) + epsa(x) + epsa(z))
 
-    G3(i) = G3tmp(t,v,y,u,x,z)
-    ! F_tvyuxz = <| e_{tv,yu,xz} E_ww f_ww |> - <| e_{tv,yu,xz} |> (f_uu + f_xx + f_zz)
-    ! Compute first term using transition RDM between <| and |'> = E_ww f_ww |>: <| e_{tv,yu,xz} |'>
-    F3(i) = TG3tmp(t,v,y,u,x,z) - G3(i) * (epsa(u) + epsa(x) + epsa(z))
+      ! do w = 1,nasht
+      !   val = get_p4_element(G4,w,t,v,y,w,u,x,z,nasht)
+      !   F3(i) = F3(i) + val * epsa(w)
+      ! end do
+   end do
 
-    ! do w = 1,nasht
-    !   val = get_p4_element(G4,w,t,v,y,w,u,x,z,nasht)
-    !   F3(i) = F3(i) + val * epsa(w)
-    ! end do
-  end do
-
-  if (allocated(G3tmp)) deallocate(G3tmp)
-  if (allocated(TG3tmp)) deallocate(TG3tmp)
-  ! call mma_deallocate(G4)
+   if (allocated(G3tmp)) deallocate (G3tmp)
+   if (allocated(TG3tmp)) deallocate (TG3tmp)
+   ! call mma_deallocate(G4)
 end subroutine mkfg3qcm
 
 #elif defined (NAGFOR)
 ! Some compilers do not like empty files
-      subroutine empty_mkfg3qcm()
-      end subroutine empty_mkfg3qcm
+subroutine empty_mkfg3qcm()
+end subroutine empty_mkfg3qcm
 #endif
 
