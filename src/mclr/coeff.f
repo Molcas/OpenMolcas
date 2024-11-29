be1d2ae2244a83345dcbb9ce505b30e922e73878
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-29 15:10:29 +0100

    Cleanup.

--- src/mclr/coeff.f
+++ src/mclr/coeff.f
@@ -10,15 +10,19 @@
 ************************************************************************
 *
       subroutine coeff(ralpha,rbetaa,rbetas)
-      Implicit Real*8(a-h,o-z)
+      use Constants, only: Zero, One, Two, Four, Six, Eight
+      use cstate_mclr, only: mS2
+      Implicit None
+      real*8 ralpha,rbetaa,rbetas
 
 #include "Input.fh"
-#include "cstate_mclr.fh"
-      Two=2.0d0
-      rOne=1.0d0
-      Zero=0.0d0
+      Real*8, parameter:: twentyfour=Four*Six
+      Real*8 Spin,RMS,rcg21,rcg11,rcg20,rcg10,rgamma
+      real*8, External:: clebsch_gordan_mclr
+
       Spin=DBLE(ispin-1)/Two
       rms=DBLE(ms2)/Two
+
       If (rms.eq.0.0d0.or.rms.ne.spin) then
       Write(6,*)
       Write(6,*) '====='
@@ -31,19 +35,21 @@
       Write(6,*)
       Call Quit_OnUserError()
       end if
-      rcg21=clebsch_gordan_mclr(Two,rone,spin,rms-rone,spin,rms)
-      rcg11=clebsch_gordan_mclr(rOne,rOne,spin,rms-rone,spin,rms)
+
+      rcg21=clebsch_gordan_mclr(Two,one,spin,rms-one,spin,rms)
+      rcg11=clebsch_gordan_mclr(One,One,spin,rms-one,spin,rms)
       rcg20=clebsch_gordan_mclr(Two,Zero,spin,rms,spin,rms)
-      rcg10=clebsch_gordan_mclr(rOne,Zero,spin,rms,spin,rms)
-      rgamma=sqrt(spin*(spin+1.0D0)-rms*(rms-1.0D0))
+      rcg10=clebsch_gordan_mclr(One,Zero,spin,rms,spin,rms)
+      rgamma=sqrt(spin*(spin+One)-rms*(rms-One))
 *
       ralpha=rMS**2
-      rBetaa=rMS/sqrt(8.0d0)*rgamma*rcg11/rcg10
-      rbetaS=0.0d0
-      If (abs(2.0d0-spin).le.spin) Then
-      rAlpha=ralpha-rMS*rgamma*rcg21/(rcg20*sqrt(6.0d0))
-      rBetas=-rMS*rgamma*rcg21/(sqrt(24.0d0)*rcg20)
+      rBetaa=rMS/sqrt(Eight)*rgamma*rcg11/rcg10
+      rbetaS=Zero
+
+      If (abs(Two-spin).le.spin) Then
+      rAlpha=ralpha-rMS*rgamma*rcg21/(rcg20*sqrt(Six))
+      rBetas=-rMS*rgamma*rcg21/(sqrt(Twentyfour)*rcg20)
+
       End If
 *
-      return
-      end
+      end subroutine coeff
