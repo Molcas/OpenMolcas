a3315210ffc3547884dc75c1340b6ad4ba90ac6a
Roland Lindh <roland.lindh@kemi.uu.se>
2024-11-29 13:53:12 +0100

    Cleanup.

--- src/mclr/rdjobiph.f
+++ src/mclr/rdjobiph.f
@@ -27,13 +27,12 @@
       use stdalloc, only: mma_allocate, mma_deallocate
       use MCLR_Data, only: nA, nNA
       use MCLR_Data, only: IRLXROOT,ISTATE,SA,OVERRIDE,ISNAC,NSSA,
-     &                       NACSTATES
+     &                     NACSTATES
       Implicit None
       Real*8, Allocatable:: CIVec(:,:)
 
 #include "Input.fh"
 #include "Files_mclr.fh"
-#include "disp_mclr.fh"
 #include "SysDef.fh"
 #include "dmrginfo_mclr.fh"
       Character(LEN=72) Line
