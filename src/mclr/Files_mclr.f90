8484261f85345fa838abf52a2f2adb884e9e2f12
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-01 21:07:47 +0100

    Cleanup.

--- src/mclr/Files_mclr.f90
+++ src/mclr/Files_mclr.f90
@@ -15,15 +15,13 @@
 !----------------------------------------------------------------------*
 !
 Module Files_mclr
-#include "Files_mclr.fh"
-Save
-!Character(LEN=8) FnOne,FnJob,FnTwo,FnMol,FnRlx,FnMck,FnTemp,           &
-!                 FnHlf1,FnHlf2,FNHlf3,FNHlf4,FnPT2,                    &
-!                 FNTrI1,FNTrI2,FNTRi3,FNTRI4,FNTRI5,FNCSF2SD,FnMOTRA,  &
-!                 FnQDAT
-!Integer LuOne,LuJob,LuTwo,LuMol,LuRlx,LuMck,LuTemp,                    &
-!        LUHLF1,LUHlf2,LUHlf3,LUHLF4,LuPt2,                      &
-!        LUTrI1,LUTrI2,LUTRi3,LUTRI4,LUTRI5,LUCSF2SD,LuMOTRA,    &
-!        LuQDAT
-!Logical Nofile
+Character(LEN=8) FnOne,FnJob,FnTwo,FnMol,FnRlx,FnMck,FnTemp,           &
+                 FnHlf1,FnHlf2,FNHlf3,FNHlf4,FnPT2,                    &
+                 FNTrI1,FNTrI2,FNTRi3,FNTRI4,FNTRI5,FNCSF2SD,FnMOTRA,  &
+                 FnQDAT
+Integer LuOne,LuJob,LuTwo,LuMol,LuRlx,LuMck,LuTemp,                    &
+        LUHLF1,LUHlf2,LUHlf3,LUHLF4,LuPt2,                      &
+        LUTrI1,LUTrI2,LUTRi3,LUTRI4,LUTRI5,LUCSF2SD,LuMOTRA,    &
+        LuQDAT
+Logical Nofile
 End Module Files_mclr
