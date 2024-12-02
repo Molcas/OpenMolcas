5fb6d38073055ae61267044971c36f4a91f0b581
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-02 09:17:46 +0100

    Cleanup.

--- src/mclr/mclr_data.f90
+++ src/mclr/mclr_data.f90
@@ -89,4 +89,35 @@ Integer LuOne,LuJob,LuTwo,LuMol,LuRlx,LuMck,LuTemp,                    &
         LuQDAT
 Logical Nofile
 
+! Stuff from cicisp_mclr.fh
+#include "detdim.fh"
+!
+! icisps **
+! smost ***
+!     niCISp                :        Number of CI-spaces (1) *
+!     iAStFI                :       Alpha string type for internal CI space *
+!     iBStFI                :       Beta string type for internal CI space (iBZTP)*
+!     iActi                :        Active/Inactive space  (1)*
+!     MnR1IC                :         Minimum number of electrons in RAS1*
+!     MxR1IC                :        Maximum    -              "             - RAS1*
+!     MnR3IC                :       Minimum    -             "       - RAS3*
+!     MxR3IC                :        Maximum    -              "             - RAS3*
+!     iZCI                :        Internal zero order space *
+!     iRCI                :        Number of zero order space *
+!     nElCI                :           Number of electrons per CI space *
+!     nAElCI                :           Number of alpha electrons per CI space *
+!     nBElCI                :           Number of beta electrons per CI space *
+!     xispsm                :       Number of det. for each (symmetry,CI space) **
+!     ismost                :       Symmetry operator ASym=ismost(BSym,iTOTSM) ***(istead of ieor)
+!     MXSB                 :       Largest symmetry block **
+!     MXSOOB                :       Largest block        **
+!
+Integer NICISP,IASTFI(MXPICI),IBSTFI(MXPICI),IACTI(MXPICI),MNR1IC(MXPICI),MXR1IC(MXPICI),    &
+        MNR3IC(MXPICI),MXR3IC(MXPICI),IZCI,IRCI(3,7,7),NELCI(MXPICI),NAELCI(MXPICI),NBELCI(MXPICI), &
+        ISMOST(MXPCSM,MXPCSM),MXSB,MXSOOB,ldet,lcsf
+Real*8 XISPSM(MXPCSM,MXPICI)
+Private MXPIRR,MXPOBS,MXPR4T,MXINKA,MXPORB,MXPXOT,MXPXST,MXPSHL, &
+        MXPL,MXPXT,MXPICI,MXPSTT,MXPCSM,MXPCTP,MXCNSM,MXPWRD, &
+        MXNMS,MTYP,MXPNGAS,MXPNSMST,MXPPTSPC
+
 End Module MCLR_Data
