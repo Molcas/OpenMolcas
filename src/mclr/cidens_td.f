4389ba427cfa21b2075f7aa4d898d80d7afe1089
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-02 17:55:30 +0100

    Cleanup.

--- src/mclr/cidens_td.f
+++ src/mclr/cidens_td.f
@@ -19,7 +19,6 @@
       Real*8 rP(*),rD(*)
 #include "detdim.fh"
 #include "Input.fh"
-#include "spinfo_mclr.fh"
 #include "cands.fh"
       Real*8, Allocatable:: De(:), Pe(:), CIL(:), CIR(:)
       Integer nConfL, nConfR, nC, iRC
