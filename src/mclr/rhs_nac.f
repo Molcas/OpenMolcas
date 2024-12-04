e70764853b776f4b263cafe776190755fdb1fd5e
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-04 21:28:31 +0100

    Cleanup.

--- src/mclr/rhs_nac.f
+++ src/mclr/rhs_nac.f
@@ -16,8 +16,9 @@
       use MCLR_Data, only: NSSA
       use MCLR_Data, only: XISPSM
       use cands, only: ICSM,ISSM
+      use input_mclr, only: ntAsh,PT2,State_Sym,nSym,nRoots,nConf,nBas,
+     &                      nCSF
       Implicit None
-#include "Input.fh"
       Real*8 Fock(*)
       real*8, optional :: SLag(*)
       Integer ng1,ng2,i,j,k,l,ij,kl,ijkl,ij2,kl2,ijkl2
