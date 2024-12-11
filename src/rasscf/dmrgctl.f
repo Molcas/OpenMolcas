49b5130a90efd8040b6f43cd61395e3507b26adb
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-11 15:26:06 +0100

    Remove output_ras.fh.

--- src/rasscf/dmrgctl.f
+++ src/rasscf/dmrgctl.f
@@ -52,7 +52,7 @@
      &                         n_Det, NAC, NACPAR, NACPR2, nFint,
      &                         nRoots, S, iAdr15, iRoot, Weight,
      &                         DFTFOCK
-      use output_ras, only: LF,DEBUG,IPRLOC
+      use output_ras, only: LF,DEBUG,INSANE,IPRLOC
 
       Implicit None
       Integer iFinal, IRst
