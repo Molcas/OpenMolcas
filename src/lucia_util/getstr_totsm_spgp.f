0b52cf5d063098f6aadafd39d665f2aaf066c390
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-15 13:50:11 +0100

    Remove gasstr.fh.

--- src/lucia_util/getstr_totsm_spgp.f
+++ src/lucia_util/getstr_totsm_spgp.f
@@ -14,6 +14,7 @@
      &                               ISTR, NORBT,IDOREO,    IZ,  IREO)
       use strbas, only: NSTSGP,ISTSGP
       use lucia_data, only: NGAS
+      use lucia_data, only: IBSPGPFTP,ISPGPFTP,NELFGP
 *
 * Obtain all super-strings of given total symmetry and given
 * occupation in each GAS space
@@ -49,7 +50,6 @@
       INTEGER ISTRTP,ISPGRP,ISPGRPSM,NEL,NSTR,NORBT,IDOREO
 !*. Input
 #include "mxpdim.fh"
-#include "gasstr.fh"
 #include "csm.fh"
       INTEGER IZ(NORBT,NEL)
 *. output
