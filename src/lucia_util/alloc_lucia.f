d5ceae5e7a1e765a890e1e78b7601b834c7707b1
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-16 15:40:12 +0100

    Remove mxpdim.fh.

--- src/lucia_util/alloc_lucia.f
+++ src/lucia_util/alloc_lucia.f
@@ -16,6 +16,7 @@
       use lucia_data, only: NBINT1,NBINT2
       use lucia_data, only: NSMOB
       use lucia_data, only: NTOOB
+      use lucia_data, only: MXPOBS
 *
 * Dimensions and
 * Allocation of static memory
@@ -45,7 +46,6 @@
 
 *. Input
       IMPLICIT None
-#include "mxpdim.fh"
 #include "csm.fh"
 #include "csmprd.fh"
 *.Output
