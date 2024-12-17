c91a4f97dc11bc11b23a3266eac5554c16c15d3f
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-17 10:09:27 +0100

    Remove general.fh.

--- src/molcas_ci_util/ciovlp.F90
+++ src/molcas_ci_util/ciovlp.F90
@@ -43,10 +43,10 @@ subroutine CiOvlp(jRoot,S1,S2,CI_vec)
 use rasscf_global, only: cCI, ITER, jCj, lRoots, nRoots
 use Constants, only: Zero
 use Definitions, only: wp, iwp
+use general_data, only: NCONF
 
 implicit none
 #include "rasdim.fh"
-#include "general.fh"
 integer(kind=iwp), intent(in) :: jRoot
 real(kind=wp), intent(inout) :: S1(lRoots,lRoots), S2(lRoots,lRoots)
 real(kind=wp), intent(in) :: CI_vec(nConf)
