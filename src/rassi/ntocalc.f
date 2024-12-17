6ed887da50eda5b94fe9bdf84b1b0bef6d2a118b
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-17 21:54:04 +0100

    Remove rassi.fh.

--- src/rassi/ntocalc.f
+++ src/rassi/ntocalc.f
@@ -47,9 +47,9 @@
       use stdalloc, only: mma_allocate, mma_deallocate
       use Symmetry_Info, only: nSym=>nIrrep
       use Constants, only: Two
+      use rassi_data, only: NASHT,NBST,NISHT,NCMO,NASH,NBASF,NISH,NOSH
 
       Implicit None
-#include "rassi.fh"
 
       Integer ISpin,JOB1,JOB2
       Integer IState, jState
