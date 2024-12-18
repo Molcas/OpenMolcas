70c239bab30e59766b2c3ca4ad436ff88e34ba83
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-18 16:21:28 +0100

    Remove csmprd\.fh.

--- src/lucia_util/csm_data.F90
+++ src/lucia_util/csm_data.F90
@@ -9,7 +9,11 @@
 ! LICENSE or in <http://www.gnu.org/licenses/>.                        *
 !***********************************************************************
 Module CSM_data
-!stuff from cms.fh
+use lucia_data, only: mxpobs
+Private mxpobs
+
+
+!stuff from csm.fh
 !        NSMSX        :        Number of symmetries single ex  (nIrrep)
 !        NSMDX        :        Number of symmetries double ex  (nIrrep)
 !        NSMST        :         Number of symmetries string        (nIrrep)
@@ -19,4 +23,17 @@ Module CSM_data
 !        ITSDX        :        Total symmetrix double excitation (1)
 !        ITSXT        :         Not in use                          (1)
 Integer    NSMSX,NSMDX,NSMST,NSMCI,NSMXT,ITSSX,ITSDX,ITSXT
+
+#include "csmprd.fh"
+!stuff from csmprd.fh
+!        ADASX        : symmetry operator         initialized in syminf
+!        ASXAD        :                           initialized in syminf
+!        ADSXA        :                           initialized in syminf
+!        SXSXDX       :                           initialized in syminf
+!        SXDXSX       :                           initialized in syminf
+!
+!integer ADASX(MXPOBS,MXPOBS),ASXAD(MXPOBS,2*MXPOBS),              &
+!              ADSXA(MXPOBS,2*MXPOBS),                             &
+!              SXSXDX(2*MXPOBS,2*MXPOBS),SXDXSX(2*MXPOBS,4*MXPOBS)
+
 End Module CSM_data
