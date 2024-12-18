613f546eac2e28099d5c71253ab953c26e934998
Roland Lindh <roland.lindh@kemi.uu.se>
2024-12-18 15:55:49 +0100

    Remove csm\.fh.

--- src/lucia_util/getstr_totsm_spgp.f
+++ src/lucia_util/getstr_totsm_spgp.f
@@ -16,6 +16,7 @@
       use lucia_data, only: NGAS
       use lucia_data, only: IBSPGPFTP,ISPGPFTP,NELFGP
       use lucia_data, only: MXPNGAS,MXPNSMST
+      use csm_data, only: NSMST
 *
 * Obtain all super-strings of given total symmetry and given
 * occupation in each GAS space
@@ -49,8 +50,6 @@
 *
       IMPLICIT NONE
       INTEGER ISTRTP,ISPGRP,ISPGRPSM,NEL,NSTR,NORBT,IDOREO
-!*. Input
-#include "csm.fh"
       INTEGER IZ(NORBT,NEL)
 *. output
       INTEGER ISTR(*), IREO(*)
