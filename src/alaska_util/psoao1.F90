c2e14fba3ffe41eca120a050c1a14b7ab0e46425
Roland Lindh <roland.lindh@kemi.uu.se>
2025-01-02 14:32:35 +0100

    cleanup.

--- src/alaska_util/psoao1.F90
+++ src/alaska_util/psoao1.F90
@@ -12,8 +12,8 @@
 !               1990, IBM                                              *
 !***********************************************************************
 
-subroutine PSOAO1(nSO,MemPrm,MemMax,iFnc,iBas,iBsInc,jBas,jBsInc,kBas,kBsInc,lBas,lBsInc,iPrim,iPrInc,jPrim, &
-                  jPrInc,kPrim,kPrInc,lPrim,lPrInc,ipMem1,ipMem2,Mem1,Mem2,MemPSO,nSD,iSD4)
+subroutine PSOAO1(nSO,MemPrm,MemMax,iFnc,iBsInc,jBsInc,kBsInc,lBsInc,iPrInc, &
+                  jPrInc,kPrInc,lPrInc,ipMem1,ipMem2,Mem1,Mem2,MemPSO,nSD,iSD4)
 !***********************************************************************
 !                                                                      *
 !  Object: to partion the SO and AO block. It will go to some length   *
@@ -47,13 +47,12 @@ use Index_Functions, only: nTri_Elem1
 use Definitions, only: iwp, u6
 
 implicit none
-integer(kind=iwp), intent(in) :: nSO, MemPrm, MemMax, iBas, jBas, kBas, lBas, iPrim, jPrim, kPrim, &
-                                 lPrim, ipMem1, nSD, iSD4(0:nSD,4)
+integer(kind=iwp), intent(in) :: nSO, MemPrm, MemMax, ipMem1, nSD, iSD4(0:nSD,4)
 integer(kind=iwp), intent(out) :: iFnc(4), iBsInc, jBsInc, kBsInc, lBsInc, iPrInc, jPrInc, kPrInc, lPrInc, ipMem2, Mem1, Mem2, &
                                   MemPSO
 integer(kind=iwp) :: i1, iCmp, iFac, iiBas(4), IncVec, iTmp1, j, jCmp, jPam, kCmp, kSOInt, la, lb, lc, lCmp, ld, lSize, mabcd, &
                      Mem0, Mem3, MemAux, MemAux0, MemDeP, MemRys, MemScr, MemSph, MemTrn, nA2, nA3, nabcd, nCache, nFac, &
-                     nPam(4,0:7), nTmp1, nTmp2, nVec1, iAO(4), iCmpa(4)
+                     nPam(4,0:7), nTmp1, nTmp2, nVec1, iAO(4), iCmpa(4), iBas, jBas, kBas, lBas, iPrim, jPrim, kPrim, lPrim
 logical(kind=iwp) :: Fail, QiBas, QjBas, QjPrim, QkBas, QlBas, QlPrim
 integer(kind=iwp), external :: MemTra
 #include "Molcas.fh"
@@ -67,6 +66,17 @@ iCmp = iSD4(2,1)
 jCmp = iSD4(2,2)
 kCmp = iSD4(2,3)
 lCmp = iSD4(2,4)
+
+iBas = iSD4(3,1)
+jBas = iSD4(3,2)
+kBas = iSD4(3,3)
+lBas = iSD4(3,4)
+
+iPrim = iSD4(5,1)
+jPrim = iSD4(5,2)
+kPrim = iSD4(5,3)
+lPrim = iSD4(5,4)
+
 
 iAO(:)  =iSD4(7,:)
 iCmpa(:)=iSD4(2,:)
