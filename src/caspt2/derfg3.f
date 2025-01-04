Molcas/OpenMolcas

commit e08defc176d47e5cec73b3cceedfbea4e6d704f6

Fix NAG compiler things

src/caspt2/derfg3.f
@@ -771,24 +771,27 @@ C
 
       IMPLICIT NONE
 
-      do ib=1,ibuf1
-        idx=ip1_buf(ib)
+      integer(kind=iwp) :: IALEVloc, IBLEVloc, ibloc
+
+      do ibloc=1,ibuf1
+        idx=ip1_buf(ibloc)
         itlev=idx2ij(1,idx)
         iulev=idx2ij(2,idx)
         !! left derivative
         CALL SIGMA1(SGS,CIS,EXS,
-     &              ITLEV,IULEV,1.0D00,STSYM,DTU(1,ib),CLAG)
+     &              ITLEV,IULEV,1.0D00,STSYM,DTU(1,ibloc),CLAG)
         !! the rest is DEPSA contribution
-        Do IALEV = 1, NLEV
-          Do IBLEV = 1, NLEV
+        Do IALEVloc = 1, NLEV
+          Do IBLEVloc = 1, NLEV
             BUF2(:) = 0.0d+00
             CALL SIGMA1(SGS,CIS,EXS,
-     &                  IALEV,IBLEV,1.0D00,STSYM,DAB(1,ib),BUF2)
-            DEPSA(IALEV,IBLEV) = DEPSA(IALEV,IBLEV)
+     &                 IALEVloc,IBLEVloc,1.0D00,STSYM,DAB(1,ibloc),BUF2)
+            DEPSA(IALEVloc,IBLEVloc) = DEPSA(IALEVloc,IBLEVloc)
      *        + DDot_(nsgm1,BUF1(1,IB),1,BUF2,1)
           End Do
         End Do
       end do
+
       END SUBROUTINE LEFT_DEPSA
 
       END SUBROUTINE DERFG3

src/ri_util/mult_with_q_caspt2.F90
@@ -378,9 +378,9 @@ contains
 
   implicit none
 
-  integer(kind=iwp) :: i3, i4, ij, ijQ, ijS, ij_Shell, iiQ, iS, iSO, iSkal, iSym_, indS, ish, jS, jSkal, jjQ, jsh, &
-                       kAO, kAOk, kBas, kBsInc, kCmp, kS, klS, kSO, kSO0, kSOk, &
-                       lAO, lAOl, lBas, lBsInc, lCmp, lMaxDens, lS, lSO, lSO0, lSOl, &
+  integer(kind=iwp) :: i3, i4, ij, ijQ, ijS, ij_Shell, iiQ, iloc, iS, iSO, iSkal, iSym_, indS, ish, jloc, jS, jSkal, jjQ, jsh, &
+                       kAO, kAOk, kBas, kBsInc, kCmp, kS, klS, kSO, kSOk, &
+                       lAO, lAOl, lBas, lBsInc, lCmp, lMaxDens, lS, lSO, lSOl, &
                        nij_Shell, nSkal, nSkal_Auxiliary, nSkal_Valence
   real(kind=wp) :: A_int_ij, Dm_ij, ThrAO, TMax_all, XDm_ii, XDm_ij, XDm_jj, XDm_max
   logical(kind=iwp) :: DoFock, DoGrad, Indexation
@@ -427,14 +427,14 @@ contains
   do iSym_=0,nSym-1
     kS = 1+nSkal_Valence*iSym_ ! note diff wrt declaration of iBDsh
 !   do j=1,nBas(iSym_)
-    do j=1,nBas(1)
-      jsh = Cho_Irange(j,iBDsh(kS),nSkal_Valence,.true.)
-      do i=1,j
+    do jloc=1,nBas(1)
+      jsh = Cho_Irange(jloc,iBDsh(kS),nSkal_Valence,.true.)
+      do iloc=1,jloc
         ish = Cho_Irange(i,iBDsh(kS),nSkal_Valence,.true.)
         ijS = nTri_Elem(jsh-1)+ish
         do iSO=1,nJDens
           if (.not. DMLT(iSO)%Active) cycle
-          ij = iTri(j,i)
+          ij = iTri(jloc,iloc)
           Dm_ij = abs(DMLT(iSO)%SB(iSym_+1)%A1(ij))
           MaxDens(ijS) = max(MaxDens(ijS),Dm_ij)
         end do
@@ -507,8 +507,6 @@ contains
       lBsInc = lBas
     end if
 
-    lSO0 = iAOtSO(lAO+1,0)-1
-    kSO0 = iAOtSO(kAO+1,0)-1
     do i4=1,lCmp
       lSO = iAOtSO(lAO+i4,0)
       do i3=1,kCmp
@@ -535,10 +533,10 @@ contains
 #endif
   kS = 1
   nij_Shell = 0
-  do i = 1, NPROCS
-    lS = nList_Shell(i)
+  do iloc = 1, NPROCS
+    lS = nList_Shell(iloc)
     nij_Shell = nij_Shell + lS
-    nList_Shell(i) = kS
+    nList_Shell(iloc) = kS
     kS = kS + lS
   end do
 
@@ -549,10 +547,10 @@ contains
 #endif
   kS = 1
   nij_Shell = 0
-  do i = 1, NPROCS
-    lS = nList_AO(i)
+  do iloc = 1, NPROCS
+    lS = nList_AO(iloc)
     nij_Shell = nij_Shell + lS
-    nList_AO(i) = kS
+    nList_AO(iloc) = kS
     kS = kS + lS
   end do
   nCalAO_tot = kS-1

src/caspt2/clagx.f
@@ -155,15 +155,14 @@ C
       use caspt2_global, only: imag_shift, iVecL,
      *                         sigma_p_epsilon, LUSBT,
      *                         LUSOLV, ipea_shift
-#ifdef _MOLCAS_MPP_
-      use caspt2_global, only: do_lindep, idSDMat, LUSTD, real_shift
-#endif
       use EQSOLV
       use Sigma_data
       use stdalloc, only: mma_allocate, mma_deallocate
-      use definitions, only: iwp,wp,u6
+      use definitions, only: wp
       use fake_GA, only: GA_Arrays
 #ifdef _MOLCAS_MPP_
+      use caspt2_global, only: do_lindep, idSDMat, LUSTD, real_shift
+      use definitions, only: u6
       USE Para_Info, ONLY: Is_Real_Par, King
 #endif
       IMPLICIT REAL*8 (A-H,O-Z)
@@ -547,12 +546,12 @@ C
           EX = EPSA(iXabs)
           EY = EPSA(iYabs)
           ATUXY = EASUM-ET-EU-EX-EY
-          iBadr = iTU + nAS*(iXY-1)
+C         iBadr = iTU + nAS*(iXY-1)
           BDERval = BDER(ITU,IXY)
 C
           !! For IPEA shift
           If (iTU.eq.iXY.and.ipea_shift.ne.0.0D+00) Then
-            idT=(iTabs*(iTabs+1))/2
+C           idT=(iTabs*(iTabs+1))/2
             ! idU=(iUabs*(iUabs+1))/2
             NSEQ = iTU*(iTU+1)/2
             bsBDER = ipea_shift*0.5D+00*BDERval
@@ -985,7 +984,7 @@ C
 C
           BDERval = BDER(ITU,IXY)
           If (iTU.eq.iXY.and.ipea_shift.ne.0.0D+00) Then
-            idT=(iTabs*(iTabs+1))/2
+C           idT=(iTabs*(iTabs+1))/2
             ! idU=(iUabs*(iUabs+1))/2
             NSEQ = iTU*(iTU+1)/2
             bsBDER = ipea_shift*0.5D+00*BDERval
@@ -1529,7 +1528,7 @@ C
 C
       real(kind=wp),allocatable :: WRK1(:),WRK2(:),WRK3(:),TRANS(:),
      *                             EIG(:)
-      nAshI = nAsh(iSym)
+C     nAshI = nAsh(iSym)
       call mma_allocate(WRK1,nAS**2,Label='WRK1')
       call mma_allocate(WRK2,MAX(nAS**2,nAS*nIS),Label='WRK2')
       call mma_allocate(WRK3,nAS**2,Label='WRK3')

src/caspt2/dens.f
@@ -1798,8 +1798,9 @@ C
       use caspt2_global, only: LuGAMMA,LuAPT2
       use ChoCASPT2
       use stdalloc, only: mma_allocate,mma_deallocate
-      use definitions, only: iwp,wp
+      use definitions, only: wp
 #ifdef _MOLCAS_MPP_
+      use definitions, only: iwp
       USE Para_Info, ONLY: Is_Real_Par, nProcs, myRank
 #endif
 C
@@ -1833,7 +1834,6 @@ C
       Do jSym = 1, nSym
         NumChoTot = NumChoTot + NumCho_PT2(jSym)
       End Do
-      NumCho=NumChoTot
       Do jSym = 1, nSym
         iSkip(jSym) = 1
 #ifdef _MOLCAS_MPP_

src/caspt2/olagns_ri.f
@@ -77,9 +77,9 @@ C
       use ChoCASPT2
       use stdalloc, only: mma_allocate, mma_deallocate
       use definitions, only: iwp,wp
-      use fake_GA, only: Allocate_GA_Array, Deallocate_GA_Array,
-     &                   GA_Arrays
+      use fake_GA, only: GA_Arrays
 #ifdef _MOLCAS_MPP_
+      use fake_GA, only: Allocate_GA_Array, Deallocate_GA_Array
       USE Para_Info, ONLY: Is_Real_Par, King
 #endif
 C

src/integral_util/doreadbpt2.F90
@@ -16,7 +16,7 @@ subroutine DoReadBPT2(kS,lS)
 ! All elements of the Jth shell (auxiliary functions) are read
 ! Only for C1 symmetry
 
-  use Definitions, only: wp, iwp
+  use Definitions, only: iwp
   use iSD_data, only: iSD
   use Gateway_global, only: force_part_c
   use pso_stuff, only: B_PT2, LuGamma2, nBasA, nCalAO, ReadBPT2
@@ -24,9 +24,7 @@ subroutine DoReadBPT2(kS,lS)
 
   implicit none
 
-! real(kind=wp), intent(out) :: B_PT2(nBasA,MxInShl,MxInShl)
   integer(kind=iwp), intent(in) :: kS, lS
-! integer(kind=iwp), intent(inout) :: nCalAO
   integer(kind=iwp) :: i3, i4, kAO, kAOk, kBas, kBsInc, kCmp, kSO, kSOk, kSO0, lAO, lAOl, lBas, lBsInc, lCmp, lSO, lSOl, lSO0
 
   kCmp = iSD(2,kS)

commit 0d25d48cfb5057f208d28fde3da996ef78d88e4a

Fixed bugs introduced in e08defc1

src/caspt2/derfg3.f
@@ -328,6 +328,7 @@ C     write(6,*) "PREP    : CPU/WALL TIME=", cput,wallt
 
       iG3OFF=0
       iTask_loc = 1
+      first = .true.
 * A *very* long loop over the symmetry of Sgm1 = E_ut Psi as segmentation.
 * This also allows precomputing the Hamiltonian (H0) diagonal elements.
       DO issg1=1,nsym
@@ -787,7 +788,7 @@ C
             CALL SIGMA1(SGS,CIS,EXS,
      &                 IALEVloc,IBLEVloc,1.0D00,STSYM,DAB(1,ibloc),BUF2)
             DEPSA(IALEVloc,IBLEVloc) = DEPSA(IALEVloc,IBLEVloc)
-     *        + DDot_(nsgm1,BUF1(1,IB),1,BUF2,1)
+     *        + DDot_(nsgm1,BUF1(1,IBloc),1,BUF2,1)
           End Do
         End Do
       end do

src/ri_util/mult_with_q_caspt2.F90
@@ -430,7 +430,7 @@ contains
     do jloc=1,nBas(1)
       jsh = Cho_Irange(jloc,iBDsh(kS),nSkal_Valence,.true.)
       do iloc=1,jloc
-        ish = Cho_Irange(i,iBDsh(kS),nSkal_Valence,.true.)
+        ish = Cho_Irange(iloc,iBDsh(kS),nSkal_Valence,.true.)
         ijS = nTri_Elem(jsh-1)+ish
         do iSO=1,nJDens
           if (.not. DMLT(iSO)%Active) cycle