13286f5ab615bca40b869e49b3143e9d51a6699e
Lila Zapp <lilajulika.zapp@gmail.com>
2025-03-06 10:28:01 +0100

    interface

--- src/scf/s-gek-optimizer.F90
+++ src/scf/s-gek-optimizer.F90
@@ -34,6 +34,37 @@ use Constants, only: One
 #endif
 
 implicit none
+
+!===========================================================================================================================
+interface
+    subroutine GEK_Optimizer(mDiis,nDiis,Max_Iter,q_diis,g_diis,dq_diis,Energy, H_diis, dqdq, Step_Trunc, UpMeth)
+        use Kriging_mod, only: blaAI, blAI, blavAI, mblAI
+        use Kriging_procedures, only: Setup_Kriging
+        use Index_Functions, only: iTri, nTri_Elem
+        use stdalloc, only: mma_allocate, mma_deallocate
+        use Constants, only: Zero, Half, One, Two, Four, Six, Ten
+        use Definitions, only: iwp, wp, u6
+
+        implicit none
+        integer(kind=iwp), intent(in) :: nDiis, mDiis, Max_Iter
+        real(kind=wp), intent(inout) :: q_diis(mDiis,nDiis+Max_Iter),g_diis(mDiis,nDiis+Max_Iter), dq_diis(mDiis), &
+                                        Energy(nDiis+Max_Iter),dqdq
+        character(len=6), intent(inout) :: UpMeth
+        character, intent(inout) :: Step_Trunc
+
+        integer(kind=iwp) :: i, j, k, ii, Iteration_Micro, Iteration_Total, Iteration
+        real(kind=wp) :: Beta_Disp, dqHdq, FAbs, Fact, RMS, RMSMx, StepMax, Variance(1), H_diis(mDiis,mDiis)
+        real(kind=wp), parameter :: Beta_Disp_Min = 5.0e-3_wp, Beta_Disp_Seed = 0.05_wp, StepMax_Seed = 0.1_wp,&
+                                    Thr_RS = 1.0e-7_wp, ThrGrd = 1.0e-7_wp
+        real(kind=wp), allocatable :: Val(:), Vec(:,:)
+        real(kind=wp), external :: DDot_
+        character :: Step_Trunc_
+        character(len=6) :: UpMeth_
+        logical(kind=iwp) :: Converged, Terminate
+    end subroutine
+end interface
+!===========================================================================================================================
+
 !just used in inner subroutine, but outer uses it as input
 character(len=6), intent(inout) :: UpMeth
 character, intent(inout) :: Step_Trunc
@@ -296,7 +327,8 @@ call mma_deallocate(q)
 write(u6,*) 'Exit S-GEK Optimizer'
 #endif
 
-contains
+end subroutine S_GEK_Optimizer
+
 subroutine GEK_Optimizer(mDiis,nDiis,Max_Iter,q_diis,g_diis,dq_diis,Energy, H_diis, dqdq, Step_Trunc, UpMeth)
 
 use Kriging_mod, only: blaAI, blAI, blavAI, mblAI
@@ -544,5 +576,3 @@ call RecPrt('g_diis(:,Iteration+1)',' ',g_diis(:,Iteration+1),size(g_diis,1),1)
 #endif
 
 end subroutine GEK_Optimizer
-
-end subroutine S_GEK_Optimizer
