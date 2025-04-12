2eaca65f71a460b1efbad6718c9d6486e21d5261
Lila Zapp <lilajulika.zapp@gmail.com>
2025-04-12 16:21:11 +0200

    cleanup getgradpm code

--- src/localisation_util/getgrad_pm.F90
+++ src/localisation_util/getgrad_pm.F90
@@ -16,7 +16,7 @@ subroutine GetGrad_PM(nAtoms,nOrb2Loc,PA,GradNorm,Rmat,Debug)
 !
 ! Purpose: compute the gradient of the Pipek-Mezey functional.
 
-use Constants, only: Zero, Four
+use Constants, only: Zero, Two, Four
 use Definitions, only: wp, iwp, u6
 
 implicit none
@@ -26,33 +26,9 @@ real(kind=wp), intent(out) :: GradNorm, Rmat(nOrb2Loc,nOrb2Loc)
 logical(kind=iwp), intent(in) :: Debug
 integer(kind=iwp) :: i, iAtom, j
 real(kind=wp) :: Fun, Rjj
-real(kind=wp) :: Grad(nOrb2Loc,nOrb2Loc)
+real(kind=wp) :: Grad(nOrb2Loc,nOrb2Loc), newGradNorm
 
 
-RMat(:,:) = Zero
-do iAtom=1,nAtoms
-  do j=1,nOrb2Loc
-    Rjj = PA(j,j,iAtom)
-    do i=1,nOrb2Loc
-      Rmat(i,j) = Rmat(i,j)+PA(i,j,iAtom)*Rjj
-    end do
-  end do
-end do
-
-if (Debug) then
-    write(u6,*) ' '
-    write(u6,*) 'In GetGrad_PM'
-    write(u6,*) '-------------'
-    call RecPrt('RMat',' ',RMat(:,:), nOrb2Loc, nOrb2Loc)
-end if
-
-GradNorm = Zero
-do i=1,nOrb2Loc-1
-  do j=i+1,nOrb2Loc
-    GradNorm = GradNorm + (Rmat(i,j) - Rmat(j,i))**2
-  end do
-end do
-GradNorm = Four*sqrt(GradNorm) !I don't know about the sqrt
 
 !New gradient calculation according to DOI: 10.1002/jcc.23281 equation (15)
 !the Grad matrix is antisymmetric
@@ -72,12 +48,41 @@ if (Debug) then
 end if
 
 
-if (Debug) then
-  Fun = Zero
-  do i=1,nOrb2Loc
-    Fun = Fun+Rmat(i,i)
+
+!Calculate the R matrix used to calculate the gradient norm
+RMat(:,:) = Zero
+do iAtom=1,nAtoms
+  do j=1,nOrb2Loc
+    Rjj = PA(j,j,iAtom)
+    do i=1,nOrb2Loc
+      Rmat(i,j) = Rmat(i,j)+PA(i,j,iAtom)*Rjj
+    end do
   end do
-  write(u6,*) 'GetGrad_PM: functional = Tr(R) = ',Fun
+end do
+
+
+! Trace of Rmat is the value of the PM functional
+if (Debug) then
+    write(u6,*) ' '
+    write(u6,*) 'In GetGrad_PM'
+    write(u6,*) '-------------'
+    call RecPrt('RMat',' ',RMat(:,:), nOrb2Loc, nOrb2Loc)
+
+    do i=1,nOrb2Loc
+        Fun = Fun+Rmat(i,i)
+    end do
+    write(u6,*) 'GetGrad_PM: functional = Tr(R) = ',Fun
 end if
+
+
+!The Gradient Norm (always positive!) is used as threshold criterium
+GradNorm = Zero
+do i=1,nOrb2Loc-1
+  do j=i+1,nOrb2Loc
+    GradNorm = GradNorm + (Rmat(i,j) - Rmat(j,i))**2 !square to avoid negative norm
+  end do
+end do
+GradNorm = Four*sqrt(GradNorm) !sqrt to complement previous **2
+
 
 end subroutine GetGrad_PM
