dce8eb4af2f24279934f74905847ccfa6d15e756
Roland Lindh <roland.lindh@kemi.uu.se>
2026-01-21 17:53:36 +0100

    Remove reference to rasdim.fh.

--- src/molcas_ci_util/rasdim.F90
+++ src/molcas_ci_util/rasdim.F90
@@ -11,21 +11,19 @@
 !
 Module RASDim
 use Molcas
-#include "rasdim.fh"
-!#include "Molcas.fh"
-!!     Parameter definitions
-!!     Note, these parameters define the size of the problems which
-!!     can be treated by the RASSCF program. Changing any of these
-!!     requires, that the program is totaly recompiled and linked.
-!!
-!      Integer, Parameter :: mxRef  = 5
-!!...  max number of reference configurations in root selectioning
-!      Integer, Parameter :: mxIter = 200
-!!...  max number of macro iterations
-!      Integer, Parameter :: mxCiIt = 502
-!!...  max number of micro iterations for the CI section
-!      Integer, Parameter :: mxSxIt = 100
-!!...  max number of micro iterations for the SX section
-!      Integer, Parameter :: mxTit  = 1
-!!...  max number of title lines
+!     Parameter definitions
+!     Note, these parameters define the size of the problems which
+!     can be treated by the RASSCF program. Changing any of these
+!     requires, that the program is totaly recompiled and linked.
+!
+      Integer, Parameter :: mxRef  = 5
+!...  max number of reference configurations in root selectioning
+      Integer, Parameter :: mxIter = 200
+!...  max number of macro iterations
+      Integer, Parameter :: mxCiIt = 502
+!...  max number of micro iterations for the CI section
+      Integer, Parameter :: mxSxIt = 100
+!...  max number of micro iterations for the SX section
+      Integer, Parameter :: mxTit  = 1
+!...  max number of title lines
 End Module RASDim
