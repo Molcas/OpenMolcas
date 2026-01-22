d60295181a3e25f591b6c34c55237510044dd32d
Roland Lindh <roland.lindh@kemi.uu.se>
2026-01-22 22:33:15 +0100

    Remove common blocks.

--- src/molcas_ci_util/caspt2_module.F90
+++ src/molcas_ci_util/caspt2_module.F90
@@ -17,76 +17,64 @@ use definitions, only: wp, iwp
       integer(kind=iwp), Parameter:: MxState=MxRoot, MxExt=MxBas-MxAct
       real(kind=wp)ENERGY(MXSTATE), REFENE(MXSTATE)
       logical(kind=iwp)  lOPTO,RHSDIRECT,                               &
-     &    RFPERT,G1SECIN,PRORB,PRSD,Found2,IfDirect,IfChol,             &
-     &    MODE_DGA_COMPAT,DoCumulant,JMS
+          RFPERT,G1SECIN,PRORB,PRSD,Found2,IfDirect,IfChol,             &
+          MODE_DGA_COMPAT,DoCumulant,JMS
       real(kind=wp)THRCONV,THRSHN,THRSHS,THRSHF,THRENE,THROCC,          &
-     &    POTNUC,ECORE,EREF,ERFSELF
+          POTNUC,ECORE,EREF,ERFSELF
 
       logical(kind=iwp) IFDENS,IFMIX,IFPROP,IFMSCOUP,                   &
-     &    IFXMS,IFRMS,IFDW,IFSILPRROT,IFSADREF,IFDORTHO,                &
-     &    IFINVAR,DMRG
+          IFXMS,IFRMS,IFDW,IFSILPRROT,IFSADREF,IFDORTHO,                &
+          IFINVAR,DMRG
 
 ! Note: MUL(8,8) is just a copy of Mul in the Symmetry_Info module
       integer(kind=iwp)MAXIT,NTIT,NACTEL,ISPIN,NSYM,STSYM,NCONF,NUNIQAT,&
-     &    NFRO(8),NFROT,NISH(8),NIES(8),NISHT,NRAS1(8),NRAS1T,NDET,     &
-     &    NRAS2(8),NRAS2T,NRAS3(8),NRAS3T,NASH(8),NAES(8),NASHT,        &
-     &    NOSH(8),NOSHT,NSSH(8),NSES(8),NSSHT,NORB(8),NORBT,NOTRI,      &
-     &    NDEL(8),NDELT,NBAS(8),NBAST,NBTRI,NIMX,NAMX,NSMX,NOMX,NBMX,   &
-     &    NINABX,NSECBX,                                                &
-     &    NOSQT,NBSQT,MUL(8,8),ISCF,                                    &
-     &    IISYM(MXINA),IASYM(MXACT),IESYM(MXEXT),IINAIS(MXINA),         &
-     &    IACTIS(MXACT),IEXTIS(MXEXT),                                  &
-     &    NSTATE,MSTATE(MXSTATE),JSTATE,LROOTS,NROOTS,IROOT(MXROOT),    &
-     &    ROOT2STATE(MXROOT),                                           &
-     &    iRlxRoot,NGROUP,NGROUPSTATE(MXSTATE),                         &
-     &    IAD1M(64),IEOF1M,NELE3,NHOLE1,IFQCAN,                         &
-     &    NLYROOT,NLYGROUP,                                             &
-     &    DWTYPE
+          NFRO(8),NFROT,NISH(8),NIES(8),NISHT,NRAS1(8),NRAS1T,NDET,     &
+          NRAS2(8),NRAS2T,NRAS3(8),NRAS3T,NASH(8),NAES(8),NASHT,        &
+          NOSH(8),NOSHT,NSSH(8),NSES(8),NSSHT,NORB(8),NORBT,NOTRI,      &
+          NDEL(8),NDELT,NBAS(8),NBAST,NBTRI,NIMX,NAMX,NSMX,NOMX,NBMX,   &
+          NINABX,NSECBX,                                                &
+          NOSQT,NBSQT,MUL(8,8),ISCF,                                    &
+          IISYM(MXINA),IASYM(MXACT),IESYM(MXEXT),IINAIS(MXINA),         &
+          IACTIS(MXACT),IEXTIS(MXEXT),                                  &
+          NSTATE,MSTATE(MXSTATE),JSTATE,LROOTS,NROOTS,IROOT(MXROOT),    &
+          ROOT2STATE(MXROOT),                                           &
+          iRlxRoot,NGROUP,NGROUPSTATE(MXSTATE),                         &
+          IAD1M(64),IEOF1M,NELE3,NHOLE1,IFQCAN,                         &
+          NLYROOT,NLYGROUP,                                             &
+          DWTYPE
       real(kind=wp) EPS(MXORB),EPSI(MXINA),EPSA(MXACT),EPSE(MXEXT),     &
-     &    EASUM,DENORM,ZETA,                                            &
-     &    ECOMM,E2CORR,E2TOT,RNORM,REFWGT
+          EASUM,DENORM,ZETA,                                            &
+          ECOMM,E2CORR,E2TOT,RNORM,REFWGT
 
       integer(kind=iwp) NSTUV(8),NSTU(8),NSTGEU(8),NSTGTU(8),           &
-     &    NTUV(8),NTU(8),NTGEU(8),NTGTU(8),                             &
-     &    NIGEJ(8),NIGTJ(8),NAGEB(8),NAGTB(8),NTUVES(8),NTUES(8),       &
-     &    NTGEUES(8),NTGTUES(8),NIGEJES(8),NIGTJES(8),NAGEBES(8),       &
-     &    NAGTBES(8),NIAES(8)
+          NTUV(8),NTU(8),NTGEU(8),NTGTU(8),                             &
+          NIGEJ(8),NIGTJ(8),NAGEB(8),NAGTB(8),NTUVES(8),NTUES(8),       &
+          NTGEUES(8),NTGTUES(8),NIGEJES(8),NIGTJES(8),NAGEBES(8),       &
+          NAGTBES(8),NIAES(8)
 ! Excitation operators, sizes and offsets
       integer(kind=iwp), Parameter:: MXCASE=13
       integer(kind=iwp) NCASES,NASUP(8,MXCASE),NISUP(8,MXCASE),         &
-     &    NINDEP(8,MXCASE),NEXC(8,MXCASE),NEXCES(8,MXCASE),             &
-     &    NBTCH(8),NBTCHES(8),                                          &
-     &    NSCP,NSCQ,NBATCH_TOT,NJSCT,NJSCT_TOT,                         &
-     &    IOFFRHS(8,MXCASE)
+          NINDEP(8,MXCASE),NEXC(8,MXCASE),NEXCES(8,MXCASE),             &
+          NBTCH(8),NBTCHES(8),                                          &
+          NSCP,NSCQ,NBATCH_TOT,NJSCT,NJSCT_TOT,                         &
+          IOFFRHS(8,MXCASE)
       CHARACTER(Len=32) HZERO
       CHARACTER(LEN=4) TITLE(18,MxTIT)
       CHARACTER(LEN=LENIN8) NAME (2*MXORB)
       CHARACTER(LEN=2) HEADER (72)
 
       CHARACTER(Len=8) CASES(MXCASE),FOCKTYPE,SMATRIX,SDECOM,           &
-     &    BMATRIX,BTRANS,BSPECT,ORBIT,ORBIN,OUTFMT,                     &
-     &    ORBNAM(MXORB),IINAM(MXINA),IANAM(MXACT),ISNAM(MXEXT)
+          BMATRIX,BTRANS,BSPECT,ORBIT,ORBIN,OUTFMT,                     &
+          ORBNAM(MXORB),IINAM(MXINA),IANAM(MXACT),ISNAM(MXEXT)
 
-      REAL*8                                                            &
-     &    CPUGIN,CPUINT,CPUFMB,                                         &
-     &    CPUSIN,CPUFG3,                                                &
-     &    CPUPT2,CPUSBM,CPUEIG,CPUNAD,CPURHS,CPUSER,                    &
-     &    CPUPCG,CPUSCA,CPULCS,CPUOVL,CPUVEC,CPUSGM,                    &
-     &    CPUPRP,CPUGRD,                                                &
-     &    TIOGIN,TIOINT,TIOFMB,                                         &
-     &    TIOSIN,TIOFG3,                                                &
-     &    TIOPT2,TIOSBM,TIOEIG,TIONAD,TIORHS,TIOSER,                    &
-     &    TIOPCG,TIOSCA,TIOLCS,TIOOVL,TIOVEC,TIOSGM,                    &
-     &    TIOPRP,TIOGRD
-      COMMON/TIMERS_CASPT2/                                             &
-     &    CPUGIN,CPUINT,CPUFMB,                                         &
-     &    CPUSIN,CPUFG3,                                                &
-     &    CPUPT2,CPUSBM,CPUEIG,CPUNAD,CPURHS,CPUSER,                    &
-     &    CPUPCG,CPUSCA,CPULCS,CPUOVL,CPUVEC,CPUSGM,                    &
-     &    CPUPRP,CPUGRD,                                                &
-     &    TIOGIN,TIOINT,TIOFMB,                                         &
-     &    TIOSIN,TIOFG3,                                                &
-     &    TIOPT2,TIOSBM,TIOEIG,TIONAD,TIORHS,TIOSER,                    &
-     &    TIOPCG,TIOSCA,TIOLCS,TIOOVL,TIOVEC,TIOSGM,                    &
-     &    TIOPRP,TIOGRD
+      real(kind=wp) CPUGIN,CPUINT,CPUFMB,                               &
+          CPUSIN,CPUFG3,                                                &
+          CPUPT2,CPUSBM,CPUEIG,CPUNAD,CPURHS,CPUSER,                    &
+          CPUPCG,CPUSCA,CPULCS,CPUOVL,CPUVEC,CPUSGM,                    &
+          CPUPRP,CPUGRD,                                                &
+          TIOGIN,TIOINT,TIOFMB,                                         &
+          TIOSIN,TIOFG3,                                                &
+          TIOPT2,TIOSBM,TIOEIG,TIONAD,TIORHS,TIOSER,                    &
+          TIOPCG,TIOSCA,TIOLCS,TIOOVL,TIOVEC,TIOSGM,                    &
+          TIOPRP,TIOGRD
 End Module CASPT2_Module
