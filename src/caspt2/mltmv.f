d743803b07aaec4e230778a9efd532b8b5618dc1
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-02 13:00:12 +0100

    Cleanup.

--- src/caspt2/mltmv.f
+++ src/caspt2/mltmv.f
@@ -17,14 +17,21 @@
 * SWEDEN                                     *
 *--------------------------------------------*
       SUBROUTINE MLTMV (IMLTOP,LST1,X,F,Y)
+      use definitions, only: iwp, wp
 #ifdef _MOLCAS_MPP_
       USE Para_Info, ONLY: MyRank, nProcs, Is_Real_Par
 #endif
-      use Sigma_data
-      IMPLICIT REAL*8 (A-H,O-Z)
-      DIMENSION X(*),F(*),Y(*)
-      DIMENSION LST1(4,NLST1)
+      use Sigma_data, only: NLST1, INCF1, INCF2, INCX1, INCY2, INCY3,
+     &                      LEN1, LEN2, NFMV, VAL1, INCY1, INCX2
+      IMPLICIT None
+      integer(kind=iwp), intent(in):: IMLTOP
+      real(kind=wp), intent(inout):: X(*),F(*),Y(*)
+      integer(kind=iwp), intent(in):: LST1(4,NLST1)
 
+      integer(kind=iwp) ILST1_IOFF, ILST1_SKIP, ILST1, L1, L2, L3, L4,
+     &                  I, IF, IX, IY
+      real(kind=wp) A, V
+      real(kind=wp), external:: DDot_
 C Given a lists with entries LST1(4,ITEM), ITEM=1,NLST1, the
 C four entries called L1,L2,L3,L4 for short, for a given
 C item, and with V=VAL1(L4),
@@ -110,5 +117,4 @@ C     F(L2,a) := Add V*X(L1,i)*Y(L3,i,a)
 
       NFMV =NFMV +2*NLST1*LEN1*LEN2
 
-      RETURN
-      END
+      END SUBROUTINE MLTMV
