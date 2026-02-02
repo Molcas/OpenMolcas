ee80e9402a32e18b154fbf32481b8daca5c6e0b0
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-02 10:53:34 +0100

    Cleanup.

--- src/caspt2/mltr1.f
+++ src/caspt2/mltr1.f
@@ -116,14 +116,17 @@ C     F(L2,p) := Add V*X(L1,p,q)*Y(L3,q)
      &                     F,NFT,NFA,
      &                     Y,NAS2,jYLo,jYHi)
       use definitions, only: iwp, wp
-      use Sigma_data
-      IMPLICIT REAL*8 (A-H,O-Z)
+      use Sigma_data, only: NLST1, INCX3, VAL1
+      IMPLICIT None
       integer(kind=iwp), intent(in):: IMLTOP, NAS1,NIS1,JXOFF,NFT,NFA,
      &                                NAS2,jYLo,jYHi
       real(kind=wp), intent(inout):: X(NAS1,NIS1),Y(NAS2,jYHi-jYLo+1)
       real(kind=wp), intent(inout):: F(NFT,NFA)
       integer(kind=iwp), intent(in):: LST1(4,NLST1)
 
+      integer(kind=iwp) NA, ILST, L1, L2, L3, L4, JX, I, J, NI
+      real(kind=wp) V, A
+      real(kind=wp), external:: DDot_
 C this routine is adapted to use chunks of a distributed array (lg_Y)
 C for the H case.  The chunks span all rows (NAS2) and columns jYlo to
 C jYHi. The array Y points to the beginning of a chunk.
@@ -184,11 +187,18 @@ C F(L2,p) := Add V*X(L1,p,q)*Y(L3,q)
      &                     X,NAS1,NIS1,JXOFF,
      &                     F,NFT,NFI,
      &                     Y,NAS2,jYLo,jYHi)
-      use Sigma_data
-      IMPLICIT REAL*8 (A-H,O-Z)
-      DIMENSION X(NAS1,NIS1),F(NFT,NFI),Y(NAS2,jYHi-jYLo+1)
-      DIMENSION LST1(4,NLST1)
+      use definitions, only: iwp, wp
+      use Sigma_data, only: NLST1, VAL1, INCX3
+      IMPLICIT None
+      integer(kind=iwp), intent(in):: IMLTOP,NAS1,NIS1,JXOFF,NFT,NFI,
+     &                                NAS2,jYLo,jYHi
+      real(kind=wp), intent(inout):: X(NAS1,NIS1),F(NFT,NFI),
+     &                               Y(NAS2,jYHi-jYLo+1)
+      integer(kind=iwp), intent(in):: LST1(4,NLST1)
 
+      integer(kind=iwp) ILST, L1, L2, L3, L4, JX, I, NI, JY
+      real(kind=wp) V, A
+      real(kind=wp), external:: DDot_
 C this routine is adapted to use chunks of a distributed array (lg_Y)
 C for the H case.  The chunks span all rows (NAS2) and columns jYlo to
 C jYHi. The array Y points to the beginning of a chunk.
@@ -257,21 +267,25 @@ C F(L2,p) := Add V*X(L1,p,q)*Y(L3,q)
      &                   X,NAS1,NIS1,JXOFF,
      &                   F,NFI,NFJ,
      &                   lg_Y,NAS2,NIS2)
+      use definitions, only: iwp, wp
 #ifdef _MOLCAS_MPP_
       USE Para_Info, ONLY: Is_Real_Par
 #endif
-      use Sigma_data
+      use Sigma_data, only: NLST1
       use fake_GA, only: GA_Arrays
-      IMPLICIT REAL*8 (A-H,O-Z)
+      IMPLICIT None
 #ifdef _MOLCAS_MPP_
 #include "global.fh"
 #include "mafdecls.fh"
+      integer(kind=iwp) myRank
+      integer(kind=iwp) iYLo,iYHi,jYLo,jYHi,mY,LDY
 #endif
-      INTEGER KOD, IMLTOP
-      INTEGER LST1(4,NLST1)
-      REAL*8 X(*)
-      INTEGER NAS1,NIS1,JXOFF, NFI,NFJ, lg_Y, NAS2, NIS2
-      REAL*8 F(NFI,NFJ)
+      integer(kind=iwp), intent(in):: KOD, IMLTOP
+      integer(kind=iwp), intent(in):: LST1(4,NLST1)
+      real(kind=wp), intent(inout):: X(*)
+      integer(kind=iwp), intent(in):: NAS1,NIS1,JXOFF, NFI,NFJ, lg_Y,
+     &                                NAS2, NIS2
+      real(kind=wp), intent(inout):: F(NFI,NFJ)
 
 #ifdef _MOLCAS_MPP_
 C SVC: Determine the index ranges of the local chunks of lg_X and lg_Y.
