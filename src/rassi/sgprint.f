5a2d00b05a3182a5ffac3952097402a6902e49c8
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-10 14:57:26 +0100

    Cleanup.

--- src/rassi/sgprint.f
+++ src/rassi/sgprint.f
@@ -9,9 +9,12 @@
 * LICENSE or in <http://www.gnu.org/licenses/>.                        *
 ************************************************************************
       Subroutine SGPrint(SGS)
+      use definitions, only: iwp, u6
       use gugx, only: SGStruct
       implicit real*8 (a-h,o-z)
-      Type (SGStruct) SGS
+      Type (SGStruct), intent(in):: SGS
+
+      integer(kind=iwp) nLev, nVert, MidLev, MVSta, MVEnd, i, iv, ic
 
 C Unpack structure SGS:
       nLev   =SGS%nLev
@@ -20,31 +23,31 @@ C Unpack structure SGS:
       MVSta  =SGS%MVSta
       MVEnd  =SGS%MVEnd
 
-      Write(6,*)' Split-Graph UGA. Graph description:'
-      Write(6,*)' Nr of levels:',nLev
-      Write(6,*)' Orbital symmetry labels:'
-      Write(6,'(1x,30i2)')(SGS%ISm(i),i=1,nLev)
-      Write(6,*)' Nr of vertices:',nVert
-      Write(6,*)
-      Write(6,*)' Vertex    L  N    A  B  C      '//
+      Write(u6,*)' Split-Graph UGA. Graph description:'
+      Write(u6,*)' Nr of levels:',nLev
+      Write(u6,*)' Orbital symmetry labels:'
+      Write(u6,'(1x,30i2)')(SGS%ISm(i),i=1,nLev)
+      Write(u6,*)' Nr of vertices:',nVert
+      Write(u6,*)
+      Write(u6,*)' Vertex    L  N    A  B  C      '//
      &               'Downchain table        Upchain table'
-      Write(6,*)
+      Write(u6,*)
       Do iv=1,nVert
-        Write(6,'(1x,i4,5x,2i3,2x,3i3,5x,4i4,5x,4i4)')iv,
+        Write(u6,'(1x,i4,5x,2i3,2x,3i3,5x,4i4,5x,4i4)')iv,
      &  (SGS%DRT(iv,i-1),i=1,5),
      &  (SGS%Down(iv,ic),ic=0,3),
      &  (SGS%Up  (iv,ic),ic=0,3)
       End Do
-      Write(6,*)
-      Write(6,*)' Mid Level:',MidLev
-      Write(6,*)' Mid Vertices:',MVSta,'...',MVEnd
-      Write(6,*)
-      Write(6,*)' Modified Arc Weight table:'
-      Write(6,*)'           Coupling case number'
-      Write(6,*)' Vertex      0    1    2    3'
-      Write(6,*)
+      Write(u6,*)
+      Write(u6,*)' Mid Level:',MidLev
+      Write(u6,*)' Mid Vertices:',MVSta,'...',MVEnd
+      Write(u6,*)
+      Write(u6,*)' Modified Arc Weight table:'
+      Write(u6,*)'           Coupling case number'
+      Write(u6,*)' Vertex      0    1    2    3'
+      Write(u6,*)
       Do iv=1,nVert
-        Write(6,'(1x,i4,5x,4i5)')
+        Write(u6,'(1x,i4,5x,4i5)')
      &             iv,(SGS%MAW(iv,ic),ic=0,3)
       End Do
       end Subroutine SGPrint
