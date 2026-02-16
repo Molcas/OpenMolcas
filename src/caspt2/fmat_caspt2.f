2ff71eac5662c4b7845786d579d8d8afee1a65bf
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-16 12:48:24 +0100

    Polish.

--- src/caspt2/fmat_caspt2.f
+++ src/caspt2/fmat_caspt2.f
@@ -30,8 +30,7 @@
       integer(kind=iwp) IAD2M(3,36*36)
       integer(kind=iwp) NDIM2M, IDISK, IFSTA, ISYR, NBR, NB3, NBNB,
      &                  ISYS, IS3RS, ISYP, NIP, NOP, NAESP, ISYQ, IS3PQ,
-     &                  ISADDR, IDISK1, IDISK2, IT, ITABS, ITU, IU,
-     &                  IUABS
+     &                  ISADDR, IDISK1, IT, ITABS, ITU, IU, IUABS, nInts
       real(kind=wp) DTU
 
 C COMPUTE THE INACTIVE FOCK MATRIX IN MO BASIS.
@@ -40,8 +39,8 @@ C DEFINITIONS ARE:
 C FIMO(P,Q)= H(P,Q)+ SUM( (2*(PQ,KK)-(PK,QK)) OVER INACTIVE K)
 C WHERE H(P,Q) IS THE CORE FOCK MATRIX OBTAINED FROM THE
 C TRANSFORMATION PART, I.E., THE CI 1-ELECTRON HAMILTONIAN.
-C SIMILARLY, FAMO(P,Q)=
-C  (1/2)*SUM( (2*(PQ,TU)-(PU,QT))*DREF(T,U) OVER ACTIVE T,U)
+C SIMILARLY,
+C FAMO(P,Q)= (1/2)*SUM( (2*(PQ,TU)-(PU,QT))*DREF(T,U) OVER ACTIVE T,U)
 C THIS ROUTINE USES DIRECTLY THE TRANSFORMED INTEGRALS TO SET UP
 C FIMO AND FAMO.
 C CODED 92-12-04 BY MALMQVIST FOR CASPT2, MOLCAS-3 VERSION.
@@ -51,51 +50,12 @@ C CODED 92-12-04 BY MALMQVIST FOR CASPT2, MOLCAS-3 VERSION.
       IDISK=0
       CALL IDAFILE(LUINTM,2,IAD2M,3*36*36,IDISK)
 
-      IFSTA=1
-      DO ISYR=1,NSYM
-        NBR=NORB(ISYR)
-        IF(NBR.EQ.0) CYCLE
-        NB3=(NBR**2+NBR)/2
-        NBNB=NBR**2
-        ISYS=ISYR
-        IS3RS=(ISYR**2+ISYR)/2
-        DO ISYP=1,NSYM
-          NIP=NISH(ISYP)
-          NOP=NOSH(ISYP)
-          NAESP=NAES(ISYP)
-          IF(NOP.EQ.0) CYCLE
-          ISYQ=ISYP
-          IS3PQ=(ISYP*(ISYP+1))/2
-          ISADDR=IS3RS+NDIM2M*(IS3PQ-1)
-          IDISK1=IAD2M(1,ISADDR)
-          IF(IDISK1.EQ.0) THEN
-            WRITE(u6,*)' FMAT: COULOMB INTEGRAL BUFFER MISSING!'
-            WRITE(u6,'(1X,A,4I4)')'SYMMETRY BLOCK:',ISYP,ISYQ,ISYR,ISYS
-            CALL ABEND()
-          END IF
-          DO IT=1,NOP
-            DO IU=1,IT
-              IF(IT.LE.NIP .AND. IT.EQ.IU) THEN
-                CALL DDAFILE(LUINTM,2,BUF,NB3,IDISK1)
-C ADD 2* BUFFER INTO CORRECT POSITION OF  FIMO.
-                CALL DAXPY_(NB3,Two,BUF,1,FIMO(IFSTA),1)
-              ELSE IF(IT.GT.NIP .AND. IT.LE.NOP .AND.
-     &                IU.GT.NIP .AND. IU.LE.NOP) THEN
-                ITABS=NAESP+(IT-NIP)
-                IUABS=NAESP+(IU-NIP)
-                ITU=(ITABS*(ITABS-1))/2 + IUABS
-                DTU=DREF(ITU)
-                IF(IT.EQ.IU) DTU=half*DTU
-                CALL DDAFILE(LUINTM,2,BUF,NB3,IDISK1)
-                CALL DAXPY_(NB3,Two*DTU,BUF,1,FAMO(IFSTA),1)
-              ELSE
-                CALL DDAFILE(LUINTM,0,BUF,NB3,IDISK1)
-              END IF
-            END DO
-          END DO
-        END DO
-        IFSTA=IFSTA+NB3
-      END DO
+      Call Do_Loops(1)
+      Call Do_Loops(2)
+
+      Contains
+      Subroutine Do_Loops(icase)
+      integer(kind=iwp), intent(in):: iCase
 
       IFSTA=1
       DO ISYR=1,NSYM
@@ -113,36 +73,61 @@ C ADD 2* BUFFER INTO CORRECT POSITION OF  FIMO.
           ISYQ=ISYP
           IS3PQ=(ISYP*(ISYP+1))/2
           ISADDR=IS3RS+NDIM2M*(IS3PQ-1)
-          IDISK2=IAD2M(2,ISADDR)
-          IF(IDISK2.EQ.0) THEN
-            WRITE(u6,*)' FMAT: EXCH-1  INTEGRAL BUFFER MISSING!'
-            WRITE(u6,'(1X,A,4I4)')'SYMMETRY BLOCK:',ISYP,ISYQ,ISYR,ISYS
-            CALL ABEND()
+          IDISK1=IAD2M(iCase,ISADDR)
+          IF (IDISK1.EQ.0) THEN
+             If (iCase==1) Then
+               WRITE(u6,*)' FMAT: COULOMB INTEGRAL BUFFER MISSING!'
+             Else
+               WRITE(u6,*)' FMAT: EXCH-1  INTEGRAL BUFFER MISSING!'
+             End If
+             WRITE(u6,'(1X,A,4I4)')'SYMMETRY BLOCK:',ISYP,ISYQ,ISYR,ISYS
+             CALL ABEND()
           END IF
+          If (iCase==1) Then
+             nInts=NB3
+          ELSE
+             nInts=NBNB
+          END IF
+
           DO IT=1,NOP
             DO IU=1,IT
+
+C Process here contributions to FIMO or FAMO.
+
               IF(IT.LE.NIP .AND. IT.EQ.IU) THEN
-                CALL DDAFILE(LUINTM,2,BUF,NBNB,IDISK2)
-                CALL TRIANG(NBR,BUF)
+                CALL DDAFILE(LUINTM,2,BUF,nInts,IDISK1)
+                If (iCase==1) Then
+C ADD 2* BUFFER INTO CORRECT POSITION OF  FIMO.
+                   CALL DAXPY_(NB3,Two,BUF,1,FIMO(IFSTA),1)
+                ELSE
+                   CALL TRIANG(NBR,BUF)
 C ADD -1* BUFFER INTO CORRECT POSITION OF  FIMO.
-                CALL DAXPY_(NB3,-One,BUF,1,FIMO(IFSTA),1)
+                   CALL DAXPY_(NB3,-One,BUF,1,FIMO(IFSTA),1)
+                End If
               ELSE IF(IT.GT.NIP .AND. IT.LE.NOP .AND.
      &                IU.GT.NIP .AND. IU.LE.NOP) THEN
                 ITABS=NAESP+(IT-NIP)
                 IUABS=NAESP+(IU-NIP)
                 ITU=(ITABS*(ITABS-1))/2 + IUABS
                 DTU=DREF(ITU)
-                IF(IT.EQ.IU) DTU=Half*DTU
-                CALL DDAFILE(LUINTM,2,BUF,NBNB,IDISK2)
-                CALL TRIANG(NBR,BUF)
-                CALL DAXPY_(NB3,-DTU,BUF,1,FAMO(IFSTA),1)
+                IF(IT.EQ.IU) DTU=half*DTU
+                CALL DDAFILE(LUINTM,2,BUF,nInts,IDISK1)
+                If (iCase==1) Then
+                   CALL DAXPY_(NB3,Two*DTU,BUF,1,FAMO(IFSTA),1)
+                Else
+                   CALL TRIANG(NBR,BUF)
+                   CALL DAXPY_(NB3,-DTU,BUF,1,FAMO(IFSTA),1)
+                End If
               ELSE
-                CALL DDAFILE(LUINTM,0,BUF,NBNB,IDISK2)
+                CALL DDAFILE(LUINTM,0,BUF,nInts,IDISK1)
               END IF
+
             END DO
           END DO
+
         END DO
         IFSTA=IFSTA+NB3
       END DO
+      End Subroutine Do_Loops
 
       END SUBROUTINE FMAT_CASPT2
