bf7a9cbb661014ac29185bf46719e15e86daec2c
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-22 14:22:36 +0100

    Polish.

--- src/caspt2/mksbmat_g.f
+++ src/caspt2/mksbmat_g.f
@@ -40,6 +40,8 @@ C Set up S and B matrices for cases 1..13.
          RETURN
       END IF
 
+      Call Store_away_parameters()
+
       IF (NASHT/=0) THEN
 
          IF (IPRGLB.GE.DEBUG) THEN
@@ -57,6 +59,7 @@ C Set up S and B matrices for cases 1..13.
          iLUID=0
          CALL I1DAFILE(LUSOLV,2,idxG3,6*NG3,iLUID)
 ***********************************************************************
+         Call Modify_Fock_and_parameters('A')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
@@ -69,7 +72,10 @@ C Set up S and B matrices for cases 1..13.
 
          CALL MKSA(DREF,SIZE(DREF),PREF,SIZE(PREF),NG3,G3,idxG3)
          CALL MKBA(DREF,SIZE(DREF),PREF,SIZE(PREF),FD,FP,NG3,F3,idxG3)
+
+         Call  ReStore_parameters()
 ***********************************************************************
+         Call Modify_Fock_and_parameters('C')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
@@ -83,6 +89,7 @@ C Set up S and B matrices for cases 1..13.
          CALL MKSC(DREF,SIZE(DREF),PREF,SIZE(PREF),NG3,G3,idxG3)
          CALL MKBC(DREF,SIZE(DREF),PREF,SIZE(PREF),FD,FP,NG3,F3,idxG3)
 
+         Call  ReStore_parameters()
 ***********************************************************************
 
          CALL mma_deallocate(idxG3)
@@ -90,6 +97,7 @@ C Set up S and B matrices for cases 1..13.
          CALL mma_deallocate(F3)
 
 ***********************************************************************
+         Call Modify_Fock_and_parameters('B')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
@@ -98,7 +106,10 @@ C Set up S and B matrices for cases 1..13.
 
          CALL MKSB(DREF,SIZE(DREF),PREF,SIZE(PREF))
          CALL MKBB(DREF,SIZE(DREF),PREF,SIZE(PREF),FD,FP)
+
+         Call  ReStore_parameters()
 ***********************************************************************
+         Call Modify_Fock_and_parameters('D')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
@@ -107,24 +118,35 @@ C Set up S and B matrices for cases 1..13.
 
          CALL MKSD(DREF,SIZE(DREF),PREF,SIZE(PREF))
          CALL MKBD(DREF,SIZE(DREF),PREF,SIZE(PREF),FD,FP)
+
+         Call  ReStore_parameters()
 ***********************************************************************
+         Call Modify_Fock_and_parameters('E')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
          CALL MKSE(DREF,SIZE(DREF))
          CALL MKBE(DREF,SIZE(DREF),FD)
+
+         Call  ReStore_parameters()
 ***********************************************************************
+         Call Modify_Fock_and_parameters('F')
          CALL PT2_GET(NG2,'DELTA2',F2)
          CALL MKPREF_RPT2(NASHT,F2,FP)
 
          CALL MKSF(PREF,SIZE(PREF))
          CALL MKBF(DREF,SIZE(DREF),PREF,SIZE(PREF),FP)
+
+         Call  ReStore_parameters()
 ***********************************************************************
+         Call Modify_Fock_and_parameters('G')
          CALL PT2_GET(NG1,'DELTA1',F1)
          CALL MKDREF_RPT2(NASHT,F1,FD)
 
          CALL MKSG(DREF,SIZE(DREF))
          CALL MKBG(DREF,SIZE(DREF),FD)
+
+         Call  ReStore_parameters()
 ***********************************************************************
          CALL mma_deallocate(F2)
          CALL mma_deallocate(F1)
@@ -133,7 +155,46 @@ C Set up S and B matrices for cases 1..13.
 
       END IF
 
+      Call  ReStore_parameters()
+
       CALL MKSH()
       CALL MKBH()
+
+      Contains
+      Subroutine Modify_Fock_and_parameters(CLASS)
+      use definitions, only: iwp
+      Implicit None
+      Character(LEN=1), Intent(in):: CLASS
+      Integer(kind=iwp) iClass
+      iClass=0
+      Select case (CLASS)
+      CASE('A')
+         iClass=1
+      CASE('B')
+         iClass=1
+      CASE('C')
+         iClass=1
+      CASE('D')
+         iClass=1
+      CASE('E')
+         iClass=1
+      CASE('F')
+         iClass=1
+      CASE('G')
+         iClass=1
+      CASE('H')
+         iClass=1
+      CASE DEFAULT
+         Call Abend()
+      End Select
+      End Subroutine Modify_Fock_and_parameters
+
+      Subroutine Store_away_parameters()
+      Implicit None
+      End Subroutine Store_away_parameters
+
+      Subroutine ReStore_parameters()
+      Implicit None
+      End Subroutine ReStore_parameters
 
       END SUBROUTINE MKSBMAT_G
