bc0a6d33c58c33275e1904897a722d6ee640e936
Roland Lindh <roland.lindh@kemi.uu.se>
2026-02-23 19:58:21 +0100

    Change idciex and idtcex to be arrays with state specific entries.

--- src/caspt2/msgrad.f
+++ src/caspt2/msgrad.f
@@ -713,37 +713,46 @@
       real(kind=wp), intent(inout) :: CI(Nconf)
       real(kind=wp), intent(in) :: U0(nState,nState)
 
-      integer(kind=iwp) :: ID, I
-      real(kind=wp),allocatable :: WRK(:)
 !
 !     MODE=0 is equivalent to LoadCI (XMS basis)
 !     MODE=1 constructs the CI vector in CASSCF basis (back-transformed)
 !     CSF in natural (Bas=N) or quasi-canonical (Bas=C) orbital basis
 !
       If (Bas == 'N' .or. Bas == 'n') Then
-        ID = IDCIEX(1) !! natural
+        Call READ_CI(iState,IDCIEX,SIZE(IDCIEX))
+!       ID = IDCIEX(1) !! natural
       Else If (Bas == 'C' .or. Bas == 'c') Then
-        ID = IDTCEX(1) !! quasi-canonical
+        Call READ_CI(iState,IDTCEX,SIZE(IDTCEX))
+!       ID = IDTCEX(1) !! quasi-canonical
       ELse
         write (u6,*)'the first argument in LoadCI_XMS should be either',
      &              'N (natural) or C (quasi-canonical)'
         call abend()
       End If
 
+      Contains
+
+      Subroutine READ_CI(iState,IDEX,nIDEX)
+      implicit none
+      integer(kind=iwp), intent(in) :: iState, nIDEX, IDEX(nIDEX)
+
+      integer(kind=iwp) :: ID, I
+      real(kind=wp),allocatable :: WRK(:)
+
       If (Mode == 0 .or. (.not.IFXMS .and. .not.IFRMS)) Then
-        do I = 1, iState-1
-          call ddafile(LUCIEX,0,CI,Nconf,ID)
-        end do
+        ID = IDEX(iState)
         call ddafile(LUCIEX,2,CI,Nconf,ID)
       Else If (Mode == 1) Then
         CI(1:nconf) = Zero
         call mma_allocate(WRK,nconf,Label='WRK')
         do I = 1, nState
+          ID = IDEX(I)
           call ddafile(LUCIEX,2,WRK,Nconf,ID)
           CI(1:nconf) = CI(1:nconf) + U0(iState,I)*WRK(1:nconf)
         end do
         call mma_deallocate(WRK)
       End If
+      End Subroutine READ_CI
 
       End Subroutine LoadCI_XMS
 !
