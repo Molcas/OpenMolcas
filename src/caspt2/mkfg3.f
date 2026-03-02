775e35e6a528f0577c362094d56dd13b329bc80e
Roland Lindh <roland.lindh@kemi.uu.se>
2026-03-02 17:41:34 +0100

    Polish.

--- src/caspt2/mkfg3.f
+++ src/caspt2/mkfg3.f
@@ -490,7 +490,12 @@ C-SVC20100309: use simpler procedure by keeping inner ip2-loop intact
         if (.not. DoFCIQMC) then
             IF(mkF) THEN
 * Elementwise multiplication of Tau with H0 diagonal - EPSA(IV):
+                Write (6,*) 'SIZE(BUFT)',SIZE(BUFT)
+                Write (6,*) 'SIZE(BUFD)',SIZE(BUFD)
+                Write (6,*) 'MxCI=',MxCI
+                Write (6,*) 'nSGM1=',nSgm1
                 BufT(1:nSgm1)=(BufD(:)-EpsA(iv))*BufT(:)
+                Write (6,*) 'PASSED'
 !               do icsf=1,nsgm1
 !                 buft(icsf)= (bufd(icsf)-epsa(iv))*buft(icsf)
 !               end do
