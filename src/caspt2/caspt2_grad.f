3f4c33cb308e5dbee463e1ab771c01aebed20d09
Yoshio Nishimoto <nishimoto@kuchem.kyoto-u.ac.jp>
2026-03-22 10:38:11 +0900

    Fix CASPT2 gradient crash in MCLR when nRoots and lRoots are different

--- src/caspt2/caspt2_grad.f
+++ src/caspt2/caspt2_grad.f
@@ -556,6 +556,8 @@
         Call Put_cArray('MCLR Root',mstate1,16)
       end if
 
+      call ModDip()
+
       !! Close files
       Call DaClos(LUSTD)
       If (IfChol) Call DaClos(LUAPT2)
@@ -574,22 +576,41 @@
 
       use stdalloc, only: mma_allocate,mma_deallocate
       use definitions, only: wp, iwp
-      use caspt2_module, only: LROOTS, NROOTS, ROOT2STATE
+      use caspt2_module, only: LROOTS, NROOTS, NSTATE, ROOT2STATE
+      use Constants, only: Zero
 
       implicit none
 
-      real(kind=wp), allocatable :: DMs1(:,:), DMs2(:,:)
-      integer(kind=iwp) :: i, j
+      real(kind=wp), allocatable :: DMs1(:,:),DMs2(:,:)
+      logical(kind=iwp) :: Found
+      integer(kind=iwp) :: nData, i, j
+!
+!     modify the Last Dipole Moments array to avoid crash in
+!     mclr/out_pt2.F90
+!     Note that Last Dipole Moments are actually not used anywhere
+!
+      if (NSTATE == lRoots) return
+
+      call qpg_dArray('Last Dipole Moments',Found,nData)
+      if (nData == 3*NSTATE) return
+
+      if (.not.Found .or. nData /= 3*lRoots) then
+        call WarningMessage(2,'Should not happen in ModDip')
+        call abend()
+      end if
 
       call mma_allocate(DMs1,3,nRoots,Label='DMs1')
+      DMs1(:,:) = Zero
       call mma_allocate(DMs2,3,lRoots,Label='DMs2')
+      DMs2(:,:) = Zero
+
       Call Get_dArray('Last Dipole Moments',DMs2,3*LROOTS)
       Do i = 1, lRoots
         j = Root2State(i)
         If (j == 0) Cycle
         DMs1(:,j) = DMs2(:,i)
       End Do
-      Call Put_dArray('Last Dipole Moments',DMs1,3*nROOTS)
+      Call Put_dArray('Last Dipole Moments',DMs1,3*nRoots)
       call mma_deallocate(DMs1)
       call mma_deallocate(DMs2)
 
