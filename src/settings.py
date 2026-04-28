deaefed735301f61549149266f7d5585074c92aa
Maru Song <m.song@fkf.mpg.de>
2026-04-28 18:52:28 +0200

    Squashed 'Tools/CSF-ROHF_interface/' content from commit 573401115
    git-subtree-dir: Tools/CSF-ROHF_interface
    git-subtree-split: 5734011154d023d474eafbd6fca00502932ad77c

---
+++ src/settings.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Local configuration for the CSF-ROHF interface."""
+
+import os
+
+
+# MOLCAS_PATH: str = os.environ.get("MOLCAS_PATH", "/path/to/your/molcas/build/directory")
