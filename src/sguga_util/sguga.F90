e6f99e700e1308f56a475154b662879306c5da53
Roland Lindh <roland.lindh@kemi.uu.se>
2026-04-30 11:13:17 +0200

    Move csfcount to module sguga.

--- src/sguga_util/sguga.F90
+++ src/sguga_util/sguga.F90
@@ -2163,4 +2163,55 @@ call mma_deallocate(EXS%USGN,safe='*')
 call mma_deallocate(EXS%LSGN,safe='*')
 
 end subroutine SG_Free
+
+subroutine CSFCOUNT(CIS,NSYM,NUW)
+
+implicit none
+type(CIStruct), intent(inout) :: CIS
+integer(kind=iwp), intent(in) :: NSYM
+integer(kind=iwp), intent(out) :: NUW
+integer(kind=iwp) :: ISYDWN, ISYM, ISYTOT, ISYUP, MV, N
+
+! CONSTRUCT OFFSET TABLES FOR UPPER AND LOWER WALKS
+! SEPARATED FOR EACH MIDVERTEX AND SYMMETRY
+
+NUW = 0
+do MV=1,CIS%nMidV
+  do ISYM=1,NSYM
+    CIS%IOW(1,ISYM,MV) = NUW*CIS%nIpWlk
+    NUW = NUW+CIS%NOW(1,ISYM,MV)
+  end do
+end do
+CIS%nWalk = NUW
+do MV=1,CIS%nMidV
+  do ISYM=1,NSYM
+    CIS%IOW(2,ISYM,MV) = CIS%nWalk*CIS%nIpWlk
+    CIS%nWalk = CIS%nWalk+CIS%NOW(2,ISYM,MV)
+  end do
+end do
+
+! CONSTRUCT COUNTER AND OFFSET TABLES FOR THE CSFS
+! SEPARATED BY MIDVERTICES AND SYMMETRY.
+! FORM ALSO CONTRACTED SUMS OVER MIDVERTICES.
+
+CIS%NCSF(:) = 0
+do ISYTOT=1,NSYM
+  do MV=1,CIS%nMidV
+    do ISYUP=1,NSYM
+      ISYDWN = Mul(ISYTOT,ISYUP)
+      N = CIS%NOW(1,ISYUP,MV)*CIS%NOW(2,ISYDWN,MV)
+      CIS%NOCSF(ISYUP,MV,ISYTOT) = N
+      CIS%IOCSF(ISYUP,MV,ISYTOT) = CIS%NCSF(ISYTOT)
+      CIS%NCSF(ISYTOT) = CIS%NCSF(ISYTOT)+N
+#     include "compiler_features.h"
+#     ifdef _BUGGY_INTEL_LLVM_
+      ! dummy statement to work around compiler bug, will never be executed
+      if (ISYUP > 99) CIS%NCSF(ISYTOT) = -1
+#     endif
+    end do
+  end do
+end do
+
+end subroutine CSFCOUNT
+
 end module SGUGA
