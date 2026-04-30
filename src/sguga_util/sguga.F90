d971a0e46419fe4300082de995d5eb2d738a6c8a
Roland Lindh <roland.lindh@kemi.uu.se>
2026-04-30 09:48:03 +0200

    Move MkMAW to module sguga.

--- src/sguga_util/sguga.F90
+++ src/sguga_util/sguga.F90
@@ -51,7 +51,7 @@ integer(kind=iwp) :: L2ACT(MXLEV), LEVEL(MXLEV)
 public :: CIS, CIStruct, EXS, EXStruct, L2ACT, LEVEL, SGS, SGStruct
 
 
-public :: SGINIT, MKSGUGA, MkCOT, MkCList
+public :: SGINIT, MKSGUGA, MkCOT, MkCList, MkMAW
 
 contains
 
@@ -1106,5 +1106,52 @@ end do
 call mma_deallocate(SGS%Scr)
 
 end subroutine MKCLIST
+
+subroutine MKMAW(SGS)
+
+implicit none
+type(SGStruct), intent(inout) :: SGS
+integer(kind=iwp) :: IC, ID, ISUM, IU, IV
+
+call mma_allocate(SGS%MAW,[1,SGS%nVert],[0,3],Label='SGS%MAW')
+
+! COPY LOWER PART OF DIRECT ARC WEIGHT TABLE INTO MAW:
+SGS%MAW(SGS%MVSta:SGS%nVert,0:3) = SGS%DAW(SGS%MVSta:SGS%nVert,0:3)
+! COPY UPPER PART OF REVERSE ARC WEIGHT TABLE INTO MAW. HOWEVER,
+!    NOTE THAT THE MAW TABLE IS ACCESSED BY THE UPPER VERTEX.
+SGS%MAW(1:SGS%MVSta-1,0:3) = 0
+do IU=1,SGS%MVSta-1
+  do IC=0,3
+    ID = SGS%Down(IU,IC)
+    if (ID /= 0) SGS%MAW(IU,IC) = SGS%RAW(ID,IC)
+  end do
+end do
+! FINALLY, ADD AN OFFSET TO ARCS LEADING TO MIDLEVELS:
+ISUM = 1
+do IV=SGS%MVSta,SGS%MVEnd
+  do IC=0,3
+    IU = SGS%Up(IV,IC)
+    if (IU == 0) cycle
+    SGS%MAW(IU,IC) = ISUM+SGS%MAW(IU,IC)
+  end do
+  ISUM = ISUM+SGS%RAW(IV,4)
+end do
+do IV=SGS%MVSta,SGS%MVEnd
+  do IC=0,3
+    if (SGS%Down(IV,IC) == 0) cycle
+    SGS%MAW(IV,IC) = ISUM+SGS%MAW(IV,IC)
+  end do
+  ISUM = ISUM+SGS%DAW(IV,4)
+end do
+#ifdef _DEBUGPRINT_
+write(u6,*)
+write(u6,*) ' THE MODIFIED ARC WEIGHT TABLE IN MKMAW:'
+do IV=1,SGS%nVert
+  write(u6,'(1X,I4,5X,5(1X,I6))') IV,SGS%MAW(IV,0:3)
+end do
+write(u6,*)
+#endif
+
+end subroutine MKMAW
 
 end module SGUGA
