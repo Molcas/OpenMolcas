b8527f5600e9847190c64b64d76e40ec8672638d
Roland Lindh <roland.lindh@kemi.uu.se>
2026-05-04 22:33:10 +0200

    Cleanup of SG interface to CASPT2.

--- src/caspt2/sg_setup_caspt2.F90
+++ src/caspt2/sg_setup_caspt2.F90
@@ -12,6 +12,7 @@
 !***********************************************************************
 SUBROUTINE SG_SETUP_CASPT2()
 
+use Molcas, only: MxLev
 use fciqmc_interface, only: DoFCIQMC
 use stdalloc, only: mma_allocate
 use RefWfn, only: L2Act, Level
@@ -19,12 +20,20 @@ use sguga, only: SGS, CIS, EXS, SG_Init, MkISM_Raw
 use caspt2_module, only: DMRG, DoCumulant, iSCF, iSpin, nActEl,        &
                          nEle3, nHole1, nRas1, nRas2, nRas3, nSym, STSym
 use caspt2_module, only: NLEV=>nAshT, nAsh
-
 use caspt2_module, only: MxCI
 use definitions, only: iwp
 IMPLICIT NONE
 
-INTEGER(kind=iwp) I,IT,ITABS,ILEV,ISYM
+INTEGER(kind=iwp) I,IT,ITABS,ILEV,ISYM, ISM(MxLev)
+
+ITABS=0
+DO ISYM=1,NSYM
+   DO IT=1,NASH(ISYM)
+      ITABS=ITABS+1
+      ILEV=LEVEL(ITABS)
+      ISM(ILEV)=ISYM
+   END DO
+END DO
 
 if ((.NOT.DoCumulant) .and. (nactel.gt.0) .and. (iscf.eq.0)            &
       .and. (.not. DoFCIQMC) .and. (.not. DMRG)) Then
@@ -41,14 +50,14 @@ else
 
    Call MkISM_Raw(SGS,nLev,xLevel=Level,xL2Act=L2Act)
 
-   ITABS=0
-   DO ISYM=1,SGS%NSYM
-      DO IT=1,NASH(ISYM)
-         ITABS=ITABS+1
-         ILEV=LEVEL(ITABS)
-         SGS%ISM(ILEV)=ISYM
-      END DO
+ITABS=0
+DO ISYM=1,NSYM
+   DO IT=1,NASH(ISYM)
+      ITABS=ITABS+1
+      ILEV=LEVEL(ITABS)
+      SGS%ISM(ILEV)=ISYM
    END DO
+END DO
 
 ! INITIALIZE SPLIT-GRAPH GUGA DATA SETS:
    Call mma_allocate(CIS%NCSF,SGS%nSym,Label='CIS%NCSF')
