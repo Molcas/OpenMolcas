0f615eee684f174278a1d597f35968123b02a1aa
Roland Lindh <roland.lindh@kemi.uu.se>
2026-06-05 16:33:29 +0200

    Manual merger of e01906.

--- src/mclr/sg_setup_mclr.F90
+++ src/mclr/sg_setup_mclr.F90
@@ -15,11 +15,12 @@ use molcas, only: MxLev
 use sguga, only: MkCOT, MkSGNum, SG_Init_Simple
 use input_mclr, only: iSpin, nActEl, nElec3, nHole1, nRS1, nRS2, nRS3, nSym
 use input_mclr, only: SGS, CIS, EXS
+use rasdef, only: nRas, nRasEl, nRsPrt
 use Definitions, only: iwp
 
 implicit none
 integer(kind=iwp), intent(in):: pState_Sym
-integer(kind=iwp) :: iBas, nLev, iSym, ISM(1:MxLev), Level(MxLev), iq
+integer(kind=iwp) :: iBas, nLev, iSym, ISM(1:MxLev), Level(MxLev), iq, nRs1T
 
 nLev = 0
 do iSym=1,nSym
@@ -41,11 +42,29 @@ do iSym=1,nSym
   end do
 end do
 
+If (nHole1+nElec3/=0) Then
+   SGS%IFRAS=1
+   nRsPrt=3
+   nRas(:,1)=nRs1(:)
+   nRas(:,2)=nRs2(:)
+   nRas(:,3)=nRs3(:)
+   nRs1T=Sum(nRs1(1:nSym))
+   nRasEl(1)=2*nRs1T-nHole1
+   nRasEl(2)=nActel-nElec3
+   nRasEl(3)=nActel
+Else
+   SGS%IFRAS=0
+   nRsPrt=1
+   nRas(:,1)=nRs2(:)
+   nRasEl(1)=nActel
+End If
+
+
 Level(1:MxLev)=[(iq,iq=1,MxLev)]
 
 Call SG_Init_Simple(nSym,nActEl,iSpin,SGS,CIS,EXS,nHole1,nElec3,nRs1,nRs2,nRs3, &
                     xLevel=Level, xL2Act=Level,                                 &
-                    xNLEV=nLev, xNSM=ISM)
+                    xNLEV=nLev, xNSM=ISM, Do_RMVERT=.True.)
 
 ! PURPOSE: FREE THE GUGA TABLES
 ! FORM VARIOUS OFFSET TABLES:
