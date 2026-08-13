7e5a892cb959d14c157017640afe449a5be414ce
Roland Lindh <roland.lindh@kemi.uu.se>
2026-08-13 17:25:42 +0200

    Disentangle Lucia and the RASSCF.

--- src/rasscf/getgdmat.F90
+++ src/rasscf/getgdmat.F90
@@ -15,7 +15,6 @@ subroutine GetGDMat(GDMat)
 
 use Index_Functions, only: iTri, nTri_Elem
 use ci_interfaces, only: Mk_T1DM
-use lucia_data, only: DStmp, Dtmp
 use rasscf_global, only: iAdr15, lRoots, nAc
 use rasscf_files, only: JOBIPH
 use general_data, only: NCONF
@@ -38,9 +37,9 @@ do jRoot=1,lRoots
   CIDisk2 = IADR15(4)
   do kRoot=1,jRoot
     call DDafile(JOBIPH,2,VecR,nConf,CIDisk2)
-    Call Mk_T1DM(VECR(:),VECL(:),nConf,DTMP,NAC**2)
+    Call Mk_T1DM(VECR(:),VECL(:),nConf,TMPD,NAC**2)
     do IOrb=1,NAC
-      GDMat(iTri(jRoot,kRoot),:,IOrb) = Dtmp((IOrb-1)*NAC+1:IOrb*NAC)
+      GDMat(iTri(jRoot,kRoot),:,IOrb) = TmpD((IOrb-1)*NAC+1:IOrb*NAC)
     end do
   end do
 end do
