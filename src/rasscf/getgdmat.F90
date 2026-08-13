db56e5c9d6a9d0a0904a30a460b9949dc05b8be8
Roland Lindh <roland.lindh@kemi.uu.se>
2026-08-13 15:52:24 +0200

    Remove more of Lucia.

--- src/rasscf/getgdmat.F90
+++ src/rasscf/getgdmat.F90
@@ -14,8 +14,8 @@
 subroutine GetGDMat(GDMat)
 
 use Index_Functions, only: iTri, nTri_Elem
+use ci_interfaces, only: Mk_T1DM
 use lucia_data, only: DStmp, Dtmp
-use Lucia_Interface, only: Lucia_Util
 use rasscf_global, only: iAdr15, lRoots, nAc
 use rasscf_files, only: JOBIPH
 use general_data, only: NCONF
@@ -26,36 +26,25 @@ use Definitions, only: wp, iwp
 implicit none
 real(kind=wp), intent(out) :: GDMat(nTri_Elem(lRoots),NAC,NAC)
 integer(kind=iwp) :: CIDisk1, CIDisk2, iOrb, jRoot, kRoot
-real(kind=wp), allocatable :: SDtmp(:), TmpD(:), VecL(:), VecR(:)
+real(kind=wp), allocatable :: TmpD(:), VecL(:), VecR(:)
 
 call mma_allocate(VecL,NConf,Label='VecL')
 call mma_allocate(VecR,NConf,Label='VecR')
 call mma_allocate(TmpD,NAC**2,Label='TmpD')
-call mma_allocate(SDtmp,NAC**2,Label='SDtmp')
-SDtmp(:) = DStmp(:)
-DStmp(:) = Zero
-TmpD(:) = Dtmp(:)
-Dtmp(:) = Zero
+
 CIDisk1 = IADR15(4)
 do jRoot=1,lRoots
   call DDafile(JOBIPH,2,VecL,nConf,CIDisk1)
   CIDisk2 = IADR15(4)
   do kRoot=1,jRoot
     call DDafile(JOBIPH,2,VecR,nConf,CIDisk2)
-    !write(u6,*) 'VecL and VecR for states',jRoot,kRoot
-    !write(u6,*) (VecL(I),I=0,NConf-1)
-    !write(u6,*) (VecR(I),I=0,NConf-1)
-    call Lucia_Util('Densi',CI_Vector=VecL(:),RVEC=VECR(:))
-    !write(u6,*) 'GDMat for states',jRoot,kRoot
+    Call Mk_T1DM(VECR(:),VECL(:),nConf,DTMP,NAC**2)
     do IOrb=1,NAC
       GDMat(iTri(jRoot,kRoot),:,IOrb) = Dtmp((IOrb-1)*NAC+1:IOrb*NAC)
-      !write(u6,'(10(F8.4,2X))') (GDMat(iTri(jRoot,kRoot),JOrb,IOrb),JOrb=1,NAC)
     end do
   end do
 end do
-DStmp(:) = SDtmp(:)
-DTmp(:) = TmpD(:)
-call mma_deallocate(SDtmp)
+
 call mma_deallocate(TmpD)
 call mma_deallocate(VecL)
 call mma_deallocate(VecR)
