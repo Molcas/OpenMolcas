7c2ad583ab7129fac493cd47f765bd3743e18547
Roland Lindh <roland.lindh@kemi.uu.se>
2026-08-19 22:27:53 +0200

    cleanup.

--- src/molcas_ci_util/spinfo.F90
+++ src/molcas_ci_util/spinfo.F90
@@ -25,7 +25,7 @@ module Spinfo
 ! nSpeed, iSpeed
 !
 ! ELIMINATED_IN_GAS, 2ELIMINATED_IN_GAS,
-! ms2, ispin, STSYM, itmax, nroots, ipt2,
+! ms2, STSYM, itmax, nroots, ipt2,
 ! iprci, ngas, INOCALC, ISAVE_EXP, N_ELIMINATED_GAS, N_2ELIMINATED_GAS,
 ! I_ELIMINATE_GAS, nCSF_HEXS
 
@@ -39,13 +39,13 @@ integer(kind=iwp), parameter :: MXTYP = 30, nSpeed = 5
 
 integer(kind=iwp) :: I2ELIMINATED_IN_GAS(MxGAS), I_ELIMINATE_GAS, IELIMINATED_IN_GAS(MxGAS), &
                      INOCALC, iprci, ipt2, ISAVE_EXP, iSpeed(nSpeed), &
-                     ispin, itmax, STSYM, MINOP, MS2, N_2ELIMINATED_GAS, &
+                     itmax, STSYM, MINOP, MS2, N_2ELIMINATED_GAS, &
                      N_ELIMINATED_GAS, NCNASM(mxSym), NCNFTP(MXTYP,mxSym), NCSASM(mxSym), nCSF_HEXS, &
                      NCSFTP(MXTYP), NDET, NDTASM(mxSym), NDTFTP(MXTYP), ngas, nroots, NTYP
 logical(kind=iwp) :: DoComb
 
 public :: DoComb, I2ELIMINATED_IN_GAS, I_ELIMINATE_GAS, IELIMINATED_IN_GAS, &
-          INOCALC, iprci, ipt2, ISAVE_EXP, iSpeed, ispin, itmax, STSYM, MINOP, &
+          INOCALC, iprci, ipt2, ISAVE_EXP, iSpeed, itmax, STSYM, MINOP, &
           MS2, N_2ELIMINATED_GAS, N_ELIMINATED_GAS, NCNASM, NCNFTP, NCSASM, nCSF_HEXS, &
           NCSFTP, NDET, NDTASM, NDTFTP, ngas, nroots, NTYP
 
