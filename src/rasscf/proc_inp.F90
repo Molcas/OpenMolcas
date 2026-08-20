df5518f61aab4e6d470852a7f3f019627623afbc
Roland Lindh <roland.lindh@kemi.uu.se>
2026-08-20 21:50:56 +0200

    Cleanup .

--- src/rasscf/proc_inp.F90
+++ src/rasscf/proc_inp.F90
@@ -4048,7 +4048,7 @@ if (.not. SkipGUGA) then
 
   if (DBG) write(u6,*) ' Construct the determinant tables.'
 
-  if (.not. (Key('DMRG') .or. DoDMRG .or. DoNECI .or. Do_CC_CI .or. DumpOnly)) then
+  if (.not. (DoNECI .or. Do_CC_CI .or. DumpOnly)) then
 
 
     ! switch on/off determinants
